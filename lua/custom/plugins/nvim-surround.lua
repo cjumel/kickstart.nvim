81907dbb7fd41dbd4ea01baad0738f8476caebfe
clementjumel <clement.jumel@gmail.com>
2023-09-06 18:48:56 +0200

    ✨ Nvim-surround plugin

---
+++ lua/custom/plugins/nvim-surround.lua
@@ -0,0 +1,9 @@
+-- nvim-surround
+--
+-- Provide tools to surround or modify surroundings efficiently, juste like vim-surround.
+
+return {
+  'kylechui/nvim-surround',
+  event = 'VeryLazy',
+  opts = {},
+}
