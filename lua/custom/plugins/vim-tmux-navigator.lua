ac3820a19851a2ee2c8f1b70409445511ecc8cb8
clementjumel <clement.jumel@gmail.com>
2023-09-06 09:33:52 +0200

    ✨ Vim-tmux-navigator plugin

---
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,15 @@
+-- vim-tmux-navigator
+--
+-- Enable to use control + h/j/k/l seamlessly to navigate between neovim windows and tmux panes
+-- when running neovim within tmux
+
+return {
+  'christoomey/vim-tmux-navigator',
+  lazy = false,
+  config = function()
+    vim.keymap.set('n', '<C-h>', '<cmd> TmuxNavigateLeft <CR>', { desc = 'Window left' })
+    vim.keymap.set('n', '<C-l>', '<cmd> TmuxNavigateRight <CR>', { desc = 'Window right' })
+    vim.keymap.set('n', '<C-j>', '<cmd> TmuxNavigateDown <CR>', { desc = 'Window down' })
+    vim.keymap.set('n', '<C-k>', '<cmd> TmuxNavigateUp <CR>', { desc = 'Window up' })
+  end,
+}
