d94a04a3f6fa2309865072499e594522a1b67c68
clementjumel <clement.jumel@gmail.com>
2023-09-06 23:58:55 +0200

    ✨ Keymaps for vsplit & split

--- init.lua
+++ init.lua
@@ -235,11 +235,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- Maps for splits
+vim.keymap.set('n', '<leader>vs', '<cmd> vsplit <CR>', { desc = '[V]ertical [S]plit' })
+vim.keymap.set('n', '<leader>hs', '<cmd> split <CR>', { desc = '[H]orizontal [S]plit' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
