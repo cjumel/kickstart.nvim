bdd76db404deb211eade3b54420fcc7c80919f4d
clementjumel <clement.jumel@gmail.com>
2023-09-06 12:11:18 +0200

    ✨ Todo-comments plugin

---
+++ lua/custom/plugins/todo-comments.lua
@@ -0,0 +1,24 @@
+-- todo-comments
+--
+-- Enable highlighting for TODO comments, but also, FIXME, BUG, and others.
+-- Additionally, provide commands to list and access them.
+
+vim.keymap.set('n', '<leader>td', '<cmd> TodoQuickFix <CR>', { desc = 'Display TODO comments in quickfix tab' })
+
+return {
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  lazy = false,
+  opts = {
+    search = {
+      args = {
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--hidden',
+      },
+    },
+  },
+}
