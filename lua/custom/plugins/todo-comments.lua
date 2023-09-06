b1b3b936c6638ceedf0e7d82315cbeb51a53d5d8
clementjumel <clement.jumel@gmail.com>
2023-09-06 12:29:24 +0200

    ⚡️ Don't recognie NOTE with todo-comments

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -10,6 +10,22 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   lazy = false,
   opts = {
+    -- keywords recognized as todo comments
+    keywords = {
+      FIX = {
+        icon = ' ', -- icon used for the sign, and in search results
+        color = 'error', -- can be a hex color, or a named color (see below)
+        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+        -- signs = false, -- configure signs for some keywords individually
+      },
+      TODO = { icon = ' ', color = 'info' },
+      HACK = { icon = ' ', color = 'warning' },
+      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+      PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+      -- NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+    },
+    merge_keywords = false, -- when true, custom keywords will be merged with the defaults
     search = {
       args = {
         '--color=never',
