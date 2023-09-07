df6dd35dde191444f93d1507227408f1d42c95e2
clementjumel <clement.jumel@gmail.com>
2023-09-07 16:44:59 +0200

    ✨ More keymaps for gitsigns

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -2,14 +2,6 @@
 --
 -- Super fast git decorations and utilities implemented purely in Lua.
 
--- TODO: try:
--- map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
--- map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
--- map('n', '<leader>hu', gs.undo_stage_hunk)
--- map('n', '<leader>td', gs.toggle_deleted)
--- -- Text object
--- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-
 return {
   'lewis6991/gitsigns.nvim',
   opts = {
@@ -46,9 +38,21 @@ return {
       -- Actions
       vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Git [H]unk: [P]review' })
       vim.keymap.set('n', '<leader>hs', require('gitsigns').stage_hunk, { buffer = bufnr, desc = 'Git [H]unk: [S]tage' })
+      vim.keymap.set('v', '<leader>hs', function()
+        require('gitsigns').stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { buffer = bufnr, desc = 'Git [H]unk: [S]tage' })
       vim.keymap.set('n', '<leader>hS', require('gitsigns').stage_buffer, { buffer = bufnr, desc = 'Git [H]unk: [S]tage All' })
+      vim.keymap.set('n', '<leader>hu', require('gitsigns').undo_stage_hunk, { buffer = bufnr, desc = 'Git [H]unk: [U]ndo Stage' })
       vim.keymap.set('n', '<leader>hr', require('gitsigns').reset_hunk, { buffer = bufnr, desc = 'Git [H]unk: [R]eset' })
+      vim.keymap.set('v', '<leader>hr', function()
+        require('gitsigns').reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { buffer = bufnr, desc = 'Git [H]unk: [R]eset' })
       vim.keymap.set('n', '<leader>hR', require('gitsigns').reset_buffer, { buffer = bufnr, desc = 'Git [H]unk: [R]eset All' })
+      vim.keymap.set('n', '<leader>hd', require('gitsigns').toggle_deleted, { buffer = bufnr, desc = 'Git [H]unk: Toggle [D]eleted' })
+
+      -- Text object
+      vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { buffer = bufnr, desc = '[I]nside Git [H]unk Object' })
+      vim.keymap.set({ 'o', 'x' }, 'ah', ':<C-U>Gitsigns select_hunk<CR>', { buffer = bufnr, desc = '[A]rround Git [H]unk Object' })
     end,
   },
 }
