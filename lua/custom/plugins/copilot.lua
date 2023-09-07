406edecf1a6fc5b07dd93d390fc79c11bed23fc7
clementjumel <clement.jumel@gmail.com>
2023-09-07 15:42:30 +0200

    🐛 Fix copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,8 +3,6 @@
 -- Vim implementation of GitHub's Copilot.
 -- Invoke `:Copilot setup` when using for the first time.
 
--- FIXME: it deson't seem to be working
-
 return {
   'github/copilot.vim',
   event = 'InsertEnter',
