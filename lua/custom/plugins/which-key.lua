acdd21988b5e0bf75553b9e54177c5b6e4d32da8
clementjumel <clement.jumel@gmail.com>
2023-09-08 15:39:30 +0200

    💡 Mention marks & registers in which-key

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,6 +1,6 @@
 -- Which Key
 --
--- Show the available keymaps once you start writink a few keys.
+-- Show the available keymaps once you start writink a few keys, the marks and registers.
 
 return {
   'folke/which-key.nvim',
