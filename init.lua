2f1659e27172ae637cedbc9603022e1b53d9a2cc
clementjumel <clement.jumel@gmail.com>
2023-09-09 04:38:41 +0200

    ⚡️ Improve treesitter defaults

--- init.lua
+++ init.lua
@@ -206,8 +206,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
-  -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = {
+  ensure_installed = { -- Languages treesitter must install
     'bash',
     'csv',
     'diff',
@@ -228,18 +227,22 @@ require('nvim-treesitter.configs').setup {
     'vim',
     'yaml',
   },
-
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
-    -- Install languages synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    -- List of parsers to ignore installing
-    ignore_install = {},
-    -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
-    modules = {},
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
+  sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
+  auto_install = false, -- Autoinstall languages that are not installed
+  ignore_install = {}, -- List of parsers to ignore installing (or "all")
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  textobjects = {
+    select = {
       enable = true,
       keymaps = {
         init_selection = '<c-space>',
@@ -248,48 +251,24 @@ require('nvim-treesitter.configs').setup {
         node_decremental = '<M-space>',
       },
     },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ['[f'] = '@function.outer',
+        ['[c'] = '@class.outer',
       },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
+      goto_next_end = {
+        ['[F'] = '@function.outer',
+        ['[C'] = '@class.outer',
       },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
+      goto_previous_start = {
+        [']f'] = '@function.outer',
+        [']c'] = '@class.outer',
+      },
+      goto_previous_end = {
+        [']F'] = '@function.outer',
+        [']C'] = '@class.outer',
       },
     },
   }
