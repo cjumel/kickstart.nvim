2d40d6f6493e4ed37a78b030afc2d336b04615ea
clementjumel <clement.jumel@gmail.com>
2023-09-09 16:09:21 +0200

    🚚 Move nvim-cmp to plugins directory

---
+++ lua/custom/plugins/nvim-cmp.lua
@@ -0,0 +1,20 @@
+-- nvim-cmp
+--
+-- Nvim-cmp is a lightweight and extensible completion plugin for neovim written in lua.
+
+return {
+  -- Autocompletion
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    -- Snippet Engine & its associated nvim-cmp source
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+
+    -- Adds LSP completion capabilities
+    'hrsh7th/cmp-nvim-lsp',
+
+    -- Adds a number of user-friendly snippets
+    'rafamadriz/friendly-snippets',
+  },
+}
