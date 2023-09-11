430345db3d78d69cb3e15061755fe81bd0d1a2a9
clementjumel <clement.jumel@gmail.com>
2023-09-11 10:07:08 +0200

    ✨ Copilot AcceptOneWord keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,4 +6,13 @@
 return {
   'github/copilot.vim',
   event = 'InsertEnter',
+  init = function()
+    local AcceptOneWord = function()
+      vim.fn['copilot#Accept'] ''
+      local bar = vim.fn['copilot#TextQueuedForInsertion']()
+      return vim.fn.split(bar, [[[ .]\zs]])[1]
+    end
+
+    vim.keymap.set('i', '<s-tab>', AcceptOneWord, { expr = true, remap = false, desc = 'copilot#AcceptOneWord()' })
+  end,
 }
