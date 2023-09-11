3a5cad91bfb18e3f4040ebcd5aefb23b4a7e480a
clementjumel <clement.jumel@gmail.com>
2023-09-11 15:30:25 +0200

    💡 Simplify lsp comments

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -27,17 +27,10 @@ return {
   config = function()
     --  This function gets run when an LSP connects to a particular buffer.
     local on_attach = function(_, bufnr)
-      -- NOTE: Remember that lua is a real programming language, and as such it is possible
-      -- to define small helper and utility functions so you don't have to repeat yourself
-      -- many times.
-      --
-      -- In this case, we create a function that lets us more easily define mappings specific
-      -- for LSP related items. It sets the mode, buffer and description for us each time.
       local nmap = function(keys, func, desc)
         if desc then
           desc = 'LSP: ' .. desc
         end
-
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
       end
 
@@ -46,6 +39,7 @@ return {
 
       nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
       -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+      -- vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
       nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
       -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
@@ -70,22 +64,12 @@ return {
       end, { desc = 'Format current buffer with LSP' })
     end
 
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. They will be passed to
-    --  the `settings` field of the server config. You must look up that documentation yourself.
-    --
-    --  If you want to override the default filetypes that your language server will attach to you can
-    --  define the property 'filetypes' to the map in question.
+    -- Enable the following language servers. They will automatically be installed.
+    -- Add any additional override configuration in the following tables. They will be passed to
+    -- the `settings` field of the server config. If you want to override the default filetypes that
+    -- your language server will attach to you can define the property 'filetypes' to the map in
+    -- question.
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- tsserver = {},
-      -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
       lua_ls = {
         Lua = {
           workspace = { checkThirdParty = false },
