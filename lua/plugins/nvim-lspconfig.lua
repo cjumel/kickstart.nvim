39d1e914a7df8217ef58249873f74d3cd79171c7
clementjumel <clement.jumel@gmail.com>
2023-09-13 10:35:56 +0200

    🚚 Move cmp definition back in own file
    This reverts commit 026b56ddf89b42ed5ea20e1d91a3e818c100d321.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,6 @@
 -- nvim-lspconfig
 --
--- Define the LSP configurations, and affilitated plugins, such as auto-completion or LSP-related
--- code-navigation.
+-- Define the LSP configurations. This is where the plugins related to LSP should be installed.
 
 return {
   -- LSP Configuration & Plugins
@@ -20,8 +19,6 @@ return {
     -- nvim-cmp supports additional completion capabilities
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
 
     -- For code navigation
     'folke/trouble.nvim',
@@ -109,32 +106,6 @@ return {
           filetypes = (servers[server_name] or {}).filetypes,
         }
       end,
-    }
-
-    -- Auto-completion
-    local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    require('luasnip.loaders.from_vscode').lazy_load()
-    luasnip.config.setup {}
-
-    cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert {
-        ['<C-o>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      },
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-      },
     }
   end,
 }
