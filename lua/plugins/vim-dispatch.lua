89b934308d9408d3cf33cfeedca0a4c712343590
clementjumel <clement.jumel@gmail.com>
2023-09-25 22:32:41 +0200

    ✨ GitCommit command & keymap with dispatch

--- lua/plugins/vim-dispatch.lua
+++ lua/plugins/vim-dispatch.lua
@@ -5,17 +5,31 @@
 -- TODO:
 -- - Setup commands: python file, pytest all, pytest file, poetry update
 
+local create_user_cmd = function()
+  vim.api.nvim_create_user_command("GitCommit", function()
+    vim.cmd("Dispatch git commit --no-edit")
+  end, { desc = "Git commit" })
+end
+
 return {
   "tpope/vim-dispatch",
-  cmd = "Dispatch",
-  -- TODO: remove init function
+  cmd = {
+    "Dispatch",
+    "GitCommit",
+  },
+  keys = {
+    {
+      "<leader>gcm",
+      function()
+        vim.cmd("GitCommit")
+      end,
+      desc = "[G]it [C]ommit with [M]essage",
+    },
+  },
   init = function()
-    vim.keymap.set("n", "<leader>rs", "<cmd> Dispatch python % <CR>", { desc = "[R]un [S]cript" })
-    vim.keymap.set(
-      "n",
-      "<leader>rt",
-      "<cmd> Dispatch pytest % --no-header <CR>",
-      { desc = "[R]un [T]ests" }
-    )
+    create_user_cmd()
+  end,
+  config = function()
+    create_user_cmd()
   end,
 }
