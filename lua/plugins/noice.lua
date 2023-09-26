ce6474082e32c42afc513a1b9c9ee81168a13c76
clementjumel <clement.jumel@gmail.com>
2023-09-26 22:15:11 +0200

    ✨ Noice plugin

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,24 @@
+-- Noice
+--
+-- Highly experimental plugin that completely replaces the UI for messages, cmdline and the
+-- popupmenu.
+
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+  },
+}
