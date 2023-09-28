f4603d94af12bdf67411ad36b9e1c8a228f1e260
clementjumel <clement.jumel@gmail.com>
2023-09-28 20:20:49 +0200

    🩹 Adapt indent-blank-line config to latest version

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,8 +4,10 @@
 
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   opts = {
-    char = "┊",
-    show_trailing_blankline_indent = false,
+    indent = {
+      char = "┊",
+    },
   },
 }
