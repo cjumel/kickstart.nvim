6588357f4ee262356977111cf629e85b646f4d31
clementjumel <clement.jumel@gmail.com>
2023-10-03 11:02:21 +0200

    ✨ Transparent.nvim plugin

---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,8 @@
+-- transparent.nvim
+--
+-- Remove all background colors to make nvim transparent. Call `TansparentToggle` to enable/disable.
+
+return {
+  "xiyaowong/transparent.nvim",
+  priority = 1000, -- UI stuff should be loaded first
+}
