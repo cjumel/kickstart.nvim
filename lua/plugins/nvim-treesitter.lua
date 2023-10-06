1b8ee3ae79f744a1a41835b049029f8a18a728c5
clementjumel <clement.jumel@gmail.com>
2023-10-06 09:46:27 +0200

    ⚡️ Better keymaps for incremental selection

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -41,10 +41,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
+          init_selection = "<leader>i",
+          node_incremental = "<leader>i",
+          scope_incremental = false,
+          node_decremental = "<bs>",
         },
       },
       textobjects = {
