809dc713ef4f02065a79c4d2443eb6df1ea37d2d
clementjumel <clement.jumel@gmail.com>
2023-10-12 14:44:42 +0200

    ✨ TreeSJ plugin

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,30 @@
+-- TreeSJ
+--
+-- Neovim plugin for splitting/joining blocks of code like arrays, hashes, statements, objects, dictionaries, etc.
+
+return {
+  "Wansmer/treesj",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  keys = {
+    {
+      "gS",
+      function()
+        require("treesj").split()
+      end,
+      desc = "[S]plit current node",
+    },
+    {
+      "gJ",
+      function()
+        require("treesj").join()
+      end,
+      desc = "[J]oin current node",
+    },
+  },
+  opts = {
+    use_default_keymaps = false,
+    max_join_length = 100,
+  },
+}
