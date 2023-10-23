6e1ba5550f8af753e2f482913c12caa6445f5bfa
clementjumel <clement.jumel@gmail.com>
2023-10-24 00:12:30 +0200

    ✨ New todo add keymap on line before cursor

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -16,7 +16,19 @@ return {
         require("Comment.api").comment.linewise.current()
         vim.api.nvim_command("normal! $")
       end,
-      desc = "[T]odo: [A]dd",
+      desc = "[T]odo: [A]dd (line after the cursor)",
+    },
+    {
+      "<leader>tA",
+      function()
+        vim.api.nvim_command("normal! O")
+        -- The new line is either empty or with a comment prefix, let's remove it, if needed,
+        -- before adding the "TODO"
+        vim.api.nvim_command("normal! ^DaTODO: ")
+        require("Comment.api").comment.linewise.current()
+        vim.api.nvim_command("normal! $")
+      end,
+      desc = "[T]odo: [A]dd (line before the cursor)",
     },
   },
   opts = {},
