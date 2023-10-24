f62facc9bb221f86d4622d7add0f910afdce11bf
clementjumel <clement.jumel@gmail.com>
2023-10-24 15:28:23 +0200

    ✨ Document keychains with which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,5 +6,20 @@ return {
   "folke/which-key.nvim",
   cmd = { "WhichKey" },
   keys = { "<leader>", "<c-r>", '"', "'", "`", "d", "c", "y", "v", "g", "[", "]" },
-  opts = {},
+  config = function()
+    require("which-key").setup({})
+
+    -- document existing key chains
+    require("which-key").register({
+      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+      ["<leader>d"] = { name = "[D]iagnostics/[D]ebug", _ = "which_key_ignore" },
+      ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
+      ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+      ["<leader>gc"] = { name = "[G]it [C]ommit", _ = "which_key_ignore" },
+      ["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
+      ["<leader>q"] = { name = "[Q]uick", _ = "which_key_ignore" },
+      ["<leader>s"] = { name = "[S]tand", _ = "which_key_ignore" },
+      ["<leader>t"] = { name = "[T]odo/[T]rouble", _ = "which_key_ignore" },
+    })
+  end,
 }
