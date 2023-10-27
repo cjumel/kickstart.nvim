4430fb455ce1d810008dc49aeb13356c01dc3584
clementjumel <clement.jumel@gmail.com>
2023-10-27 11:03:04 +0200

    💡 Simplify some comments

--- lua/plugins/treesitter/nvim-treesitter-textobjects.lua
+++ lua/plugins/treesitter/nvim-treesitter-textobjects.lua
@@ -15,20 +15,17 @@ return {
   config = function()
     local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
-    -- Repeat movement with ; and ,
-    -- ensure ; goes forward and , goes backward regardless of the last direction
+    -- Repeat movement with ; (forward) and , (backward)
     vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
     vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
 
-    -- Define repeatable goto next/previous paragraph movements
-    -- make sure forward function comes first
+    -- Repeatable paragraph movements
     local next_paragraph_repeat, prev_paragraph_repeat =
       ts_repeat_move.make_repeatable_move_pair(next_paragraph, previous_paragraph)
     vim.keymap.set({ "n", "x", "o" }, "[p", next_paragraph_repeat, { desc = "Next paragraph" })
     vim.keymap.set({ "n", "x", "o" }, "]p", prev_paragraph_repeat, { desc = "Previous paragraph" })
 
-    -- Make diagnostic navigation repeatable
-    -- make sure forward function comes first
+    -- Repeatable diagnostic movements
     local next_diagnostic_repeat, prev_diagnostic_repeat =
       ts_repeat_move.make_repeatable_move_pair(vim.diagnostic.goto_next, vim.diagnostic.goto_prev)
     vim.keymap.set("n", "[?", next_diagnostic_repeat, { desc = "Next diagnostic" })
