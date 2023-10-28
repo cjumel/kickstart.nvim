eb63dfd64d7e942323a609d63bf08de59af37186
clementjumel <clement.jumel@gmail.com>
2023-10-28 10:53:10 +0200

    ♻️ Separate telescope config in several files

--- lua/plugins/telescope/telescope.lua
+++ lua/plugins/telescope/telescope.lua
@@ -1,4 +1,4 @@
--- Telescope.nvim
+-- telescope.nvim
 --
 -- Gaze deeply into unknwn regions of your code with powerful and blazing fast fuzzy finding
 -- tools.
@@ -8,21 +8,10 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope-fzf-native.nvim",
+    "nvim-treesitter/nvim-treesitter",
     "folke/trouble.nvim",
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-    -- Only load if `make` is available. Make sure you have the system
-    -- requirements installed.
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      -- NOTE: If you are having trouble with this installation,
-      --       refer to the README for telescope-fzf-native for more instructions.
-      build = "make",
-      cond = function()
-        return vim.fn.executable("make") == 1
-      end,
-    },
   },
   keys = {
     -- General
