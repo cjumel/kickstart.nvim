6711791759309cbaac6b62197385cc0933265f03
clementjumel <clement.jumel@gmail.com>
2023-10-31 17:12:18 +0100

    ✨ telescope-undo plugin

---
+++ lua/plugins/telescope/telescope-undo.lua
@@ -0,0 +1,41 @@
+-- telescope-undo.nvim
+--
+-- Visualize your undo tree and fuzzy-search changes in it.
+
+return {
+  "debugloop/telescope-undo.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  keys = {
+    {
+      "<leader>u",
+      function()
+        require("telescope").extensions.undo.undo()
+      end,
+      desc = "[U]ndo tree",
+    },
+  },
+  config = function()
+    require("telescope").setup({
+      extensions = {
+        undo = {
+          diff_context_lines = 3,
+          layout_strategy = "vertical",
+          initial_mode = "normal",
+          mappings = {
+            i = {
+              ["<cr>"] = require("telescope-undo.actions").yank_additions,
+            },
+            n = {
+              ["<cr>"] = require("telescope-undo.actions").yank_additions,
+              ["<bs>"] = require("telescope-undo.actions").yank_deletions,
+              ["="] = require("telescope.actions").nop,
+            },
+          },
+        },
+      },
+    })
+    require("telescope").load_extension("undo")
+  end,
+}
