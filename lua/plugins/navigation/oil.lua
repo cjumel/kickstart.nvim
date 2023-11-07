b3d5537444220447f0ecc041b30d9730b1648a4a
clementjumel <clement.jumel@gmail.com>
2023-11-07 11:17:10 +0100

    ⚡️ Disable line numbers in oil buffer

--- lua/plugins/navigation/oil.lua
+++ lua/plugins/navigation/oil.lua
@@ -19,56 +19,61 @@ return {
       desc = "[-] Open parent directory",
     },
   },
-  opts = {
-    default_file_explorer = false,
-    keymaps = {
-      ["g?"] = "actions.show_help",
-      ["<CR>"] = "actions.select",
-      ["<C-v>"] = "actions.select_vsplit",
-      ["<C-x>"] = "actions.select_split",
-      ["<C-t>"] = "actions.select_tab",
-      ["<tab>"] = "actions.preview",
-      ["q"] = "actions.close",
-      ["-"] = "actions.parent",
-      ["_"] = "actions.open_cwd",
-      ["H"] = "actions.toggle_hidden",
-      ["R"] = "actions.refresh",
-      -- Overwrite Harpoon keymap to add the file under the cursor in Oil buffer instead
-      -- of Oil buffer itself
-      ["<leader><CR>"] = function()
-        local entry = require("oil").get_cursor_entry()
-        if entry == nil then
-          return
-        end
-        local dir = require("oil").get_current_dir()
-        local path = dir .. entry.name
-        require("plugins.navigation.utils.harpoon").add_harpoon_file(path)
-      end,
-    },
-    use_default_keymaps = false,
-    view_options = {
-      show_hidden = false,
-      is_always_hidden = function(name, _)
-        local always_hidden_names = {
-          ".git",
-          ".DS_Store",
-          "__pycache__",
-        }
-        for _, always_hidden_name in ipairs(always_hidden_names) do
-          if name == always_hidden_name then
-            return true
+  config = function()
+    require("oil").setup({
+      default_file_explorer = false,
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-x>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<tab>"] = "actions.preview",
+        ["q"] = "actions.close",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["H"] = "actions.toggle_hidden",
+        ["R"] = "actions.refresh",
+        -- Overwrite Harpoon keymap to add the file under the cursor in Oil buffer instead
+        -- of Oil buffer itself
+        ["<leader><CR>"] = function()
+          local entry = require("oil").get_cursor_entry()
+          if entry == nil then
+            return
           end
-        end
+          local dir = require("oil").get_current_dir()
+          local path = dir .. entry.name
+          require("plugins.navigation.utils.harpoon").add_harpoon_file(path)
+        end,
+      },
+      use_default_keymaps = false,
+      view_options = {
+        show_hidden = false,
+        is_always_hidden = function(name, _)
+          local always_hidden_names = {
+            ".git",
+            ".DS_Store",
+            "__pycache__",
+          }
+          for _, always_hidden_name in ipairs(always_hidden_names) do
+            if name == always_hidden_name then
+              return true
+            end
+          end
 
-        local always_hidden_name_starts = {
-          ".null-ls_",
-        }
-        for _, always_hidden_name_start in ipairs(always_hidden_name_starts) do
-          if vim.startswith(name, always_hidden_name_start) then
-            return true
+          local always_hidden_name_starts = {
+            ".null-ls_",
+          }
+          for _, always_hidden_name_start in ipairs(always_hidden_name_starts) do
+            if vim.startswith(name, always_hidden_name_start) then
+              return true
+            end
           end
-        end
-      end,
-    },
-  },
+        end,
+      },
+    })
+
+    -- Disable line numbers in Oil buffer
+    vim.api.nvim_command("autocmd FileType oil setlocal nonumber")
+  end,
 }
