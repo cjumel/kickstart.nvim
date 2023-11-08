bd19229b643eff1797beb8b05e67d3a0aed2bb0d
clementjumel <clement.jumel@gmail.com>
2023-11-08 20:02:05 +0100

    🚚 Move neoclip functions to utils

--- lua/plugins/navigation/telescope/nvim-neoclip.lua
+++ lua/plugins/navigation/telescope/nvim-neoclip.lua
@@ -2,19 +2,6 @@
 --
 -- neoclip is a clipboard manager for neovim inspired for example by clipmenu.
 
-local function is_whitespace(line)
-  return vim.fn.match(line, [[^\s*$]]) ~= -1
-end
-
-local function all(tbl, check)
-  for _, entry in ipairs(tbl) do
-    if not check(entry) then
-      return false
-    end
-  end
-  return true
-end
-
 return {
   "AckslD/nvim-neoclip.lua",
   dependencies = {
@@ -66,8 +53,6 @@ return {
       },
     },
     -- Don't store pure whitespace yanks
-    filter = function(data)
-      return not all(data.event.regcontents, is_whitespace)
-    end,
+    filter = require("plugins.navigation.telescope.utils.filters").whitespace_yank_filter_fn,
   },
 }
