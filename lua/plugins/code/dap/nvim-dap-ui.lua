e6b973b8e0d8172d3c68afdadd619bd67d67ec25
clementjumel <clement.jumel@gmail.com>
2023-11-13 15:18:04 +0100

    ⚡️ Customize DAP UI

--- lua/plugins/code/dap/nvim-dap-ui.lua
+++ lua/plugins/code/dap/nvim-dap-ui.lua
@@ -21,7 +21,52 @@ return {
     local dap = require("dap")
     local dapui = require("dapui")
 
-    dapui.setup()
+    dapui.setup({
+      layouts = {
+        {
+          elements = {
+            -- Vertical elements are displayed bottom to top
+            {
+              id = "scopes",
+              size = 0.3,
+            },
+            {
+              id = "watches",
+              size = 0.3,
+            },
+            {
+              id = "breakpoints",
+              size = 0.3,
+            },
+            {
+              id = "repl",
+              size = 0.1,
+            },
+          },
+          position = "right",
+          size = 30,
+        },
+        {
+          elements = {
+
+            {
+              id = "console",
+              size = 1.0,
+            },
+          },
+          position = "bottom",
+          size = 10,
+        },
+      },
+      mappings = {
+        edit = "e",
+        expand = { "<CR>", "<2-LeftMouse>", "<tab>" },
+        open = "o",
+        remove = "d",
+        repl = "r",
+        toggle = "t",
+      },
+    })
 
     dap.listeners.after.event_initialized["dapui_config"] = function()
       dapui.open()
