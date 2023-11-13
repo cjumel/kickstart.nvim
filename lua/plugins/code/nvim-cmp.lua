3736f8cd3d08b0d290e2de1cb20f129fc4c7a82b
clementjumel <clement.jumel@gmail.com>
2023-11-13 23:53:04 +0100

    ✨ Add snippet-related keymaps

--- lua/plugins/code/nvim-cmp.lua
+++ lua/plugins/code/nvim-cmp.lua
@@ -50,5 +50,15 @@ return {
         { name = "buffer" },
       },
     })
+
+    -- Snippet-related keymaps
+    vim.keymap.set({ "i", "s" }, "<C-y>", function()
+      ls.jump(1)
+    end, { silent = true, desc = "Jump to next snippet placeholder" })
+    vim.keymap.set({ "i", "s" }, "<C-e>", function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end, { silent = true, desc = "Change snippet choice" })
   end,
 }
