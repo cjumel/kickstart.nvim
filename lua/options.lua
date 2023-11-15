7cdc570781079c444792a6191fb1e0a6cd2fa300
clementjumel <clement.jumel@gmail.com>
2023-11-15 02:09:17 +0100

    ✨ Merge sign & number columns

--- lua/options.lua
+++ lua/options.lua
@@ -28,8 +28,8 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
+-- Merge signcolumn and number column
+vim.wo.signcolumn = "number"
 
 -- Decrease update time
 vim.o.updatetime = 250
