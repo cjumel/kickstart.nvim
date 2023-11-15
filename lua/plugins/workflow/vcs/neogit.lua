da1893a9c6385c285537fb4f3e741b700f147a36
clementjumel <clement.jumel@gmail.com>
2023-11-15 10:58:43 +0100

    🐛 Fix wrong blue background color in neogit status buffer

--- lua/plugins/workflow/vcs/neogit.lua
+++ lua/plugins/workflow/vcs/neogit.lua
@@ -55,5 +55,11 @@ return {
 
     -- Disable column ruler in Neogit buffers
     vim.api.nvim_command("autocmd FileType Neogit* setlocal colorcolumn=")
+
+    -- Fix wrong blue background color in Neogit buffer for non-highlighted hunks
+    -- The command `hi NeogitDiffContext` gives `guibg=#b4befe` (blue), whereas the
+    -- command `hi NeogitDiffContextHighlight` gives `guifg=#cdd6f4 guibg=#25283b`
+    -- Let's set the same colors for both commands to remove the blue background color
+    vim.api.nvim_command("hi NeogitDiffContext guifg=#cdd6f4 guibg=#25283b")
   end,
 }
