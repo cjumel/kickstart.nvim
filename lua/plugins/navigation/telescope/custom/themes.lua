cc2c942bf134ce110c1689d9511b53119d341e5b
clementjumel <clement.jumel@gmail.com>
2023-11-16 13:29:36 +0100

    🐛 Disable winblend in telescope as it messes with transparency

--- lua/plugins/navigation/telescope/custom/themes.lua
+++ lua/plugins/navigation/telescope/custom/themes.lua
@@ -4,7 +4,6 @@ M.get_dropdown = function(initial_mode)
   return require("telescope.themes").get_dropdown({
     initial_mode = initial_mode or "insert",
     previewer = false,
-    winblend = 10,
     layout_config = {
       width = 0.7,
     },
