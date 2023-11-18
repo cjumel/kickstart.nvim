fb1004ca54d3cbf31aafbacfaed4f491674b3679
clementjumel <clement.jumel@gmail.com>
2023-11-18 12:50:45 +0100

    🔧 Simplify todo-comments config

--- lua/plugins/edition/todo-comments.lua
+++ lua/plugins/edition/todo-comments.lua
@@ -1,6 +1,6 @@
 -- todo-comments
 --
--- Enable highlighting for TODO, FIXME, BUG, FIXIT, ISSUE & NOTE comments.
+-- Enable highlighting for todo, fixme, bug, fixit, issue & note comments.
 -- This plugin can also be used as a provider for trouble.nvim.
 
 return {
@@ -20,7 +20,7 @@ return {
       function()
         vim.api.nvim_command("normal! o")
         -- The new line is either empty or with a comment prefix, let's remove it, if needed,
-        -- before adding the "TODO"
+        -- before adding the todo-comment
         vim.api.nvim_command("normal! ^DaTODO: ")
         require("Comment.api").comment.linewise.current()
         vim.api.nvim_command("normal! $")
@@ -32,7 +32,7 @@ return {
       function()
         vim.api.nvim_command("normal! O")
         -- The new line is either empty or with a comment prefix, let's remove it, if needed,
-        -- before adding the "TODO"
+        -- before adding the todo-comment
         vim.api.nvim_command("normal! ^DaTODO: ")
         require("Comment.api").comment.linewise.current()
         vim.api.nvim_command("normal! $")
@@ -47,37 +47,22 @@ return {
       desc = "[T]odo: [L]ist",
     },
   },
-  config = function()
-    require("todo-comments").setup({
-      sign_priority = 1,
-      -- keywords recognized as todo comments
-      keywords = {
-        FIX = {
-          icon = " ", -- icon used for the sign, and in search results
-          color = "error", -- can be a hex color, or a named color (see below)
-          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-          -- signs = false, -- configure signs for some keywords individually
-        },
-        TODO = { icon = " ", color = "info" },
-        HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        -- NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
-        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+  opts = {
+    sign_priority = 1, -- Low sign priority to not override other signs
+    search = {
+      args = {
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--hidden",
+        "--glob=!.git/",
       },
-      merge_keywords = false, -- when true, custom keywords will be merged with the defaults
-      search = {
-        args = {
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--hidden",
-          "--glob=!.git/",
-        },
-      },
-    })
+    },
+  },
+  config = function(_, opts)
+    require("todo-comments").setup(opts)
 
     local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
     local tdc = require("todo-comments")
