f2901d1b2c61a5c712fda3ee770b90ec43964053
clementjumel <clement.jumel@gmail.com>
2023-11-21 10:46:21 +0100

    ⚡️ Improve existing snippets

--- lua/plugins/code/luasnip/snippets/lua.lua
+++ lua/plugins/code/luasnip/snippets/lua.lua
@@ -8,18 +8,24 @@ local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
+  s("local", fmt("local {i1} = {i2}", { i1 = i(1, "name"), i2 = i(2, [["var"]]) })),
+  s("require", fmt([[require("{i1}")]], { i1 = i(1, "module") })),
+  s(
+    "function",
+    fmt(
+      [[
+      function {i1}({i2})
+        {i3}
+      end
+      ]],
+      { i1 = i(1), i2 = i(2), i3 = i(3, "-- code") }
+    )
+  ),
   s(
     "todo-comment",
-    fmt("-- {c1}", {
-      c1 = c(1, {
-        t("TODO: "),
-        t("NOTE: "),
-        t("BUG: "),
-        t("FIXME: "),
-        t("ISSUE: "),
-      }),
-    })
+    fmt(
+      "-- {c1}: {i2}",
+      { c1 = c(1, { t("TODO"), t("NOTE"), t("BUG"), t("FIXME"), t("ISSUE") }), i2 = i(2, "text") }
+    )
   ),
-  s("local", fmt("local {i1} = {i2}", { i1 = i(1), i2 = i(2) })),
-  s("local-require", fmt([[local {i1} = require("{i2}")]], { i1 = i(1), i2 = i(2) })),
 }
--- lua/plugins/code/luasnip/snippets/python.lua
+++ lua/plugins/code/luasnip/snippets/python.lua
@@ -1,18 +1,18 @@
 local ls = require("luasnip")
 
 local c = ls.choice_node
+local i = ls.insert_node
 local s = ls.snippet
 local t = ls.text_node
 
+local fmt = require("luasnip.extras.fmt").fmt
+
 return {
-  s("todo-comment", {
-    t("# "),
-    c(1, {
-      t("TODO: "),
-      t("NOTE: "),
-      t("BUG: "),
-      t("FIXME: "),
-      t("ISSUE: "),
-    }),
-  }),
+  s(
+    "todo-comment",
+    fmt(
+      "# {c1}: {i2}",
+      { c1 = c(1, { t("TODO"), t("NOTE"), t("BUG"), t("FIXME"), t("ISSUE") }), i2 = i(2, "text") }
+    )
+  ),
 }
