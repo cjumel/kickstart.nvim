432121fb3d3f340c598e911a48e63e2475a9cd8b
clementjumel <clement.jumel@gmail.com>
2023-11-21 09:40:00 +0100

    ✨ Snippets for lua local variables

--- lua/plugins/code/luasnip/snippets/lua.lua
+++ lua/plugins/code/luasnip/snippets/lua.lua
@@ -1,18 +1,25 @@
 local ls = require("luasnip")
 
 local c = ls.choice_node
+local i = ls.insert_node
 local s = ls.snippet
 local t = ls.text_node
 
+local fmt = require("luasnip.extras.fmt").fmt
+
 return {
-  s("todo-comment", {
-    t("-- "),
-    c(1, {
-      t("TODO: "),
-      t("NOTE: "),
-      t("BUG: "),
-      t("FIXME: "),
-      t("ISSUE: "),
-    }),
-  }),
+  s(
+    "todo-comment",
+    fmt("-- {c1}", {
+      c1 = c(1, {
+        t("TODO: "),
+        t("NOTE: "),
+        t("BUG: "),
+        t("FIXME: "),
+        t("ISSUE: "),
+      }),
+    })
+  ),
+  s("local", fmt("local {i1} = {i2}", { i1 = i(1), i2 = i(2) })),
+  s("local-require", fmt([[local {i1} = require("{i2}")]], { i1 = i(1), i2 = i(2) })),
 }
