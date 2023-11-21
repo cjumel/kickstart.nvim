89ca69b02d34c09987319e3a10f0f7f3d41fec2f
clementjumel <clement.jumel@gmail.com>
2023-11-21 10:48:35 +0100

    ✨ New snippets for Python

--- lua/plugins/code/luasnip/snippets/python.lua
+++ lua/plugins/code/luasnip/snippets/python.lua
@@ -8,6 +8,26 @@ local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
+  s("import", fmt("import {i1}", { i1 = i(1, "module") })),
+  s("import-as", fmt("import {i1} as {i2}", { i1 = i(1, "module"), i2 = i(2, "name") })),
+  s("from-import", fmt("from {i1} import {i2}", { i1 = i(1, "module"), i2 = i(2, "var") })),
+  s(
+    "from-import-as",
+    fmt(
+      "from {i1} import {i2} as {i3}",
+      { i1 = i(1, "module"), i2 = i(2, "var"), i3 = i(3, "name") }
+    )
+  ),
+  s(
+    "def",
+    fmt(
+      [[
+      def {i1}() -> {i2}:
+          {i3}
+      ]],
+      { i1 = i(1, "name"), i2 = i(2, "type"), i3 = i(3, "# code") }
+    )
+  ),
   s(
     "todo-comment",
     fmt(
