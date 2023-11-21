0eea34d82afb2e525dfc28c5a67484fb197f0993
clementjumel <clement.jumel@gmail.com>
2023-11-21 09:21:00 +0100

    ✨ First lua & python snippets

---
+++ lua/plugins/code/luasnip/snippets/lua.lua
@@ -0,0 +1,18 @@
+local ls = require("luasnip")
+
+local c = ls.choice_node
+local s = ls.snippet
+local t = ls.text_node
+
+return {
+  s("todo-comment", {
+    t("-- "),
+    c(1, {
+      t("TODO: "),
+      t("BUG: "),
+      t("FIXME: "),
+      t("ISSUE: "),
+      t("NOTE: "),
+    }),
+  }),
+}
