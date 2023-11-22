4bc33ed3667c57603d9e151218edc052547378b9
clementjumel <clement.jumel@gmail.com>
2023-11-22 15:14:04 +0100

    ✨ Completion snippets for Python loops

--- lua/plugins/code/luasnip/snippets/python.lua
+++ lua/plugins/code/luasnip/snippets/python.lua
@@ -19,6 +19,26 @@ return {
     )
   ),
   s(
+    "for",
+    fmt(
+      [[
+      for {i1} in {i2}:
+          {i3}
+      ]],
+      { i1 = i(1, "var"), i2 = i(2, "iterable"), i3 = i(3, "pass") }
+    )
+  ),
+  s(
+    "for-enumerate",
+    fmt(
+      [[
+      for {i1}, {i2} in enumerate({i3}):
+          {i4}
+      ]],
+      { i1 = i(1, "i"), i2 = i(2, "var"), i3 = i(3, "iterable"), i4 = i(4, "pass") }
+    )
+  ),
+  s(
     "def",
     fmt(
       [[
