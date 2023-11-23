8c5a53e38395a3737151763bdad884fba1bccc87
clementjumel <clement.jumel@gmail.com>
2023-11-23 20:04:49 +0100

    ♻️ Simplify GH copilot config using latest features

--- lua/plugins/code/copilot.lua
+++ lua/plugins/code/copilot.lua
@@ -7,20 +7,6 @@ return {
   "github/copilot.vim",
   event = { "BufNewFile", "BufReadPre" },
   config = function()
-    local AcceptOneWord = function()
-      vim.fn["copilot#Accept"]("")
-      local bar = vim.fn["copilot#TextQueuedForInsertion"]()
-      local word = vim.fn.split(bar, [[[ .]\zs]])[1]
-      -- Escape '<', as it causes trouble when inserted (e.g. "<leader>" is inserted as " ")
-      -- This doesn't work if escaping '>' as well
-      return string.gsub(word, "<", "<lt>")
-    end
-
-    vim.keymap.set(
-      "i",
-      "<s-tab>",
-      AcceptOneWord,
-      { expr = true, remap = false, desc = "copilot#AcceptOneWord()" }
-    )
+    vim.keymap.set("i", "<S-Tab>", "<Plug>(copilot-accept-word)")
   end,
 }
