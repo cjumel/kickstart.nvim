9f76717fe82c32c2e2f91c5bc556e91dc637b279
clementjumel <clement.jumel@gmail.com>
2023-12-06 11:19:15 +0100

    ♻️ Simplify neoscroll config

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -7,38 +7,27 @@ return {
   keys = {
     {
       "<C-u>",
-      function()
-        require("neoscroll").scroll(-vim.wo.scroll, true, 250)
-      end,
       desc = "Scroll up half of a window height",
     },
     {
       "<C-d>",
-      function()
-        require("neoscroll").scroll(vim.wo.scroll, true, 250)
-      end,
       desc = "Scroll down half of a window height",
     },
     {
       "<C-b>",
-      function()
-        require("neoscroll").scroll(-vim.api.nvim_win_get_height(0), true, 450)
-      end,
       desc = "Scroll up a window height",
     },
     {
       "<C-f>",
-      function()
-        require("neoscroll").scroll(vim.api.nvim_win_get_height(0), true, 450)
-      end,
       desc = "Scroll down a window height",
     },
     {
       "<C-y>",
-      function()
-        require("neoscroll").scroll(-0.10, false, 100)
-      end,
       desc = "Scroll up 10% of a window height",
+    },
+    {
+      "<C-e>",
+      desc = "Scroll down 10% of a window height",
     },
     {
       "<BS>",
@@ -46,13 +35,6 @@ return {
         require("neoscroll").scroll(-0.10, false, 100)
       end,
       desc = "Scroll up 10% of a window height",
-    },
-    {
-      "<C-e>",
-      function()
-        require("neoscroll").scroll(0.10, false, 100)
-      end,
-      desc = "Scroll down 10% of a window height",
     },
     {
       "<CR>",
@@ -63,6 +45,6 @@ return {
     },
   },
   opts = {
-    mappings = {}, -- Disable default mappings as mappings are defined in keys
+    mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>" },
   },
 }
