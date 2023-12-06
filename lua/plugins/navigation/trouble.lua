b1f30a3a5858602c0b7f008bcc70fc931db519ec
clementjumel <clement.jumel@gmail.com>
2023-12-06 12:05:20 +0100

    ♻️ Change Trouble keymap prefix to t

--- lua/plugins/navigation/trouble.lua
+++ lua/plugins/navigation/trouble.lua
@@ -3,96 +3,59 @@
 -- Trouble provides a pretty list for showing diagnostics, references, telescope results,
 -- quickfix and location lists to help you solve all the trouble your code is causing.
 
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+local next_trouble_item, previous_trouble_item = ts_repeat_move.make_repeatable_move_pair(function()
+  require("trouble").next({ skip_groups = true, jump = true })
+end, function()
+  require("trouble").previous({ skip_groups = true, jump = true })
+end)
+
 return {
   "folke/trouble.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    "nvim-treesitter/nvim-treesitter-textobjects",
     -- The following dependencies are needed but don't need to be loaded when the plugin is loaded
     -- "folke/todo-comments.nvim",
   },
   keys = {
     {
-      "<leader>xx",
+      "<leader>tt",
       function()
         require("trouble").toggle()
       end,
-      desc = "Trouble: toggle Trouble",
+      desc = "[T]rouble: [T]oggle",
     },
     {
-      "<leader>xq",
-      function()
-        require("trouble").toggle("quickfix")
-      end,
-      desc = "Trouble: toggle [Q]uickfix",
-    },
-    {
-      "<leader>xl",
-      function()
-        require("trouble").toggle("loclist")
-      end,
-      desc = "Trouble: toggle [L]oclist",
-    },
-    {
-      "<leader>xd",
+      "<leader>td",
       function()
         require("trouble").toggle("document_diagnostics")
       end,
-      desc = "Trouble: [D]ocument diagnostics",
+      desc = "[T]rouble: [D]iagnostics (document)",
     },
     {
-      "<leader>xw",
+      "<leader>tD",
       function()
         require("trouble").toggle("workspace_diagnostics")
       end,
-      desc = "Trouble: [W]orkspace diagnostics",
+      desc = "[T]rouble: [D]iagnostics (workspace)",
     },
     {
-      "<leader>xt",
+      "<leader>tl",
       function()
         require("todo-comments") -- Lazy load todo-comments which adds a "todo" source to trouble
         require("trouble").toggle("todo")
       end,
-      desc = "Trouble: [T]odo-list",
+      desc = "[T]rouble: todo [L]ist",
     },
     {
-      "[x",
-      function()
-        vim.cmd("TroubleNext")
-      end,
+      "[t",
+      next_trouble_item,
       desc = "Next Trouble item",
     },
     {
-      "]x",
-      function()
-        vim.cmd("TroublePrevious")
-      end,
+      "]t",
+      previous_trouble_item,
       desc = "Previous Trouble item",
     },
   },
-  config = function()
-    require("trouble").setup()
-
-    local next_trouble_item = function()
-      require("trouble").next({ skip_groups = true, jump = true })
-    end
-    local previous_trouble_item = function()
-      require("trouble").previous({ skip_groups = true, jump = true })
-    end
-
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-    local next_trouble_item_repeat, previous_trouble_item_repeat =
-      ts_repeat_move.make_repeatable_move_pair(next_trouble_item, previous_trouble_item)
-
-    vim.api.nvim_create_user_command(
-      "TroubleNext",
-      next_trouble_item_repeat,
-      { desc = "Next Trouble item" }
-    )
-    vim.api.nvim_create_user_command(
-      "TroublePrevious",
-      previous_trouble_item_repeat,
-      { desc = "Previous Trouble item" }
-    )
-  end,
 }
