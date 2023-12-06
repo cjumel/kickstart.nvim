6d777030122fa137f1400145c8c651797c5e38dd
clementjumel <clement.jumel@gmail.com>
2023-12-06 11:28:05 +0100

    🔥 Remove next/prev todo comment keymaps

--- lua/plugins/edition/todo-comments.lua
+++ lua/plugins/edition/todo-comments.lua
@@ -8,7 +8,6 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
-    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   event = { "BufNewFile", "BufReadPre" },
   opts = {
@@ -31,25 +30,4 @@ return {
       },
     },
   },
-  config = function(_, opts)
-    require("todo-comments").setup(opts)
-
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-    local tdc = require("todo-comments")
-    -- make sure forward function comes first
-    local next_todo_comment_repeat, prev_todo_comment_repeat =
-      ts_repeat_move.make_repeatable_move_pair(tdc.jump_next, tdc.jump_prev)
-    vim.keymap.set(
-      { "n", "x", "o" },
-      "[t",
-      next_todo_comment_repeat,
-      { desc = "Next todo comment" }
-    )
-    vim.keymap.set(
-      { "n", "x", "o" },
-      "]t",
-      prev_todo_comment_repeat,
-      { desc = "Previous todo comment" }
-    )
-  end,
 }
