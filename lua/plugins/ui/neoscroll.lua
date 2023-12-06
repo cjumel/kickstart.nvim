6dca3627994614f0ba1a6d13ea2b47d1b64a2cfc
clementjumel <clement.jumel@gmail.com>
2023-12-06 10:52:31 +0100

    ✨ Keymaps for <CR> & <BS> in normal mode

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -6,5 +6,21 @@ return {
   "karb94/neoscroll.nvim",
   -- Can't be loaded on `event = { "BufNewFile", "BufReadPre" }` to work with plugins like Neogit
   event = "VeryLazy",
+  keys = {
+    {
+      "<BS>",
+      function()
+        require("neoscroll").scroll(-0.10, false, 100)
+      end,
+      desc = "Scroll up 10% of a window height",
+    },
+    {
+      "<CR>",
+      function()
+        require("neoscroll").scroll(0.10, false, 100)
+      end,
+      desc = "Scroll down 10% of a window height",
+    },
+  },
   opts = {},
 }
