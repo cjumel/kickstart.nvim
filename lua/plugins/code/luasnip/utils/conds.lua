2ee97c2488166d2c57ad1952c2ed06bb6072929a
clementjumel <clement.jumel@gmail.com>
2023-12-21 11:10:59 +0100

    ♻️ Refacto luasnip custom conditions

--- lua/plugins/code/luasnip/utils/conds.lua
+++ lua/plugins/code/luasnip/utils/conds.lua
@@ -1,11 +1,18 @@
+-- These functions must be used with the `condition` option in snippets, they are not suited to
+-- the `show_condition` as they don't support the right parameters for it.
+
 local M = {}
 
--- Condition determining wether a snippet is in actual code or not, using treesitter.
-function M.is_in_code(line_to_cursor, matched_trigger, captures)
+local function get_treesitter_node(matched_trigger)
   local _, row, col, _, _ = unpack(vim.fn.getcurpos())
-  local node = vim.treesitter.get_node({
+  return vim.treesitter.get_node({
     pos = { row - 1, col - #matched_trigger - 1 },
   })
+end
+
+-- Condition determining wether a snippet is in actual code or not, using treesitter.
+function M.is_in_code(_, matched_trigger, _)
+  local node = get_treesitter_node(matched_trigger)
   return node
     and not (
       node:type() == "comment"
@@ -16,11 +23,8 @@ function M.is_in_code(line_to_cursor, matched_trigger, captures)
 end
 
 -- Condition determining wether a snippet is in a comment or not, using treesitter.
-function M.is_in_comment(line_to_cursor, matched_trigger, captures)
-  local _, row, col, _, _ = unpack(vim.fn.getcurpos())
-  local node = vim.treesitter.get_node({
-    pos = { row - 1, col - #matched_trigger - 1 },
-  })
+function M.is_in_comment(_, matched_trigger, _)
+  local node = get_treesitter_node(matched_trigger)
   return node and (node:type() == "comment" or node:type() == "comment_content")
 end
 
