f770258750e379f634cce735a2f442ee527d995b
clementjumel <clement.jumel@gmail.com>
2024-01-04 00:07:50 +0100

    🩹 Open preview on the right in Oil

--- lua/plugins/navigation/oil.lua
+++ lua/plugins/navigation/oil.lua
@@ -5,6 +5,29 @@
 
 local custom_actions = {}
 
+-- Overwrite the default preview action to open the preview window on the right hand side
+custom_actions.preview = {
+  desc = "Open the entry under the cursor in a preview window, or close the preview window if already open",
+  callback = function()
+    local oil = require("oil")
+    local util = require("oil.util")
+    local entry = oil.get_cursor_entry()
+    if not entry then
+      vim.notify("Could not find entry under cursor", vim.log.levels.ERROR)
+      return
+    end
+    local winid = util.get_preview_win()
+    if winid then
+      local cur_id = vim.w[winid].oil_entry_id
+      if entry.id == cur_id then
+        vim.api.nvim_win_close(winid, true)
+        return
+      end
+    end
+    oil.select({ preview = true, split = "belowright" }) -- Open on the right hand side
+  end,
+}
+
 -- Overwrite Harpoon keymap to add the file under the cursor in Oil buffer instead
 -- of Oil buffer itself
 custom_actions.add_harpoon_mark = {
@@ -70,7 +93,7 @@ return {
     cleanup_delay_ms = 0,
     keymaps = {
       ["<CR>"] = "actions.select",
-      ["<tab>"] = "actions.preview",
+      ["<tab>"] = custom_actions.preview,
       ["<C-v>"] = "actions.select_vsplit",
       ["<C-x>"] = "actions.select_split",
       ["q"] = "actions.close",
