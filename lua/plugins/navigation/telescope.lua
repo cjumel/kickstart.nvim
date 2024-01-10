967334d5f45f65190a1b8c6ffe602da3f468b865
clementjumel <clement.jumel@gmail.com>
2024-01-10 14:44:19 +0100

    🚚 Re-organize Telescope plugins

--- lua/plugins/navigation/telescope.lua
+++ lua/plugins/navigation/telescope.lua
@@ -59,8 +59,17 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
-    "nvim-telescope/telescope-fzf-native.nvim",
     "nvim-treesitter/nvim-treesitter",
+    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+    -- Only load if `make` is available. Make sure you have the system
+    -- requirements installed.
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      cond = function()
+        return vim.fn.executable("make") == 1
+      end,
+    },
   },
   keys = {
     -- General
