26b9c3c2515007eebe05908f65f7dce0bfb46121
clementjumel <clement.jumel@gmail.com>
2024-01-10 11:58:32 +0100

    ⚰️ Remove unused get_commands_dropdown Telescope function

--- lua/plugins/navigation/telescope/utils/themes.lua
+++ lua/plugins/navigation/telescope/utils/themes.lua
@@ -10,40 +10,4 @@ M.get_dropdown = function(initial_mode)
   })
 end
 
-M.get_commands_dropdown = function()
-  local opts = M.get_dropdown()
-
-  -- Simplify the display of commands with only name & description
-  local displayer = require("telescope.pickers.entry_display").create({
-    separator = "▏",
-    items = {
-      { width = 0.33 },
-      { remaining = true },
-    },
-  })
-  local make_display = function(entry)
-    return displayer({
-      { entry.name, "TelescopeResultsIdentifier" },
-      entry.definition,
-    })
-  end
-  local entry_maker = function(entry)
-    return {
-      name = entry.name,
-      bang = entry.bang,
-      nargs = entry.nargs,
-      complete = entry.complete,
-      definition = entry.definition,
-      --
-      value = entry,
-      valid = true,
-      ordinal = entry.name,
-      display = make_display,
-    }
-  end
-  opts.entry_maker = entry_maker
-
-  return opts
-end
-
 return M
