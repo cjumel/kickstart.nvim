d444d3b586642271d1caa61f6a85f07a1dbec560
clementjumel <clement.jumel@gmail.com>
2024-01-14 21:29:21 +0100

    ✨ Add Oil buffer case in Overseer templates paths

--- lua/plugins/external_tools/overseer/arguments.lua
+++ lua/plugins/external_tools/overseer/arguments.lua
@@ -3,19 +3,25 @@ local M = {}
 local mods = ":p:~:."
 
 M.get_path = function(params)
-  -- In priority output the command input
-  if params.args ~= "" then
+  if params.args ~= "" then -- An argument was passed
     return vim.fn.fnamemodify(params.args, mods)
   end
 
-  -- If a buffer is open, output its path
-  local file_path = vim.fn.expand("%:p") -- Current file or ""
-  if file_path ~= "" then
-    return vim.fn.fnamemodify(file_path, mods)
+  local file_path = vim.fn.expand("%:p")
+
+  if file_path == "" then -- No file open
+    return "."
   end
 
-  -- Default to the current directory
-  return "."
+  if string.sub(file_path, 1, 4) == "oil:" then -- Oil buffer open
+    local ok, oil = pcall(require, "oil")
+    if ok then
+      return vim.fn.fnamemodify(oil.get_current_dir(), mods)
+    end
+  end
+
+  -- Regular file open
+  return vim.fn.fnamemodify(file_path, mods)
 end
 
 return M
