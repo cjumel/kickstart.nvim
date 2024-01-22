04d1ba63be273b7d270be70817579dfe7c040868
clementjumel <clement.jumel@gmail.com>
2024-01-22 11:45:18 +0100

    🐛 Fix Neoscroll making Neovim crash

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -6,51 +6,43 @@ return {
   "karb94/neoscroll.nvim",
   -- Use neoscroll's default mappings as lazy keys; using a "BufNewFile" or "BufReadPre" lazy
   -- event doesn't work well for some buffers like with Neogit
+  -- For some reason, adding `ft = "*"` in the lazy keys causes Neoscroll to make Neovim crash
   keys = {
     {
       "<C-u>",
       desc = "Scroll up half of a window height",
-      ft = "*",
     },
     {
       "<C-d>",
       desc = "Scroll down half of a window height",
-      ft = "*",
     },
     {
       "<C-b>",
       desc = "Scroll up a window height",
-      ft = "*",
     },
     {
       "<C-f>",
       desc = "Scroll down a window height",
-      ft = "*",
     },
     {
       "<C-y>",
       desc = "Scroll up 10% of a window height",
-      ft = "*",
     },
     {
       "<C-e>",
       desc = "Scroll down 10% of a window height",
-      ft = "*",
     },
     {
       "zt",
       desc = "Top this line",
-      ft = "*",
     },
     {
       "zz",
       desc = "Center this line",
-      ft = "*",
     },
     {
       "zb",
       desc = "Bottom this line",
-      ft = "*",
     },
   },
   opts = {},
