0268be7b758313c16d4a78d43c7268c3cc52089e
clementjumel <clement.jumel@gmail.com>
2024-01-30 18:03:03 +0100

    🐛 Fix OverseerPythonFile command

--- lua/plugins/tools/overseer/templates/python.lua
+++ lua/plugins/tools/overseer/templates/python.lua
@@ -8,17 +8,20 @@ return {
     builder = function(params)
       local arguments_path = arguments.get_path(params, { mode = "file" })
 
-      if vim.fn.filereadable(arguments_path) == 1 and vim.bo.filetype == "python" then
-        return {
-          cmd = { "python", arguments_path },
-        }
-      elseif arguments_path == nil then
+      if arguments_path == nil then
         print("No path was provided")
         return {}
-      else
-        print("Not a readable Python file: " .. arguments_path)
+      elseif vim.fn.filereadable(arguments_path) ~= 1 then
+        print("Not a readable file: " .. arguments_path)
+        return {}
+      elseif string.sub(arguments_path, -3) ~= ".py" then
+        print("Not a Python file: " .. arguments_path)
         return {}
       end
+
+      return {
+        cmd = { "python", arguments_path },
+      }
     end,
     desc = "Run a Python file passed as argument or the current buffer",
     tags = tags,
