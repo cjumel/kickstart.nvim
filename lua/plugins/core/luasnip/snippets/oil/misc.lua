eeb009436388f4c01b687bbb76d3cbd479907780
clementjumel <clement.jumel@gmail.com>
2024-02-05 15:43:17 +0100

    ✨ Some more snippets for Oil buffers

--- lua/plugins/core/luasnip/snippets/oil/misc.lua
+++ lua/plugins/core/luasnip/snippets/oil/misc.lua
@@ -4,8 +4,11 @@ local s = ls.snippet
 local t = ls.text_node
 
 return {
-  -- Scratch files
+  -- Lua
+  s("init.lua", { t("init.lua") }),
   s("scratch.lua", { t("scratch.lua") }),
+  -- Python
+  s("__init__.py", { t("__init__.py") }),
   s("scratch.py", { t("scratch.py") }),
   -- Norg
   s("notes.norg", { t("notes.norg") }),
