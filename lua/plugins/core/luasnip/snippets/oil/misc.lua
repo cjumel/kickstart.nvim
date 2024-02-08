921cfb9bcbea22cc470422e3c8c640360f5d38ea
clementjumel <clement.jumel@gmail.com>
2024-02-08 12:16:37 +0100

    ✨ Add conditions on Oil buffer snippets

--- lua/plugins/core/luasnip/snippets/oil/misc.lua
+++ lua/plugins/core/luasnip/snippets/oil/misc.lua
@@ -3,14 +3,35 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 
+local show_conds = require("luasnip.extras.conditions.show")
+local custom_show_conds = require("plugins.core.luasnip.show_conds")
+
 return {
   -- Lua
-  s("init.lua", { t("init.lua") }),
-  s("scratch.lua", { t("scratch.lua") }),
+  s({
+    trig = "init.lua",
+    show_condition = custom_show_conds.line_begin * show_conds.line_end,
+  }, { t("init.lua") }),
+  s({
+    trig = "scratch.lua",
+    show_condition = custom_show_conds.line_begin * show_conds.line_end,
+  }, { t("scratch.lua") }),
   -- Python
-  s("__init__.py", { t("__init__.py") }),
-  s("scratch.py", { t("scratch.py") }),
+  s({
+    trig = "__init__.py",
+    show_condition = custom_show_conds.line_begin * show_conds.line_end,
+  }, { t("__init__.py") }),
+  s({
+    trig = "scratch.py",
+    show_condition = custom_show_conds.line_begin * show_conds.line_end,
+  }, { t("scratch.py") }),
   -- Norg
-  s("notes.norg", { t("notes.norg") }),
-  s("todo.norg", { t("todo.norg") }),
+  s({
+    trig = "notes.norg",
+    show_condition = custom_show_conds.line_begin * show_conds.line_end,
+  }, { t("notes.norg") }),
+  s({
+    trig = "todo.norg",
+    show_condition = custom_show_conds.line_begin * show_conds.line_end,
+  }, { t("todo.norg") }),
 }
