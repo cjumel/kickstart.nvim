fb0eae2dafdb03415697ad120a63f7aedb0bc109
clementjumel <clement.jumel@gmail.com>
2024-02-09 13:03:44 +0100

    ⏪️ Make Noice mandatory again

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -3,22 +3,8 @@
 -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the
 -- popupmenu.
 
--- Handle the case the theme file is missing
-local ok, theme = pcall(require, "theme")
-if not ok then
-  theme = {}
-end
-
--- By default we want to enable Noice but we can't use `theme.noice_enabled or true` (always true)
-local noice_enabled = true
-if theme.noice_enabled == false then
-  noice_enabled = false
-end
-
 return {
   "folke/noice.nvim",
-  -- If plugin is not enabled, just make it lazy to avoid changing the lazy lock file
-  lazy = not noice_enabled,
   dependencies = {
     "MunifTanjim/nui.nvim",
     {
