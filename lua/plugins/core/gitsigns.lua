4f8d72df4d9d0d829602da887e014d0d0e9872a7
clementjumel <clement.jumel@gmail.com>
2024-02-18 18:59:08 +0100

    🩹 Simplify gitsigns keymaps

--- lua/plugins/core/gitsigns.lua
+++ lua/plugins/core/gitsigns.lua
@@ -41,25 +41,25 @@ return {
         ts_repeat_move.make_repeatable_move_pair(gs.next_hunk, gs.prev_hunk)
       local next_conflict_repeat, prev_conflict_repeat =
         ts_repeat_move.make_repeatable_move_pair(next_conflict, prev_conflict)
-      map({ "n", "x", "o" }, "[h", next_hunk_repeat, { desc = "Next Git hunk" })
-      map({ "n", "x", "o" }, "]h", prev_hunk_repeat, { desc = "Previous Git hunk" })
-      map({ "n", "x", "o" }, "[H", next_conflict_repeat, { desc = "Next Git conflict hunk" })
-      map({ "n", "x", "o" }, "]H", prev_conflict_repeat, { desc = "Previous Git conflict hunk" })
+      map({ "n", "x", "o" }, "[h", next_hunk_repeat, { desc = "Next hunk" })
+      map({ "n", "x", "o" }, "]h", prev_hunk_repeat, { desc = "Previous hunk" })
+      map({ "n", "x", "o" }, "[H", next_conflict_repeat, { desc = "Next conflict hunk" })
+      map({ "n", "x", "o" }, "]H", prev_conflict_repeat, { desc = "Previous conflict hunk" })
 
       -- Action
-      map({ "n", "v" }, "<leader><TAB>", gs.preview_hunk, { desc = "Expand Git hunk" })
+      map({ "n", "v" }, "<leader>h", gs.preview_hunk, { desc = "[H]unk" })
 
-      map("n", "<leader>a", gs.stage_hunk, { desc = "[A]dd Git hunk" })
+      map("n", "<leader>a", gs.stage_hunk, { desc = "[A]dd hunk" })
       map("v", "<leader>a", function()
         gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, { desc = "[A]dd Git lines" })
-      map("n", "<leader>A", gs.undo_stage_hunk, { desc = "[A]dd-Git-hunk undo" })
+      end, { desc = "[A]dd lines" })
+      map("n", "<leader>A", gs.undo_stage_hunk, { desc = "[A]dd-hunk undo" })
       map("n", "<leader>ga", gs.stage_buffer, { desc = "[G]it: [A]dd buffer" })
 
-      map("n", "<leader>r", gs.reset_hunk, { desc = "[R]eset Git hunk" })
+      map("n", "<leader>r", gs.reset_hunk, { desc = "[R]eset hunk" })
       map("v", "<leader>r", function()
         gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, { desc = "[R]eset Git lines" })
+      end, { desc = "[R]eset lines" })
       map("n", "<leader>gr", gs.reset_buffer, { desc = "[G]it: [R]eset buffer" })
 
       map("n", "<leader>gd", function()
@@ -71,8 +71,8 @@ return {
       end, { desc = "[G]it: [B]lame" })
 
       -- Text objects
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "inner Git hunk" })
-      map({ "o", "x" }, "ah", ":<C-U>Gitsigns select_hunk<CR>", { desc = "a Git hunk" })
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "inner hunk" })
+      map({ "o", "x" }, "ah", ":<C-U>Gitsigns select_hunk<CR>", { desc = "a hunk" })
     end,
   },
 }
