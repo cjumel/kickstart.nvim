e474068454f9b68b04999b6c26f0bb570f2a5273
clementjumel <clement.jumel@gmail.com>
2024-02-21 14:07:23 +0100

    🩹 Make Neoscroll lazy keys also in visual mode

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -7,15 +7,15 @@ return {
   -- Using `event = { "BufNewFile", "BufReadPre" }` makes Neoscroll not activated in some buffers
   -- like Neogit's status
   keys = {
-    { "<C-u>", desc = "Scroll up half of a window height" },
-    { "<C-d>", desc = "Scroll down half of a window height" },
-    { "<C-b>", desc = "Scroll up a window height" },
-    { "<C-f>", desc = "Scroll down a window height" },
-    { "<C-y>", desc = "Scroll up 10% of a window height" },
-    { "<C-e>", desc = "Scroll down 10% of a window height" },
-    { "zt", desc = "Top this line" },
-    { "zz", desc = "Center this line" },
-    { "zb", desc = "Bottom this line" },
+    { "<C-u>", mode = { "n", "v" }, desc = "Scroll up half of a window height" },
+    { "<C-d>", mode = { "n", "v" }, desc = "Scroll down half of a window height" },
+    { "<C-b>", mode = { "n", "v" }, desc = "Scroll up a window height" },
+    { "<C-f>", mode = { "n", "v" }, desc = "Scroll down a window height" },
+    { "<C-y>", mode = { "n", "v" }, desc = "Scroll up 10% of a window height" },
+    { "<C-e>", mode = { "n", "v" }, desc = "Scroll down 10% of a window height" },
+    { "zt", mode = { "n", "v" }, desc = "Top this line" },
+    { "zz", mode = { "n", "v" }, desc = "Center this line" },
+    { "zb", mode = { "n", "v" }, desc = "Bottom this line" },
   },
   opts = {},
 }
