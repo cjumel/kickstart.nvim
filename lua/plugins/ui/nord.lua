7d1ca7f382c7e56d159b7bb186a4c2f372ba38d9
clementjumel <clement.jumel@gmail.com>
2024-02-25 16:48:47 +0100

    🐛 Fix Nord theme hightligh groups for Neogit
    Neogit was not usable with Nord theme before that, this fixes it.

--- lua/plugins/ui/nord.lua
+++ lua/plugins/ui/nord.lua
@@ -16,5 +16,17 @@ return {
   config = function()
     vim.g.nord_disable_background = true -- Enable transparent background
     vim.cmd.colorscheme("nord")
+
+    -- Fix unreadable Neogit highlight groups with this color scheme
+    -- Default highlight groups with this color scheme are:
+    --   NeogitDiffAdd guifg=#a3be8c guibg=#a3be8c
+    --   NeogitDiffAddHighlight gui=reverse guifg=#a3be8c
+    --   NeogitDiffDelete guifg=#bf616a
+    --   NeogitDiffDeleteHighlight gui=reverse guifg=#bf616a
+    -- Let's use the same colors with a black foreground
+    vim.api.nvim_command("hi NeogitDiffAdd guifg=#000000 guibg=#a3be8c")
+    vim.api.nvim_command("hi NeogitDiffAddHighlight guibg=#000000 guifg=#a3be8c") -- reversed
+    vim.api.nvim_command("hi NeogitDiffDelete guifg=#000000 guibg=#bf616a")
+    vim.api.nvim_command("hi NeogitDiffDeleteHighlight guibg=#000000 guifg=#bf616a") -- reversed
   end,
 }
