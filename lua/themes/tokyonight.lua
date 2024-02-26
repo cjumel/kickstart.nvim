c0da9dcfd92f862e218e0b1e214a2b8222b8874b
clementjumel <clement.jumel@gmail.com>
2024-02-26 15:43:25 +0100

    🩹 Adapt tokyonight theme to new tmux theme

--- lua/themes/tokyonight.lua
+++ lua/themes/tokyonight.lua
@@ -6,7 +6,7 @@ M.lualine_opts = {
   options = {
     theme = "tokyonight",
     component_separators = "",
-    section_separators = { left = "", right = "" },
+    section_separators = "",
     refresh = {
       statusline = 50, -- Decrease refresh rate to make modules more responsive (e.g. Harpoon's)
     },
