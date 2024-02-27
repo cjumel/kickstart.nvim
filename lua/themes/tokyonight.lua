6a90c3ae2cd8b7ed5056aff9f4063dd49205838e
clementjumel <clement.jumel@gmail.com>
2024-02-27 10:10:58 +0100

    ⚡️ Simplify tokyonight theme lualine

--- lua/themes/tokyonight.lua
+++ lua/themes/tokyonight.lua
@@ -1,3 +1,8 @@
+local utils = require("utils")
+
+local modules = require("plugins.ui.lualine.modules")
+local sections = require("plugins.ui.lualine.sections")
+
 local M = {}
 
 M.tokyonight_enabled = true
@@ -9,6 +14,48 @@ M.lualine_opts = {
     section_separators = "",
     refresh = {
       statusline = 50, -- Decrease refresh rate to make modules more responsive (e.g. Harpoon's)
+    },
+  },
+  sections = utils.table.concat_dicts({
+    sections.empty,
+    {
+      lualine_c = {
+        {
+          "filename",
+          path = 1, -- Relative path
+        },
+        "diff",
+        "diagnostics",
+      },
+      lualine_x = utils.table.concat_arrays({
+        { modules.macro, modules.harpoon },
+        { "location", "progress" },
+      }),
+    },
+  }),
+  extensions = {
+    -- Redefine some extensions to customize them (see lualine/extensions/ for the initial
+    -- implementations)
+    {
+      sections = utils.table.concat_dicts({
+        sections.empty,
+        { lualine_c = modules.oil },
+      }),
+      filetypes = { "oil" },
+    },
+    {
+      sections = utils.table.concat_dicts({
+        sections.empty,
+        { lualine_c = modules.trouble },
+      }),
+      filetypes = { "Trouble" },
+    },
+    {
+      sections = utils.table.concat_dicts({
+        sections.empty,
+        { lualine_c = modules.toggleterm },
+      }),
+      filetypes = { "toggleterm" },
     },
   },
 }
