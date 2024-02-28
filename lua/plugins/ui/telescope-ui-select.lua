9c4a9006a13b7237661a5811a67fb18d71255fd9
clementjumel <clement.jumel@gmail.com>
2024-02-28 19:02:33 +0100

    ✨ Add telescope-ui-select plugin

---
+++ lua/plugins/ui/telescope-ui-select.lua
@@ -0,0 +1,25 @@
+-- telescope-ui-select.nvim
+--
+-- Sets vim.ui.select to use Telescope, meaning that any Neovim function (even core ones) can use
+-- the Telescope picker.
+
+return {
+  "nvim-telescope/telescope-ui-select.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  event = "VeryLazy",
+  config = function()
+    local telescope = require("telescope")
+
+    telescope.setup({
+      extensions = {
+        ["ui-select"] = {
+          layout_strategy = "vertical",
+        },
+      },
+    })
+
+    telescope.load_extension("ui-select")
+  end,
+}
