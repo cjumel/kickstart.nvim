b27a017223eae7070ea8aa662184117c02904436
clementjumel <clement.jumel@gmail.com>
2024-03-02 20:31:34 +0100

    ⚡️ Improve treesitter-context usability

--- lua/plugins/core/nvim-treesitter-context.lua
+++ lua/plugins/core/nvim-treesitter-context.lua
@@ -7,7 +7,7 @@ return {
   lazy = true,
   opts = {
     enable = false, -- Keep the plugin disabled by default and only enable it via keymaps
-    mode = "topline",
+    separator = "-", -- Add a dash-line between the context and the buffer contents
   },
   config = function(_, opts)
     local treesitter_context = require("treesitter-context")
@@ -18,6 +18,19 @@ return {
       "<leader>,c",
       treesitter_context.toggle,
       { desc = "Settings: toggle [C]ontext" }
+    )
+    vim.keymap.set(
+      "n",
+      "gP", -- gp is used to go to treesitter parent node
+      function()
+        if not treesitter_context.enabled() then
+          return
+        end
+
+        treesitter_context.go_to_context(vim.v.count1)
+        vim.cmd("normal! zz") -- make sure remaining context can be displayed
+      end,
+      { desc = "Go to context parent node", silent = true }
     )
   end,
 }
