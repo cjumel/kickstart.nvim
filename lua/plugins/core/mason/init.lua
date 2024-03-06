7d9889a4f0be7fc906d9e4edf35567a61956dd56
clementjumel <clement.jumel@gmail.com>
2024-03-06 21:34:56 +0100

    🔥 Clean Mason config

--- lua/plugins/core/mason/init.lua
+++ lua/plugins/core/mason/init.lua
@@ -1,34 +1,20 @@
 -- mason.nvim
 --
 -- Mason.nvim is a portable package manager for Neovim written in Lua.
--- In this implementation, packages are installed in two ways: packages defined in
--- `ensure_installed` are installed by running `:MasonInstallAll`, and packages defined in
--- the LSP configuration are automatically installed when the LSP server is initialized.
 
 return {
   "williamboman/mason.nvim",
   cmd = {
     "Mason",
+    "MasonUpdate",
     "MasonInstall",
-    "MasonInstallAll", -- Custom command to install all Mason packages (taken from NvChad)
+    "MasonUninstall",
+    "MasonUninstallAll",
+    "MasonLog",
   },
   opts = {
     ui = {
-      border = "rounded", -- Adding a border is lot better for transparent background
-    },
-    ensure_installed = {
-      -- Python
-      "ruff", -- Formatter & linter
+      border = "rounded", -- A border is better for transparent backgrounds
     },
   },
-  config = function(_, opts)
-    require("mason").setup(opts)
-
-    -- Custom command to install all Mason packages (taken from NvChad)
-    vim.api.nvim_create_user_command("MasonInstallAll", function()
-      vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
-    end, { desc = "Install all Mason packages." })
-
-    vim.g.mason_binaries_list = opts.ensure_installed
-  end,
 }
