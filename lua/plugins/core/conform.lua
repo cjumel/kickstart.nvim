c499c359fe6f714385f65e8eb12c46ee5b5b9100
clementjumel <clement.jumel@gmail.com>
2024-03-09 15:54:22 +0100

    ♻️ Dynamically resolve plugin options in conform & lint

--- lua/plugins/core/conform.lua
+++ lua/plugins/core/conform.lua
@@ -2,12 +2,41 @@
 --
 -- Lightweight yet powerful formatter plugin for Neovim
 
+-- Define here which formatter to use for each file type
+-- Keys must be simple file types, and values arrays of formatters
+local formatters_by_ft = {
+  gitignore = { "trim_newlines", "trim_whitespace" },
+  json = { "prettier" },
+  lua = { "stylua" },
+  markdown = { "prettier" },
+  norg = { "trim_newlines", "trim_whitespace" },
+  python = {
+    "ruff_fix", -- Fix lint diagnostics
+    "ruff_format", -- Regular formatting
+  },
+  text = { "trim_newlines", "trim_whitespace" },
+  yaml = { "prettier" },
+  zsh = { "trim_newlines", "trim_whitespace" },
+}
+
+-- Specify the formatters which have no Mason package
+local formatters_without_mason_package = {
+  "trim_newlines",
+  "trim_whitespace",
+}
+
+-- Specify the name of the Mason package for formatters where they differ
+local formatter_to_mason_name = {
+  ruff_fix = "ruff",
+  ruff_format = "ruff",
+}
+
 return {
   "stevearc/conform.nvim",
   dependencies = {
     "williamboman/mason.nvim",
   },
-  event = { "BufNewFile", "BufReadPre" },
+  ft = vim.tbl_keys(formatters_by_ft),
   keys = {
     {
       "<leader>,f",
@@ -23,44 +52,42 @@ return {
       desc = "Settings: toggle [F]ormat on save",
     },
   },
-  opts = {
-    mason_ensure_installed = { -- Custom option to automatically install missing Mason packages
-      "prettier",
-      "ruff",
-      "stylua",
-    },
-    formatters_by_ft = {
-      json = { "prettier" },
-      lua = { "stylua" },
-      markdown = { "prettier" },
-      python = {
-        "ruff_fix", -- Fix lint diagnostics
-        "ruff_format", -- Regular formatting
-      },
-      yaml = { "prettier" },
-      ["_"] = { -- Files with no other formatter configured
-        "trim_newlines",
-        "trim_whitespace",
-      },
-    },
-    format_on_save = function(bufnr)
-      -- Disable autoformat on certain filetypes
-      local ignore_filetypes = { "oil" }
-      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-        return
+  opts = function()
+    local mason_ensure_installed = {}
+    for _, formatters in pairs(formatters_by_ft) do
+      for _, formatter in ipairs(formatters) do
+        if not vim.tbl_contains(formatters_without_mason_package, formatter) then
+          local mason_name = formatter_to_mason_name[formatter] or formatter
+          if not vim.tbl_contains(mason_ensure_installed, mason_name) then
+            table.insert(mason_ensure_installed, mason_name)
+          end
+        end
       end
+    end
 
-      -- Disable with a global variable
-      if vim.g.disable_autoformat then
-        return
-      end
+    return {
+      -- Custom option to automatically install missing Mason packages
+      mason_ensure_installed = mason_ensure_installed,
+      formatters_by_ft = formatters_by_ft,
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = { "oil" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
 
-      return {
-        lsp_fallback = true, -- Some formatters (like taplo) are setup with lspconfig
-        timeout_ms = 500,
-      }
-    end,
-  },
+        -- Disable with a global variable
+        if vim.g.disable_autoformat then
+          return
+        end
+
+        return {
+          lsp_fallback = true, -- Some formatters (like taplo) are setup with lspconfig
+          timeout_ms = 500,
+        }
+      end,
+    }
+  end,
   config = function(_, opts)
     local ensure_installed = require("plugins.core.mason.ensure_installed")
     ensure_installed(opts.mason_ensure_installed)
--- lua/plugins/core/nvim-lint.lua
+++ lua/plugins/core/nvim-lint.lua
@@ -3,12 +3,27 @@
 -- An asynchronous linter plugin for Neovim complementary to the built-in Language Server Protocol
 -- support.
 
+-- Define here which linter to use for each file type
+-- Keys must be simple file types, and values arrays of linters
+local linters_by_ft = {
+  json = { "jsonlint" },
+  markdown = { "markdownlint" },
+  python = { "ruff" },
+  yaml = { "yamllint" },
+}
+
+-- Specify the linters which have no Mason package
+local linters_without_mason_package = {}
+
+-- Specify the name of the Mason package for linters where they differ
+local linter_to_mason_name = {}
+
 return {
   "mfussenegger/nvim-lint",
   dependencies = {
     "williamboman/mason.nvim",
   },
-  event = { "BufNewFile", "BufReadPre" },
+  ft = vim.tbl_keys(linters_by_ft),
   keys = {
     {
       "<leader>,l",
@@ -26,41 +41,46 @@ return {
       desc = "Settings: toggle [L]int",
     },
   },
-  opts = {
-    mason_ensure_installed = { -- Custom option to automatically install missing Mason packages
-      "jsonlint",
-      "markdownlint",
-      "ruff",
-      "yamllint",
-    },
-    linters_by_ft = {
-      json = { "jsonlint" },
-      markdown = { "markdownlint" },
-      python = { "ruff" },
-      yaml = { "yamllint" },
-    },
-    should_lint = function() -- Custom option to enable/disable linting
-      if vim.g.disable_lint then
-        return false
+  opts = function()
+    local mason_ensure_installed = {}
+    for _, linters in pairs(linters_by_ft) do
+      for _, linter in ipairs(linters) do
+        if not vim.tbl_contains(linters_without_mason_package, linter) then
+          local mason_name = linter_to_mason_name[linter] or linter
+          if not vim.tbl_contains(mason_ensure_installed, mason_name) then
+            table.insert(mason_ensure_installed, mason_name)
+          end
+        end
       end
+    end
 
-      local path = require("utils").path.get_current_file_path()
-      if path == nil then
-        return false
-      end
+    return {
+      -- Custom option to automatically install missing Mason packages
+      mason_ensure_installed = mason_ensure_installed,
+      linters_by_ft = linters_by_ft,
+      should_lint = function() -- Custom option to enable/disable linting
+        if vim.g.disable_lint then
+          return false
+        end
 
-      -- Files outside of the current working directory
-      if vim.startswith(path, "/") or vim.startswith(path, "~") then
-        return false
+        local path = require("utils").path.get_current_file_path()
+        if path == nil then
+          return false
+        end
 
-      -- Files inside Python virtual environments
-      elseif vim.startswith(path, ".venv/") then
-        return false
-      end
+        -- Files outside of the current working directory
+        if vim.startswith(path, "/") or vim.startswith(path, "~") then
+          return false
 
-      return true
-    end,
-  },
+        -- Files inside Python virtual environments
+        elseif vim.startswith(path, ".venv/") then
+          return false
+        end
+
+        return true
+      end,
+    }
+  end,
   config = function(_, opts)
     local ensure_installed = require("plugins.core.mason.ensure_installed")
     ensure_installed(opts.mason_ensure_installed)
