71821b22b51672409e06d92e168a98b3811c29be
clementjumel <clement.jumel@gmail.com>
2024-03-13 13:21:37 +0100

    ♻️ Simplify Luasnip configuration

--- lua/plugins/core/luasnip/init.lua
+++ lua/plugins/core/luasnip/init.lua
@@ -5,51 +5,40 @@
 return {
   "L3MON4D3/LuaSnip",
   version = "v2.*",
-  keys = {
-    {
-      "<C-l>",
-      function()
-        local ls = require("luasnip")
-        ls.jump(1)
-      end,
-      mode = { "i", "s" },
-      desc = "Move to next snippet node",
-    },
-    {
-      "<C-h>",
-      function()
-        local ls = require("luasnip")
-        ls.jump(-1)
-      end,
-      mode = { "i", "s" },
-      desc = "Move to previous snippet node",
-    },
-    {
-      "<C-j>",
-      function()
-        local ls = require("luasnip")
-        if ls.choice_active() then
-          ls.change_choice(1)
-        end
-      end,
-      mode = { "i", "s" },
-      desc = "Next snippet choice option",
-    },
-    {
-      "<C-k>",
-      function()
-        local ls = require("luasnip")
-        if ls.choice_active() then
-          ls.change_choice(-1)
-        end
-      end,
-      mode = { "i", "s" },
-      desc = "Previous snippet choice option",
-    },
-  },
+  lazy = true,
   opts = {},
   config = function(_, opts)
-    require("luasnip").setup(opts)
-    require("luasnip.loaders.from_lua").load({ paths = { "./lua/plugins/core/luasnip/snippets" } })
+    local ls = require("luasnip")
+
+    ls.setup(opts)
+
+    ---@param lhs string
+    ---@param rhs fun()
+    ---@param desc string
+    local map = function(lhs, rhs, desc)
+      vim.keymap.set({ "i", "s" }, lhs, rhs, { desc = desc })
+    end
+
+    -- Define snippet navigation & choice keymaps
+    map("<C-l>", function()
+      ls.jump(1)
+    end, "Move to next snippet node")
+    map("<C-h>", function()
+      ls.jump(-1)
+    end, "Move to previous snippet node")
+    map("<C-j>", function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end, "Next snippet choice option")
+    map("<C-k>", function()
+      if ls.choice_active() then
+        ls.change_choice(-1)
+      end
+    end, "Previous snippet choice option")
+
+    -- Load custom snippets
+    local ls_loader = require("luasnip.loaders.from_lua")
+    ls_loader.load({ paths = { "./lua/plugins/core/luasnip/snippets" } })
   end,
 }
