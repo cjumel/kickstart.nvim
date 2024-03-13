70aa92a337abc32c8d22a16cac28a949fa781b8a
clementjumel <clement.jumel@gmail.com>
2024-03-13 22:14:38 +0100

    🚚 Move Oil re-usable snippets into snippet files

--- lua/plugins/core/luasnip/snippets/oil/files_by_type.lua
+++ lua/plugins/core/luasnip/snippets/oil/files_by_type.lua
@@ -1,6 +1,16 @@
-local oil_snippets = require("plugins.core.luasnip.reusable-snippets.oil")
+local ls = require("luasnip")
+local show_conds = require("luasnip.extras.conditions.show")
 
--- extension can be set to null with 1-item lists
+local custom_conds = require("plugins.core.luasnip.conditions")
+
+local i = ls.insert_node
+local s = ls.snippet
+local t = ls.text_node
+
+-- Define here the file types for which to create a snippet
+-- Extension can be set to null with 1-item lists (meaning the extension is the same as the file
+-- type)
+-- Some file types (e.g. Python & Lua) whill be associated with a condition on the project content
 local filetypes_and_extensions = {
   { "lua" },
   { "markdown", "md" },
@@ -8,10 +18,24 @@ local filetypes_and_extensions = {
   { "python", "py" },
 }
 
+-- Build the snippets
 local snippets = {}
 for _, filetype_and_extension in ipairs(filetypes_and_extensions) do
-  local snippet =
-    oil_snippets.get_filetype_snippet(filetype_and_extension[1], filetype_and_extension[2])
+  local filetype = filetype_and_extension[1]
+  local extension = filetype_and_extension[2] or filetype
+
+  local show_condition = custom_conds.ts.line_begin * show_conds.line_end
+  if filetype == "python" then
+    show_condition = show_condition * custom_conds.project.is_python
+  elseif filetype == "lua" then
+    show_condition = show_condition * custom_conds.project.is_lua
+  end
+
+  local snippet = s(
+    { trig = filetype .. "-file", show_condition = show_condition },
+    { i(1, "name"), t("." .. extension) }
+  )
+
   table.insert(snippets, snippet)
 end
 
--- lua/plugins/core/luasnip/snippets/oil/preset_files.lua
+++ lua/plugins/core/luasnip/snippets/oil/preset_files.lua
@@ -1,5 +1,13 @@
-local oil_snippets = require("plugins.core.luasnip.reusable-snippets.oil")
+local ls = require("luasnip")
+local show_conds = require("luasnip.extras.conditions.show")
 
+local custom_conds = require("plugins.core.luasnip.conditions")
+
+local s = ls.snippet
+local t = ls.text_node
+
+-- Define here the preset files for each file type
+-- Some file types (e.g. Python & Lua) whill be associated with a condition on the project content
 local filetype_to_preset_files = {
   lua = {
     "init.lua",
@@ -18,11 +26,21 @@ local filetype_to_preset_files = {
   },
 }
 
+-- Build the snippets
 local snippets = {}
 for filetype, preset_files in pairs(filetype_to_preset_files) do
-  for _, snippet in
-    ipairs(oil_snippets.get_simple_file_snippets(preset_files, { project_type = filetype }))
-  do
+  local show_condition = custom_conds.ts.line_begin * show_conds.line_end
+  if filetype == "python" then
+    show_condition = show_condition * custom_conds.project.is_python
+  elseif filetype == "lua" then
+    show_condition = show_condition * custom_conds.project.is_lua
+  end
+
+  for _, file_name in ipairs(preset_files) do
+    local snippet = s({
+      trig = file_name,
+      show_condition = show_condition * custom_conds.oil.file_not_exists(file_name),
+    }, { t(file_name) })
     table.insert(snippets, snippet)
   end
 end
