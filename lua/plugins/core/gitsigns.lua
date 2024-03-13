dea678da8d18ce9593ec85e8b4fe41c2a2d82e92
clementjumel <clement.jumel@gmail.com>
2024-03-13 13:31:01 +0100

    ♻️ Simplify gitsigns setup

--- lua/plugins/core/gitsigns.lua
+++ lua/plugins/core/gitsigns.lua
@@ -20,74 +20,71 @@ return {
     attach_to_untracked = true,
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
+      local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+      ---@param mode string|table<string>
+      ---@param lhs string
+      ---@param rhs fun()|string
+      ---@param desc string
+      local function map(mode, lhs, rhs, desc)
+        vim.keymap.set(mode, lhs, rhs, { desc = desc, buffer = bufnr })
       end
 
       -- Navigation
-      local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
+      -- Navigation messages are inconsistent with other movements & are displayed far away from
+      -- the hunk itself so it's more distracting than helpful
       local next_hunk = function()
-        gs.next_hunk({
-          -- Navigation message is inconsistent with other movements & is displayed far away from
-          -- the hunk itself so it's more distracting than helpful
-          navigation_message = false,
-        })
+        gs.next_hunk({ navigation_message = false })
       end
       local prev_hunk = function()
-        gs.prev_hunk({
-          -- Navigation message is inconsistent with other movements & is displayed far away from
-          -- the hunk itself so it's more distracting than helpful
-          navigation_message = false,
-        })
+        gs.prev_hunk({ navigation_message = false })
       end
       next_hunk, prev_hunk = ts_repeat_move.make_repeatable_move_pair(next_hunk, prev_hunk)
-      map({ "n", "x", "o" }, "[h", next_hunk, { desc = "Next hunk" })
-      map({ "n", "x", "o" }, "]h", prev_hunk, { desc = "Previous hunk" })
+      map({ "n", "x", "o" }, "[h", next_hunk, "Next hunk")
+      map({ "n", "x", "o" }, "]h", prev_hunk, "Previous hunk")
 
       local conflict_marker = "<<<<<<< \\|=======\\|>>>>>>> "
+      -- "nohlsearch" clears optional search highlights on the conflict marker
       local next_conflict = function()
         vim.cmd("silent!/" .. conflict_marker)
-        vim.cmd("nohlsearch") -- Clear optional search highlights on the conflict marker
+        vim.cmd("nohlsearch")
       end
       local prev_conflict = function()
         vim.cmd("silent!?" .. conflict_marker)
-        vim.cmd("nohlsearch") -- Clear optional search highlights on the conflict marker
+        vim.cmd("nohlsearch")
       end
       next_conflict, prev_conflict =
         ts_repeat_move.make_repeatable_move_pair(next_conflict, prev_conflict)
-      map({ "n", "x", "o" }, "[H", next_conflict, { desc = "Next conflict hunk" })
-      map({ "n", "x", "o" }, "]H", prev_conflict, { desc = "Previous conflict hunk" })
+      map({ "n", "x", "o" }, "[H", next_conflict, "Next conflict hunk")
+      map({ "n", "x", "o" }, "]H", prev_conflict, "Previous conflict hunk")
 
-      -- Action
-      map({ "n", "v" }, "<leader>h", gs.preview_hunk, { desc = "[H]unk" })
+      -- Actions
+      map({ "n", "v" }, "<leader>h", gs.preview_hunk, "[H]unk")
 
-      map("n", "<leader>a", gs.stage_hunk, { desc = "[A]dd hunk" })
+      map("n", "<leader>a", gs.stage_hunk, "[A]dd hunk")
       map("v", "<leader>a", function()
         gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, { desc = "[A]dd lines" })
-      map("n", "<leader>A", gs.undo_stage_hunk, { desc = "[A]dd-hunk undo" })
-      map("n", "<leader>ga", gs.stage_buffer, { desc = "[G]it: [A]dd buffer" })
+      end, "[A]dd lines")
+      map("n", "<leader>A", gs.undo_stage_hunk, "[A]dd-hunk undo")
+      map("n", "<leader>ga", gs.stage_buffer, "[G]it: [A]dd buffer")
 
-      map("n", "<leader>r", gs.reset_hunk, { desc = "[R]eset hunk" })
+      map("n", "<leader>r", gs.reset_hunk, "[R]eset hunk")
       map("v", "<leader>r", function()
         gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, { desc = "[R]eset lines" })
-      map("n", "<leader>gr", gs.reset_buffer, { desc = "[G]it: [R]eset buffer" })
+      end, "[R]eset lines")
+      map("n", "<leader>gr", gs.reset_buffer, "[G]it: [R]eset buffer")
 
       map("n", "<leader>gd", function()
         gs.diffthis("~")
-      end, { desc = "[G]it: [D]iff" })
+      end, "[G]it: [D]iff")
 
       map("n", "<leader>gB", function()
         gs.blame_line({ full = true })
-      end, { desc = "[G]it: [B]lame" })
+      end, "[G]it: [B]lame")
 
       -- Text objects
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "inner hunk" })
-      map({ "o", "x" }, "ah", ":<C-U>Gitsigns select_hunk<CR>", { desc = "a hunk" })
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "inner hunk")
+      map({ "o", "x" }, "ah", ":<C-U>Gitsigns select_hunk<CR>", "a hunk")
     end,
   },
 }
