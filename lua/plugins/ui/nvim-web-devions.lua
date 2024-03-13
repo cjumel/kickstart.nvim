c030512ae618c93532336e0d2e6fc3bddcbd41d7
clementjumel <clement.jumel@gmail.com>
2024-03-13 11:49:08 +0100

    🩹 Fix Telescope directory icons directly in nvim-web-devions

--- lua/plugins/ui/nvim-web-devions.lua
+++ lua/plugins/ui/nvim-web-devions.lua
@@ -5,7 +5,24 @@
 return {
   "nvim-tree/nvim-web-devicons",
   lazy = true,
-  opts = {},
+  opts = function()
+    -- Fetch the highlight group for Directory, which is the one used by Oil for directories
+    local hl_id = vim.api.nvim_get_hl_id_by_name("Directory")
+    local hl = vim.api.nvim_get_hl(0, { id = hl_id })
+    -- Convert the highlight group's foreground color to a hex color
+    local color = "#" .. string.format("%06x", hl.fg)
+
+    return {
+      override_by_filename = {
+        -- Fix Telescope not showing icons for directories
+        [""] = { -- Telescope uses get_icon with "" as argument for directories
+          icon = "",
+          color = color,
+          name = "Directory",
+        },
+      },
+    }
+  end,
   config = function(_, opts)
     local web_devicons = require("nvim-web-devicons")
 
