ee54f19f45e842f42dd15708e06c5402cb957b84
clementjumel <clement.jumel@gmail.com>
2024-03-14 22:15:02 +0100

    ✨ Small addition to Lua snippets

--- lua/plugins/core/luasnip/snippets/lua/keywords.lua
+++ lua/plugins/core/luasnip/snippets/lua/keywords.lua
@@ -17,6 +17,8 @@ local is_in_code_empty_line = custom_conds.ts.is_in_code
   * custom_conds.ts.line_begin
   * show_conds.line_end
 
+local todo = "-- TO" .. "DO: implement" -- not recognized by todo-comments
+
 return {
 
   -- Local
@@ -24,7 +26,7 @@ return {
     t("local "),
     i(1),
     t(" = "),
-    c(2, { i(2), t("{}"), t("nil") }),
+    c(2, { i(2), t("{}"), t("nil"), t("false"), t("true") }),
   }),
 
   -- Function
@@ -34,7 +36,7 @@ return {
     t("("),
     i(2),
     t({ ")", "\t" }),
-    i(3),
+    c(3, { i(1), t(todo) }),
     t({ "", "end" }),
   }),
 
@@ -43,18 +45,18 @@ return {
     t("if "),
     i(1),
     t({ " then", "\t" }),
-    i(2),
+    c(2, { i(1), t(todo) }),
     t({ "", "end" }),
   }),
   s({ trig = "elsif .. then", show_condition = is_in_code_empty_line }, {
     t("elseif "),
     i(1),
     t({ " then", "\t" }),
-    i(2),
+    c(2, { i(1), t(todo) }),
   }),
   s({ trig = "else", show_condition = is_in_code_empty_line }, {
     t({ "else", "\t" }),
-    i(1),
+    c(1, { i(1), t(todo) }),
   }),
 
   -- For
@@ -62,7 +64,7 @@ return {
     t("for "),
     i(1),
     t({ " do", "\t" }),
-    i(2),
+    c(2, { i(1), t(todo) }),
     t({ "", "end" }),
   }),
   s({ trig = "for .. pairs ..", show_condition = is_in_code_empty_line }, {
@@ -73,7 +75,7 @@ return {
     t(" in pairs("),
     i(3, "t"),
     t({ ") do", "\t" }),
-    i(4),
+    c(4, { i(1), t(todo) }),
     t({ "", "end" }),
   }),
   s({ trig = "for .. ipairs ..", show_condition = is_in_code_empty_line }, {
@@ -84,7 +86,7 @@ return {
     t(" in ipairs("),
     i(3, "t"),
     t({ ") do", "\t" }),
-    i(4),
+    c(4, { i(1), t(todo) }),
     t({ "", "end" }),
   }),
 
@@ -93,7 +95,7 @@ return {
     t("while "),
     i(1),
     t({ " do", "\t" }),
-    i(2),
+    c(2, { i(1), t(todo) }),
     t({ "", "end" }),
   }),
 }
