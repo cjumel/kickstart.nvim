ed0a92ec7044fb286ab1f906cbaf0fe9607bec03
clementjumel <clement.jumel@gmail.com>
2024-03-15 00:35:05 +0100

    ⚡️ Improve zsh snippets

---
+++ lua/plugins/core/luasnip/snippets/zsh/keywords.lua
@@ -0,0 +1,13 @@
+local ls = require("luasnip")
+
+local i = ls.insert_node
+local s = ls.snippet
+local t = ls.text_node
+
+return {
+  s("alias", { t("alias "), i(1, "name"), t("='"), i(2), t("'") }),
+  s(
+    "function",
+    { t("function "), i(1, "name"), t("("), i(2), t({ ") { ", "\t" }), i(3), t({ "", "}" }) }
+  ),
+}
