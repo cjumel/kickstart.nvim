8c5f3774912c14a3af001ea03a9b04f7e06c9bb5
clementjumel <clement.jumel@gmail.com>
2024-03-15 14:03:11 +0100

    ⚡️ Improve Python typing snippets

--- lua/plugins/core/luasnip/snippets/python/typing.lua
+++ lua/plugins/core/luasnip/snippets/python/typing.lua
@@ -3,12 +3,14 @@
 -- not implemented because the completion provided by the language server also comes with a cool
 -- auto-importation.
 
+local extras = require("luasnip.extras")
 local ls = require("luasnip")
 
 local custom_conds = require("plugins.core.luasnip.conditions")
 
 local c = ls.choice_node
 local i = ls.insert_node
+local rep = extras.rep
 local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
@@ -16,25 +18,27 @@ local t = ls.text_node
 local is_in_code_inline = custom_conds.ts.is_in_code * -custom_conds.ts.line_begin
 
 return {
+
+  -- [[ Builtins ]]
+
   s({ trig = "list[..]", show_condition = is_in_code_inline }, {
     t("list["),
     c(1, { i(nil), t("str"), t("bool"), t("float"), t("int") }),
     t("]"),
-    c(2, { i(nil), sn(nil, { t(" | "), i(1) }), t(" | None") }),
   }),
   s({ trig = "set[..]", show_condition = is_in_code_inline }, {
     t("set["),
     c(1, { i(nil), t("str"), t("bool"), t("float"), t("int") }),
     t("]"),
-    c(2, { i(nil), sn(nil, { t(" | "), i(1) }), t(" | None") }),
   }),
   s({ trig = "tuple[..]", show_condition = is_in_code_inline }, {
-    t("tuple["),
-    c(1, { i(nil), t("str"), t("bool"), t("float"), t("int") }),
-    t(", "),
-    c(2, { i(nil), t("..."), t("str"), t("bool"), t("float"), t("int") }),
-    t("]"),
-    c(3, { i(nil), sn(nil, { t(" | "), i(1) }), t(" | None") }),
+    c(1, {
+      sn(nil, { t("tuple["), i(1), t(", "), i(2), t("]") }),
+      sn(nil, { t("tuple["), i(1), t(", "), t("..."), t("]") }),
+      sn(nil, { t("tuple["), i(1), t(", "), rep(1), t("]") }),
+      sn(nil, { t("tuple["), i(1), t(", "), rep(1), t(", "), rep(1), t("]") }),
+      sn(nil, { t("tuple["), i(1), t(", "), rep(1), t(", "), rep(1), t(", "), rep(1), t("]") }),
+    }),
   }),
   s({ trig = "dict[..]", show_condition = is_in_code_inline }, {
     t("dict["),
@@ -42,6 +46,14 @@ return {
     t(", "),
     c(2, { i(nil), t("str"), t("bool"), t("float"), t("int") }),
     t("]"),
-    c(3, { i(nil), sn(nil, { t(" | "), i(1) }), t(" | None") }),
+  }),
+
+  -- [[ Typing inspired ]]
+
+  -- Inspired by `typing.Optional` but with no import required
+  -- Works both before typing the other type (like `typing.Optional`) or after (like `| None`)
+  s({ trig = "optional", show_condition = is_in_code_inline }, {
+    i(1),
+    t(" | None"),
   }),
 }
