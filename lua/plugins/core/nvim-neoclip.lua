fa7dfd7648ad5fbbbaea8d71ba326520780a4b09
clementjumel <clement.jumel@gmail.com>
2024-03-27 18:48:25 +0100

    🩹 Simplify Neoclip configuration

--- lua/plugins/core/nvim-neoclip.lua
+++ lua/plugins/core/nvim-neoclip.lua
@@ -2,23 +2,6 @@
 --
 -- neoclip is a clipboard manager for neovim inspired for example by clipmenu.
 
-local function is_whitespace(line)
-  return vim.fn.match(line, [[^\s*$]]) ~= -1
-end
-
-local function all(tbl, check)
-  for _, entry in ipairs(tbl) do
-    if not check(entry) then
-      return false
-    end
-  end
-  return true
-end
-
-local whitespace_yank_filter_fn = function(data)
-  return not all(data.event.regcontents, is_whitespace)
-end
-
 return {
   "AckslD/nvim-neoclip.lua",
   dependencies = {
@@ -26,63 +9,72 @@ return {
   },
   event = { "BufNewFile", "BufReadPre" },
   keys = {
+    -- Search in yank history is not really convenient (often, older entries are shown before
+    -- the target I'm looking for). Besides, since the yank history is shared between all the
+    -- projects, it is really messy, unlike the undo tree for instance. For these reasons, let's
+    -- only implement simple normal mode interfaces.
     {
-      "<leader>p",
+      "<leader>y",
       function()
-        local opts = require("telescope.themes").get_dropdown({
+        local telescope = require("telescope")
+        local themes = require("telescope.themes")
+
+        local opts = themes.get_dropdown({
           previewer = false,
           layout_config = { width = 0.7 },
+          initial_mode = "normal",
+          prompt_title = "Yank history",
         })
-        opts.initial_mode = "normal"
-        require("telescope").extensions.neoclip.default(opts)
+
+        telescope.extensions.neoclip.default(opts)
       end,
-      desc = "[P]aste candidates history",
-    },
-    {
-      "<leader>q",
-      function()
-        local opts = require("telescope.themes").get_dropdown({
-          previewer = false,
-          layout_config = { width = 0.7 },
-        })
-        opts.initial_mode = "normal"
-        require("telescope").extensions.macroscope.default(opts)
-      end,
-      desc = "[Q]-register macro history",
+      desc = "[Y]ank history",
     },
   },
   opts = {
     enable_persistent_history = true,
+    filter = function(data) -- Don't store pure whitespace yanks
+      local function all(tbl, check)
+        for _, entry in ipairs(tbl) do
+          if not check(entry) then
+            return false
+          end
+        end
+        return true
+      end
+      return not all(data.event.regcontents, function(line)
+        return vim.fn.match(line, [[^\s*$]]) ~= -1
+      end)
+    end,
+    -- Macros saved in history are super hard to read (unlike in the yank history), and they are
+    -- very specific to a given situation, so in practice I'm never using the macro history
+    enable_macro_history = false,
     content_spec_column = true,
     on_select = {
       move_to_front = true,
+      close_telescope = true,
     },
     on_paste = {
       set_reg = true,
       move_to_front = true,
+      close_telescope = true,
     },
-    on_replay = {
-      set_reg = true,
-      move_to_front = true,
-    },
-    -- Don't store pure whitespace yanks
-    filter = whitespace_yank_filter_fn,
     keys = {
       telescope = {
-        i = {
+        i = { -- Insert mode is not the main use case for me, so let's keep only the bare necessary
           select = "<CR>",
           paste = false,
           paste_behind = false,
           replay = false,
-          delete = "<C-d>",
-          edit = "<C-e>",
+          delete = false,
+          edit = false,
         },
         n = {
           select = "<CR>",
-          paste = false,
-          paste_behind = false,
+          paste = "p",
+          paste_behind = "P",
           replay = false,
-          delete = "d",
+          delete = false,
           edit = "e",
         },
       },
