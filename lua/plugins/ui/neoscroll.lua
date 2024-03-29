7822c0824677bfa3521d4cb0bbf701161985981d
clementjumel <clement.jumel@gmail.com>
2024-03-29 09:00:39 +0100

    ✨ Small improvements in Neoscroll setup
    These improvements include mainly a better <C-e> and <C-y>
    behaviour, and <C-u>/<C-d> scrolling with the arrow keys.

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -2,6 +2,22 @@
 --
 -- A smooth scrolling Neovim plugin written in Lua.
 
+local function c_u()
+  require("neoscroll").scroll(-vim.wo.scroll, true, 250) -- Defaults
+end
+local function c_d()
+  require("neoscroll").scroll(vim.wo.scroll, true, 250) -- Defaults
+end
+
+-- For <C-y> & <C-e>, a relative number of lines to scroll (the default) doesn't work well in very
+-- small windows, whereas an absolute number of lines has no real drawback for me
+local function c_y()
+  require("neoscroll").scroll(-5, false, 100) -- Defaults: -0.10, false, 100
+end
+local function c_e()
+  require("neoscroll").scroll(5, false, 100) -- Defaults: 0.10, false, 100
+end
+
 return {
   "karb94/neoscroll.nvim",
   -- Using `event = { "BufNewFile", "BufReadPre" }` makes Neoscroll not activated in some buffers
@@ -9,32 +25,22 @@ return {
   event = "VeryLazy",
   keys = {
     -- Use regular scrolling features with Neoscroll
-    { "<C-u>", mode = { "n", "v" }, desc = "Scroll up half of a window height" },
-    { "<C-d>", mode = { "n", "v" }, desc = "Scroll down half of a window height" },
-    { "<C-b>", mode = { "n", "v" }, desc = "Scroll up a window height" },
-    { "<C-f>", mode = { "n", "v" }, desc = "Scroll down a window height" },
-    { "<C-y>", mode = { "n", "v" }, desc = "Scroll up 10% of a window height (without cursor)" },
-    { "<C-e>", mode = { "n", "v" }, desc = "Scroll down 10% of a window height (without cursor)" },
+    { "<C-u>", c_u, mode = { "n", "v" }, desc = "Scroll up half a page" },
+    { "<C-d>", c_d, mode = { "n", "v" }, desc = "Scroll down half a page" },
+    { "<C-b>", mode = { "n", "v" }, desc = "Scroll up a page" },
+    { "<C-f>", mode = { "n", "v" }, desc = "Scroll down a page" },
+    { "<C-y>", c_y, mode = { "n", "v" }, desc = "Move window 5 lines up" },
+    { "<C-e>", c_e, mode = { "n", "v" }, desc = "Move window 5 lines down" },
     { "zt", mode = { "n", "v" }, desc = "Top this line" },
     { "zz", mode = { "n", "v" }, desc = "Center this line" },
     { "zb", mode = { "n", "v" }, desc = "Bottom this line" },
-    -- Use Neoscroll for up & down arrow keys
-    {
-      "<Up>",
-      function()
-        require("neoscroll").scroll(-5, false, 100)
-      end,
-      mode = { "n", "v" },
-      desc = "Scroll up 5 lines",
-    },
-    {
-      "<Down>",
-      function()
-        require("neoscroll").scroll(5, false, 100)
-      end,
-      mode = { "n", "v" },
-      desc = "Scroll down 5 lines",
-    },
+    -- Map arrow keys to Neoscroll features to navigate within a document with only one hand
+    { "<Left>", c_u, mode = { "n", "v" }, desc = "Scroll up half a page" },
+    { "<Right>", c_d, mode = { "n", "v" }, desc = "Scroll down half a page" },
+    { "<Up>", c_y, mode = { "n", "v" }, desc = "Move window 5 lines up" },
+    { "<Down>", c_e, mode = { "n", "v" }, desc = "Move window 5 lines down" },
   },
-  opts = {},
+  opts = {
+    mappings = { "<C-b>", "<C-f>", "zt", "zz", "zb" }, -- Only those which are not redefined above
+  },
 }
