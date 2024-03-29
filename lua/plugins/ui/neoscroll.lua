38252cb0735a1960aa05530506fb4e4e8ec035d3
clementjumel <clement.jumel@gmail.com>
2024-03-29 17:45:33 +0100

    🩹 Some fixes & improvements of Neoscroll keymaps

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -2,42 +2,73 @@
 --
 -- A smooth scrolling Neovim plugin written in Lua.
 
-local function c_u()
-  require("neoscroll").scroll(-vim.wo.scroll, true, 250) -- Defaults
-end
-local function c_d()
-  require("neoscroll").scroll(vim.wo.scroll, true, 250) -- Defaults
-end
-
--- For <C-y> & <C-e>, a relative number of lines to scroll (the default) doesn't work well in very
--- small windows, whereas an absolute number of lines has no real drawback for me
-local function c_y()
-  require("neoscroll").scroll(-5, false, 100) -- Defaults: -0.10, false, 100
-end
-local function c_e()
-  require("neoscroll").scroll(5, false, 100) -- Defaults: 0.10, false, 100
-end
-
 return {
   "karb94/neoscroll.nvim",
   keys = {
-    -- Use regular scrolling features with Neoscroll
-    { "<C-u>", c_u, mode = { "n", "v" }, desc = "Scroll up half a page" },
-    { "<C-d>", c_d, mode = { "n", "v" }, desc = "Scroll down half a page" },
-    { "<C-b>", mode = { "n", "v" }, desc = "Scroll up a page" },
-    { "<C-f>", mode = { "n", "v" }, desc = "Scroll down a page" },
-    { "<C-y>", c_y, mode = { "n", "v" }, desc = "Move window 5 lines up" },
-    { "<C-e>", c_e, mode = { "n", "v" }, desc = "Move window 5 lines down" },
+    -- Mappings used with the default options
+    { "<C-u>", mode = { "n", "v" }, desc = "Scroll half a page up" },
+    { "<C-d>", mode = { "n", "v" }, desc = "Scroll half a page down" },
+    { "<C-b>", mode = { "n", "v" }, desc = "Scroll a page up" },
+    { "<C-f>", mode = { "n", "v" }, desc = "Scroll a page down" },
     { "zt", mode = { "n", "v" }, desc = "Top this line" },
     { "zz", mode = { "n", "v" }, desc = "Center this line" },
     { "zb", mode = { "n", "v" }, desc = "Bottom this line" },
-    -- Map arrow keys to Neoscroll features to navigate within a document with only one hand
-    { "<Left>", c_u, mode = { "n", "v" }, desc = "Scroll up half a page" },
-    { "<Right>", c_d, mode = { "n", "v" }, desc = "Scroll down half a page" },
-    { "<Up>", c_y, mode = { "n", "v" }, desc = "Move window 5 lines up" },
-    { "<Down>", c_e, mode = { "n", "v" }, desc = "Move window 5 lines down" },
+
+    -- For <C-y> & <C-e>, a relative number of lines to scroll (the default) doesn't work well in
+    -- very small windows, whereas an absolute number of lines has no real drawback
+    {
+      "<C-y>",
+      function()
+        require("neoscroll").scroll(-5, false, 100) -- Defaults: -0.10, false, 100
+      end,
+      mode = { "n", "v" },
+      desc = "Move window 5 lines up",
+    },
+    {
+      "<C-e>",
+      function()
+        require("neoscroll").scroll(5, false, 100) -- Defaults: 0.10, false, 100
+      end,
+      mode = { "n", "v" },
+      desc = "Move window 5 lines down",
+    },
+
+    -- Map arrow keys with Neoscroll to navigate within a document with only one hand
+    {
+      "<Left>",
+      function()
+        require("neoscroll").scroll(-vim.wo.scroll, false, 250)
+      end,
+      mode = { "n", "v" },
+      desc = "Move window half a page up",
+    },
+    {
+      "<Right>",
+      function()
+        require("neoscroll").scroll(vim.wo.scroll, false, 250)
+      end,
+      mode = { "n", "v" },
+      desc = "Move window half a page down",
+    },
+    {
+      "<Up>",
+      function()
+        require("neoscroll").scroll(-5, false, 100)
+      end,
+      mode = { "n", "v" },
+      desc = "Move window 5 lines up",
+    },
+    {
+      "<Down>",
+      function()
+        require("neoscroll").scroll(5, false, 100)
+      end,
+      mode = { "n", "v" },
+      desc = "Move window 5 lines down",
+    },
   },
   opts = {
-    mappings = { "<C-b>", "<C-f>", "zt", "zz", "zb" }, -- Only those which are not redefined above
+    -- Define the mappings used with the default options
+    mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
   },
 }
