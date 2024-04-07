7d6457caab0c36afb43a2d9552038c32631be959
clementjumel <clement.jumel@gmail.com>
2024-04-07 21:19:36 +0100

    ⚡️ Improve Dracula theme
    This makes the Dracula theme config similar to Kanagawa's one,
    which works better than with the acutal Lualine theme.

--- lua/themes/dracula.lua
+++ lua/themes/dracula.lua
@@ -4,8 +4,7 @@ M.dracula_enabled = true
 
 M.lualine_opts = {
   options = {
-    theme = "dracula-nvim",
-    component_separators = "|",
+    component_separators = "",
     section_separators = "",
     refresh = {
       statusline = 50, -- Decrease refresh rate to make modules more responsive (e.g. Harpoon's)
