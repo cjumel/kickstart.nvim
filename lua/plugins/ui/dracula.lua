b8e6c4ac226d11eed97d6045157787ed4fa23882
clementjumel <clement.jumel@gmail.com>
2024-04-08 11:58:20 +0200

    🩹 Fix Dracula highlight groups for Neogit

--- lua/plugins/ui/dracula.lua
+++ lua/plugins/ui/dracula.lua
@@ -19,5 +19,13 @@ return {
   config = function(_, opts)
     require("dracula").setup(opts)
     vim.cmd.colorscheme("dracula")
+
+    -- Fix hard-to-read highlight groups for Neogit (foreground color hasn't enought contrast)
+    -- Default highlight groups for Neogit with this color scheme are:
+    --   NeogitDiffAddxxx cterm= gui= guifg=#c6cd73 guibg=#50fa7b
+    --   NeogitDiffAddHighlightxxx cterm= gui= guifg=#f1fa8c guibg=#50fa7b
+    -- Let's make the highlight group foreground color use darker greens
+    vim.api.nvim_command("hi NeogitDiffAdd guifg=#00a300 guibg=#50fa7b")
+    vim.api.nvim_command("hi NeogitDiffAddHighlight guifg=#006400 guibg=#50fa7b")
   end,
 }
