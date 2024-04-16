cde710efecedfa9492e40da429e3c4bcf0383ba6
clementjumel <clement.jumel@gmail.com>
2024-04-16 17:06:37 +0200

    🔥 Remove global colorcolumn setting
    I'm removing this to implement a smarter & file-type specific
    setting instead.

--- lua/options.lua
+++ lua/options.lua
@@ -9,9 +9,6 @@ vim.opt.showmode = false
 -- Status line mode
 vim.o.laststatus = 3 -- Use a global status line & a thin line to separate splits
 
--- Display a column ruler after the 100th character
-vim.o.colorcolumn = "101"
-
 -- Support additional pairs of characters
 vim.o.matchpairs = vim.o.matchpairs .. ",<:>"
 
