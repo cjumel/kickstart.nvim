fb9eab92e3e0110e86a5a2d913318e672d95b5de
clementjumel <clement.jumel@gmail.com>
2024-04-17 17:38:46 +0200

    ✨ Add ruler column for markdown, toml & yaml files

---
+++ ftplugin/yaml.lua
@@ -0,0 +1,45 @@
+--- Look for a yamlfmt configuration file and output a colorcolumn value based on it or nil.
+---@return string|nil
+local function get_colorcolumn()
+  local config_file_names = { ".yamlfmt", "yamlfmt.yml", "yamlfmt.yaml", ".yamlfmt.yaml", ".yamlfmt.yml" }
+  local line_length_pattern = "  max_line_length: "
+
+  local dir = vim.fn.getcwd()
+
+  while true do
+    for _, config_file_name in ipairs(config_file_names) do
+      local config_file_path = dir .. "/" .. config_file_name
+      if vim.fn.filereadable(config_file_path) == 1 then -- A configuration file was found
+        local file = io.open(config_file_path, "r")
+        if not file then -- Unable to open file
+          return nil
+        end
+
+        for line in file:lines() do
+          local start, end_ = string.find(line, line_length_pattern, 1, true)
+          if start == 1 and end_ ~= nil then -- The target line was found and is not a comment (start == 1)
+            file:close()
+
+            local line_length = tonumber(line.sub(line, end_ + 1))
+            return tostring(line_length + 1)
+          end
+        end
+
+        -- A config file is found but no column_width line
+        file:close()
+        return nil
+      end
+    end
+
+    if dir == vim.env.HOME then
+      return nil
+    else
+      dir = vim.fn.fnamemodify(dir, ":h") -- Change dir to its parent directory & loop again
+    end
+  end
+end
+
+-- Display a column ruler at the relevant line length
+if not vim.g.disable_colorcolumn then
+  vim.opt_local.colorcolumn = get_colorcolumn()
+end
