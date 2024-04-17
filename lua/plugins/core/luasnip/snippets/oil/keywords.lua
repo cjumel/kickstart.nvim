fa71eaae64240f483ecbd571256b1521c138e91e
clementjumel <clement.jumel@gmail.com>
2024-04-17 15:07:48 +0200

    ✨ Further improve Oil snippets

--- lua/plugins/core/luasnip/snippets/oil/keywords.lua
+++ lua/plugins/core/luasnip/snippets/oil/keywords.lua
@@ -17,42 +17,58 @@ local t = ls.text_node
 local is_in_empty_line = custom_conds.line_begin * show_conds.line_end
 
 return {
-  s({ trig = "directory", show_condition = is_in_empty_line }, { i(1, "name"), t("/") }),
-  s({ trig = "json file", show_condition = is_in_empty_line }, { i(1, "name"), t(".json") }),
-  s({ trig = "lua file", show_condition = is_in_empty_line * custom_conds.project.is_lua }, {
+  s({ trig = ".json", show_condition = is_in_empty_line }, { i(1, "name"), t(".json") }),
+  s({ trig = ".json", show_condition = -is_in_empty_line }, { t(".json") }),
+
+  s({ trig = ".lua", show_condition = custom_conds.project.is_lua * is_in_empty_line }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".lua") }),
       t("init.lua"),
       t("temp.lua"),
     }),
   }),
-  s({ trig = "markdown file", show_condition = is_in_empty_line }, {
+  s({ trig = ".lua", show_condition = custom_conds.project.is_lua - is_in_empty_line }, { t(".lua") }),
+
+  s({ trig = ".md", show_condition = is_in_empty_line }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".md") }),
       t("README.md"),
       t("notes.md"),
     }),
   }),
-  s({ trig = "norg file", show_condition = is_in_empty_line }, {
+  s({ trig = ".md", show_condition = -is_in_empty_line }, { t(".md") }),
+
+  s({ trig = ".norg", show_condition = is_in_empty_line }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".norg") }),
       t("todo.norg"),
     }),
   }),
-  s({ trig = "python file", show_condition = is_in_empty_line * custom_conds.project.is_python }, {
+  s({ trig = ".norg", show_condition = -is_in_empty_line }, { t(".norg") }),
+
+  s({ trig = ".py", show_condition = custom_conds.project.is_python * is_in_empty_line }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".py") }),
       t("__init__.py"),
       t("temp.py"),
     }),
   }),
-  s({ trig = "sh file", show_condition = is_in_empty_line }, { i(1, "name"), t(".sh") }),
-  s({ trig = "toml file", show_condition = is_in_empty_line }, { i(1, "name"), t(".toml") }),
-  s({ trig = "yaml file", show_condition = is_in_empty_line }, {
+  s({ trig = ".py", show_condition = custom_conds.project.is_python - is_in_empty_line }, { t(".py") }),
+
+  s({ trig = ".sh", show_condition = is_in_empty_line }, { i(1, "name"), t(".sh") }),
+  s({ trig = ".sh", show_condition = -is_in_empty_line }, { t(".sh") }),
+
+  s({ trig = ".toml", show_condition = is_in_empty_line }, { i(1, "name"), t(".toml") }),
+  s({ trig = ".toml", show_condition = -is_in_empty_line }, { t(".toml") }),
+
+  s({ trig = ".yaml", show_condition = is_in_empty_line }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".yaml") }),
       sn(nil, { i(1, "name"), t(".yml") }),
     }),
   }),
-  s({ trig = "zsh file", show_condition = is_in_empty_line }, { i(1, "name"), t(".zsh") }),
+  s({ trig = ".yaml", show_condition = -is_in_empty_line }, { c(1, { t(".yaml"), t(".yml") }) }),
+
+  s({ trig = ".zsh", show_condition = is_in_empty_line }, { i(1, "name"), t(".zsh") }),
+  s({ trig = ".zsh", show_condition = -is_in_empty_line }, { t(".zsh") }),
 }
