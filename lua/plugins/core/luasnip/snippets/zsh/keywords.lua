53e8dfdb6379cf17155324a175f8ff82d67f66bf
clementjumel <clement.jumel@gmail.com>
2024-04-19 09:32:41 +0200

    ✨ Add line begin condition in zsh snippets

--- lua/plugins/core/luasnip/snippets/zsh/keywords.lua
+++ lua/plugins/core/luasnip/snippets/zsh/keywords.lua
@@ -1,12 +1,20 @@
 local ls = require("luasnip")
 
+local custom_conds = require("plugins.core.luasnip.conditions")
+
 local i = ls.insert_node
 local s = ls.snippet
 local t = ls.text_node
 
 return {
-  s("alias", { t("alias "), i(1, "name"), t("='"), i(2), t("'") }),
-  s("export", { t("export "), i(1, "name"), t("='"), i(2), t("'") }),
-  s("function", { t("function "), i(1, "name"), t("("), i(2), t({ ") { ", "\t" }), i(3), t({ "", "}" }) }),
-  s("local", { t("local "), i(1, "name"), t("="), i(2) }),
+  s({ trig = "alias", show_condition = custom_conds.line_begin }, { t("alias "), i(1, "name"), t("='"), i(2), t("'") }),
+  s(
+    { trig = "export", show_condition = custom_conds.line_begin },
+    { t("export "), i(1, "name"), t("='"), i(2), t("'") }
+  ),
+  s(
+    { trig = "function", show_condition = custom_conds.line_begin },
+    { t("function "), i(1, "name"), t("("), i(2), t({ ") { ", "\t" }), i(3), t({ "", "}" }) }
+  ),
+  s({ trig = "local", show_condition = custom_conds.line_begin }, { t("local "), i(1, "name"), t("="), i(2) }),
 }
