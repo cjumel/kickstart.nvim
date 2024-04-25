f7f28b586344719ae51c7569b49120bec8b0d4c8
clementjumel <clement.jumel@gmail.com>
2024-04-25 16:26:18 +0200

    ♻️ Simplify nvim-various-textobjs config

--- lua/plugins/core/nvim-various-textobjs.lua
+++ lua/plugins/core/nvim-various-textobjs.lua
@@ -5,87 +5,23 @@
 
 return {
   "chrisgrieser/nvim-various-textobjs",
-  keys = {
-    -- as/is overwrite the builtin outer/inner sentence text objects (useless when writing code)
-    {
-      "as",
-      function() require("various-textobjs").subword("outer") end,
-      mode = { "x", "o" },
-      desc = "a subword",
-    },
-    {
-      "is",
-      function() require("various-textobjs").subword("inner") end,
-      mode = { "x", "o" },
-      desc = "inner subword",
-    },
-    {
-      "gr", -- gb & gc are taken by comments.nvim
-      function() require("various-textobjs").toNextClosingBracket() end,
-      mode = { "x", "o" },
-      desc = "Next right bracket",
-    },
-    {
-      "gq",
-      function() require("various-textobjs").toNextQuotationMark() end,
-      mode = { "x", "o" },
-      desc = "Next quotation mark",
-    },
-    {
-      "gm", -- Similar to ag/ig in multi-line mode
-      function() require("various-textobjs").multiCommentedLines() end,
-      mode = { "x", "o" },
-      desc = "Multi-line comment",
-    },
-    {
-      "gG", -- Combine gg and G
-      function() require("various-textobjs").entireBuffer() end,
-      mode = { "x", "o" },
-      desc = "Entire buffer",
-    },
-    {
-      "gn",
-      function() require("various-textobjs").nearEoL() end,
-      mode = { "x", "o" },
-      desc = "Near end of line",
-    },
-    {
-      "gl",
-      function()
-        require("various-textobjs").lineCharacterwise("inner") -- "outer" is useless
-      end,
-      mode = { "x", "o" },
-      desc = "Line characterwise",
-    },
-    {
-      "av",
-      function() require("various-textobjs").value("outer") end,
-      mode = { "x", "o" },
-      desc = "a key-value pair value",
-    },
-    {
-      "iv",
-      function() require("various-textobjs").value("inner") end,
-      mode = { "x", "o" },
-      desc = "inner key-value pair value",
-    },
-    {
-      "ak",
-      function() require("various-textobjs").key("outer") end,
-      mode = { "x", "o" },
-      desc = "a key-value pair key",
-    },
-    {
-      "ik",
-      function() require("various-textobjs").key("inner") end,
-      mode = { "x", "o" },
-      desc = "inner key-value pair key",
-    },
-    {
-      "gu",
-      function() require("various-textobjs").url() end,
-      mode = { "x", "o" },
-      desc = "URL",
-    },
-  },
+  keys = function()
+    local textobjs = require("various-textobjs")
+    local mode = { "x", "o" }
+    return {
+      { "as", function() textobjs.subword("outer") end, mode = mode, desc = "a subword" },
+      { "is", function() textobjs.subword("inner") end, mode = mode, desc = "inner subword" },
+      { "gr", textobjs.toNextClosingBracket, mode = mode, desc = "Next right bracket" },
+      { "gq", textobjs.toNextQuotationMark, mode = mode, desc = "Next quotation mark" },
+      { "gm", textobjs.multiCommentedLines, mode = mode, desc = "Multi-line comment" },
+      { "gG", textobjs.entireBuffer, mode = mode, desc = "Entire buffer" },
+      { "gn", textobjs.nearEoL, mode = mode, desc = "Near end of line" },
+      { "gl", function() textobjs.lineCharacterwise("inner") end, mode = mode, desc = "Line characterwise" },
+      { "av", function() textobjs.value("outer") end, mode = mode, desc = "a key-value pair value" },
+      { "iv", function() textobjs.value("inner") end, mode = mode, desc = "inner key-value pair value" },
+      { "ak", function() textobjs.key("outer") end, mode = mode, desc = "a key-value pair key" },
+      { "ik", function() textobjs.key("inner") end, mode = mode, desc = "inner key-value pair key" },
+      { "gu", textobjs.url, mode = mode, desc = "URL" },
+    }
+  end,
 }
