f6516ebb2de8ae72864b2139aab61de047c7181d
clementjumel <clement.jumel@gmail.com>
2024-04-26 12:41:37 +0200

    🩹 Fix invalid window id issue in clear window

--- lua/actions.lua
+++ lua/actions.lua
@@ -68,14 +68,16 @@ function M.clear_window()
   vim.cmd("nohlsearch")
 
   -- Dismiss Noice messages if Noice is loaded
-  if package.loaded.noice ~= nil then
-    require("noice").cmd("dismiss")
+  local noice = package.loaded.noice
+  if noice ~= nil then
+    noice.cmd("dismiss")
   end
 
   -- If zen-mode is loaded, fetch its window ids to avoid closing them
+  local zen_mode = package.loaded["zen-mode"]
   local zen_mode_win = nil -- Main window
   local zen_mode_bg_win = nil -- Background window
-  if package.loaded["zen-mode"] ~= nil then
+  if zen_mode ~= nil then
     local zen_mode_view = require("zen-mode.view")
     if zen_mode_view.is_open() then
       zen_mode_win = zen_mode_view.win
@@ -83,10 +85,10 @@ function M.clear_window()
     end
   end
 
-  -- Clear remaining relative windows (e.g. preview or hover floating windows) except the zen-mode
-  -- windows
+  -- Clear remaining relative windows (e.g. preview or hover floating windows) except the zen-mode windows
   for _, id in ipairs(vim.api.nvim_list_wins()) do
-    if vim.api.nvim_win_get_config(id).relative ~= "" and id ~= zen_mode_win and id ~= zen_mode_bg_win then
+    local ok, win_config = pcall(vim.api.nvim_win_get_config, id) -- Handle cases of invalid window ids
+    if ok and win_config.relative ~= "" and id ~= zen_mode_win and id ~= zen_mode_bg_win then
       vim.api.nvim_win_close(id, false)
     end
   end
