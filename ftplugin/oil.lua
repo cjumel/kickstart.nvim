8b7379a9ae7a599935037ceecc6ef463e1a03b5c
clementjumel <clement.jumel@gmail.com>
2024-04-29 20:48:29 +0200

    🩹 Udpate & simplify Oil config

--- ftplugin/oil.lua
+++ ftplugin/oil.lua
@@ -1,3 +1,6 @@
 -- Disable line numbering
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
+
+-- Make Oil preview show files on the right hand side instead of left (default)
+vim.opt_local.splitright = true
--- lua/plugins/navigation/oil.lua
+++ lua/plugins/navigation/oil.lua
@@ -1,81 +1,45 @@
 -- oil.nvim
 --
--- Enable netwrc-style file navigation in buffers.
--- Additionally, this plugin supports editing files like editing a buffer (renaming, writing, etc.)
+-- A netwrc-like file explorer that lets you edit your file system like a regular Neovim buffer.
+--
+-- Oil is my plugin of choice for file navigation. It's a powerful alternative to tree explorers, focusing on the
+-- current directory in a single window, instead of the whole project tree in a side window. The file editing features
+-- it provides are very elegant and natural to learn, and they are usable with regular edition plugins like Copilot.vim
+-- or nvim-cmp with Luasnip, which makes creating files customizable & very simple.
 
 return {
   "stevearc/oil.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  -- To use oil as default file explorer, it must not be lazy loaded or with VeryLazy event
-  lazy = false,
+  lazy = false, -- Required to be used as default file explorer
   keys = {
-    {
-      "-",
-      function() require("oil").open() end,
-      desc = "Open Oil buffer",
-    },
+    { "-", function() require("oil").open() end, desc = "Open parent directory" },
   },
   opts = {
-    win_options = {
-      signcolumn = "yes",
-    },
-    -- Cleanup the oil buffer right away to avoid jumping back to it with <C-o> and <C-i>
-    cleanup_delay_ms = 0,
+    win_options = { signcolumn = "yes" }, -- Add an empty column, improving the look of the Oil buffer
+    cleanup_delay_ms = 0, -- Cleanup the oil buffer right away to avoid jumping back to it with <C-o> and <C-i>
     keymaps = {
+      ["?"] = "actions.show_help",
       ["<CR>"] = "actions.select",
       ["<C-v>"] = "actions.select_vsplit",
       ["<C-x>"] = "actions.select_split",
+      ["<C-i>"] = "actions.preview",
       ["q"] = "actions.close",
+      ["R"] = "actions.refresh",
       ["-"] = "actions.parent",
       ["_"] = "actions.open_cwd",
       ["H"] = "actions.toggle_hidden",
-      ["R"] = "actions.refresh",
-      ["?"] = "actions.show_help",
-      -- Custom actions
-      -- Overwrite the default preview action to open the preview window on the right hand side
-      ["<C-i>"] = {
-        desc = "Open the entry under the cursor in a preview window, or close the preview window if already open",
-        callback = function()
-          local oil = require("oil")
-          local util = require("oil.util")
-          local entry = oil.get_cursor_entry()
-          if not entry then
-            vim.notify("Could not find entry under cursor", vim.log.levels.ERROR)
-            return
-          end
-          local winid = util.get_preview_win()
-          if winid then
-            local cur_id = vim.w[winid].oil_entry_id
-            if entry.id == cur_id then
-              vim.api.nvim_win_close(winid, true)
-              return
-            end
-          end
-          oil.select({ preview = true, split = "belowright" }) -- Open on the right hand side
-        end,
-      },
     },
     use_default_keymaps = false,
     view_options = {
-      show_hidden = false,
       is_hidden_file = function(name, _)
-        -- Add some stuff globally git-ignored but not hidden
         if name == "__pycache__" then -- Python cache files
           return true
         end
-
         return vim.startswith(name, ".")
       end,
-      is_always_hidden = function(name, _)
-        -- Displaying ".." doesn't bring neither new information nor features
-        if name == ".." then
-          return true
-        end
-
-        return false
-      end,
+      is_always_hidden = function(name, _) return name == ".." end,
     },
   },
 }
