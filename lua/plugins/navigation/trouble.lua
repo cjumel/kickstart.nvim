91b9b2deda7d139d28606d60effeef63c7f07105
clementjumel <clement.jumel@gmail.com>
2024-04-30 15:19:20 +0200

    🩹 Update & improve Trouble config

--- lua/plugins/navigation/trouble.lua
+++ lua/plugins/navigation/trouble.lua
@@ -1,53 +1,33 @@
 -- Trouble
 --
--- Trouble provides a pretty list for showing diagnostics, references, telescope results,
--- quickfix and location lists to help you solve all the trouble your code is causing.
+-- Trouble provides pretty lists for showing diagnostics, references, telescope results, quickfix and location lists
+-- to help you solve all the trouble your code is causing. It is a very nice interface for very various stuff, quite
+-- complementary with Telescope.
 
 return {
   "folke/trouble.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  keys = {
-    {
-      "<leader>xx",
-      function() require("trouble").toggle() end,
-      desc = "Trouble: toggle",
-    },
-    {
-      "<leader>xd",
-      function() require("trouble").toggle("document_diagnostics") end,
-      desc = "Trouble: [D]ocument diagnostics",
-    },
-    {
-      "<leader>xw",
-      function() require("trouble").toggle("workspace_diagnostics") end,
-      desc = "Trouble: [W]orkspace diagnostics",
-    },
-    {
-      "<leader>xq",
-      function() require("trouble").toggle("quickfix") end,
-      desc = "Trouble: [Q]uickfix",
-    },
-    {
-      "<leader>xl",
-      function() require("trouble").toggle("loclist") end,
-      desc = "Trouble: [L]oclist",
-    },
-  },
-  opts = {
-    mode = "document_diagnostics", -- Default mode for toggle
-  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = function()
+    local trouble = require("trouble")
+    local toggle = trouble.toggle
+    return {
+      { "<leader>xx", function() toggle() end, desc = "Trouble: toggle" },
+      { "<leader>xd", function() toggle("document_diagnostics") end, desc = "Trouble: [D]ocument diagnostics" },
+      { "<leader>xw", function() toggle("workspace_diagnostics") end, desc = "Trouble: [W]orkspace diagnostics" },
+      { "<leader>xq", function() toggle("quickfix") end, desc = "Trouble: [Q]uickfix" },
+      { "<leader>xl", function() toggle("loclist") end, desc = "Trouble: [L]oclist" },
+    }
+  end,
+  opts = {},
   config = function(_, opts)
     local trouble = require("trouble")
+    local utils = require("utils")
 
     trouble.setup(opts)
 
-    local utils = require("utils")
-
     utils.keymap.set_move_pair({ "[x", "]x" }, {
-      function() require("trouble").next({ skip_groups = true, jump = true }) end,
-      function() require("trouble").previous({ skip_groups = true, jump = true }) end,
+      function() trouble.next({ skip_groups = true, jump = true }) end,
+      function() trouble.previous({ skip_groups = true, jump = true }) end,
     }, { { desc = "Next Trouble item" }, { desc = "Previous Trouble item" } })
   end,
 }
