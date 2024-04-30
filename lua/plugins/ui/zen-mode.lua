d9ca21df2ffa55d49cf08ad06700d30e91cc1c00
clementjumel <clement.jumel@gmail.com>
2024-04-30 20:19:40 +0200

    🩹 Simplify Zen Mode config

--- lua/plugins/ui/zen-mode.lua
+++ lua/plugins/ui/zen-mode.lua
@@ -1,32 +1,21 @@
--- Zen-mode
+-- Zen Mode
 --
--- Zen mode, striped out of distractions, for Neovim.
+-- Zen mode provides a centered mode striped out of distractions. It's a very nice addition to Neovim, especially
+-- since it works over window splits (useful to center one split) and with Tmux.
 
 return {
   "folke/zen-mode.nvim",
-  keys = {
-    {
-      "<leader>z",
-      function() require("zen-mode").toggle() end,
-      desc = "[Z]en mode",
-    },
-  },
+  keys = { { "<leader>z", function() require("zen-mode").toggle() end, desc = "[Z]en mode" } },
   opts = {
     window = {
       backdrop = 1, -- No difference between the background and the window
       width = 126, -- Base width (120) + some room for line numbers and column signs
-      options = {
-        number = false, -- Disable number column
-        relativenumber = false,
-        colorcolumn = "0", -- Disable column ruler
-      },
+      options = { number = false, relativenumber = false, colorcolumn = "0" }, -- Disable numbers and ruler column
     },
     plugins = {
       twilight = { enabled = false }, -- Don't enable Twilight when entering zen mode
       tmux = { enabled = true }, -- Disable Tmux status line when entering zen mode
-      options = {
-        laststatus = 0, -- turn off the statusline in zen mode
-      },
+      options = { laststatus = 0 }, -- Turn off the statusline in zen mode
     },
   },
 }
