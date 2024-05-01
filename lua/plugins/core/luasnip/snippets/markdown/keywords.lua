c97067d5e99b7b59d0c9c7f24748f7b46db88df0
clementjumel <clement.jumel@gmail.com>
2024-05-02 00:31:35 +0200

    ✨ Todo-item snippets for markdown

--- lua/plugins/core/luasnip/snippets/markdown/keywords.lua
+++ lua/plugins/core/luasnip/snippets/markdown/keywords.lua
@@ -6,6 +6,7 @@ local custom_conds = require("plugins.core.luasnip.conditions")
 local c = ls.choice_node
 local i = ls.insert_node
 local s = ls.snippet
+local sn = ls.snippet_node
 local t = ls.text_node
 
 return {
@@ -16,6 +17,14 @@ return {
     t({ "", "" }),
     i(2),
     t({ "", "```" }),
+  }),
+  s({ trig = "todo-item", show_condition = custom_conds.line_begin }, {
+    c(1, {
+      sn(nil, { t("- 🎯 "), i(1) }),
+      sn(nil, { t("  - 🎯 "), i(1) }),
+      sn(nil, { t("    - 🎯 "), i(1) }),
+      sn(nil, { t("      - 🎯 "), i(1) }),
+    }),
   }),
   s({ trig = "toggle-block", show_condition = custom_conds.line_begin * show_conds.line_end }, {
     t({ "<details>", "<summary>" }),
