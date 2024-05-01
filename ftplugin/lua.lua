108ab9c30a4daefbfe3442ca46ee9f852ccf5274
clementjumel <clement.jumel@gmail.com>
2024-05-01 18:22:59 +0200

    ✨ Keymaps to yank Lua or Python modules

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -47,3 +47,20 @@ end
 if not vim.g.disable_colorcolumn then
   vim.opt_local.colorcolumn = get_colorcolumn()
 end
+
+--- Yank the path of the current Lua module.
+---@return nil
+local function yank_module_path()
+  local path = vim.fn.expand("%")
+  path = vim.fn.fnamemodify(path, ":.")
+
+  path = path:gsub("^lua/", "") -- Remove the "lua/" prefix if it exists
+  path = path:gsub("%.lua$", "") -- Remove the ".lua" extension
+  path = path:gsub(".init$", "") -- Remove the ".init" suffix (in case in an init.lua file)
+  path = path:gsub("/", ".") -- Replace slashes with dots
+
+  vim.fn.setreg('"', path)
+  vim.notify('Yanked "' .. path .. '"')
+end
+
+vim.keymap.set("n", "<leader>ym", yank_module_path, { buffer = true, desc = "[Y]ank: current Lua [M]odule" })
--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -87,3 +87,19 @@ end
 if not vim.g.disable_colorcolumn then
   vim.opt_local.colorcolumn = get_colorcolumn()
 end
+
+--- Yank the path of the current Python module.
+---@return nil
+local function yank_module_path()
+  local path = vim.fn.expand("%")
+  path = vim.fn.fnamemodify(path, ":.")
+
+  path = path:gsub("%.py$", "") -- Remove the ".py" extension
+  path = path:gsub(".__init__$", "") -- Remove the ".__init__" suffix (in case in an __init__.py file)
+  path = path:gsub("/", ".") -- Replace slashes with dots
+
+  vim.fn.setreg('"', path)
+  vim.notify('Yanked "' .. path .. '"')
+end
+
+vim.keymap.set("n", "<leader>ym", yank_module_path, { buffer = true, desc = "[Y]ank: current Python [M]odule" })
