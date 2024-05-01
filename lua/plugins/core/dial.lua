496e5268b965027b054562d765b21fc35fc98a20
clementjumel <clement.jumel@gmail.com>
2024-05-02 00:12:48 +0200

    ✨ Some improvements in Dial config

--- lua/plugins/core/dial.lua
+++ lua/plugins/core/dial.lua
@@ -5,8 +5,10 @@
 return {
   "monaqa/dial.nvim",
   keys = {
-    { "<C-a>", function() require("dial.map").manipulate("increment", "normal") end, desc = "Increment" },
-    { "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end, desc = "Decrement" },
+    { "<C-a>", function() require("dial.map").manipulate("increment", "normal") end, mode = "n", desc = "Increment" },
+    { "<C-a>", function() require("dial.map").manipulate("increment", "visual") end, mode = "v", desc = "Increment" },
+    { "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end, mode = "n", desc = "Decrement" },
+    { "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end, mode = "v", desc = "Decrement" },
   },
   config = function(_, _)
     local augend = require("dial.augend")
@@ -14,27 +16,25 @@ return {
 
     config.augends:register_group({
       default = {
-        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" } }),
-        augend.constant.new({ elements = { "true", "false" } }),
-        augend.constant.new({ elements = { "True", "False" } }),
+        augend.integer.alias.decimal,
+        augend.constant.alias.bool,
       },
     })
 
     config.augends:on_filetype({
       lua = {
-        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" } }),
-        augend.constant.new({ elements = { "true", "false" } }),
+        augend.integer.alias.decimal,
+        augend.constant.alias.bool,
         augend.constant.new({ elements = { "==", "~=" }, word = false }),
         augend.constant.new({ elements = { "if", "elseif", "else" } }),
       },
       markdown = {
-        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" } }),
-        augend.constant.new({ elements = { "true", "false" } }),
-        augend.constant.new({ elements = { "True", "False" } }),
-        augend.constant.new({ elements = { "🎯", "✅", "❌" } }),
+        augend.integer.alias.decimal,
+        augend.misc.alias.markdown_header,
+        augend.constant.new({ elements = { "🎯", "⏳", "✅", "❌" } }),
       },
       python = {
-        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" } }),
+        augend.integer.alias.decimal,
         augend.constant.new({ elements = { "True", "False" } }),
         augend.constant.new({ elements = { "==", "!=" } }),
         augend.constant.new({ elements = { "if", "elif", "else" } }),
