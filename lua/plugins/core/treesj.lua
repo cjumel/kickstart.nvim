182ed554a2ebdf402cfe00a6cc501f72a2edc54d
clementjumel <clement.jumel@gmail.com>
2024-05-01 20:58:49 +0200

    ♻️ Improve TreeSJ config writting

--- lua/plugins/core/treesj.lua
+++ lua/plugins/core/treesj.lua
@@ -1,21 +1,15 @@
 -- TreeSJ
 --
--- Neovim plugin for splitting/joining blocks of code like arrays, hashes, statements, objects, dictionaries, etc.
+-- TreeSJ is a small plugin for easily splitting/joining blocks of code like lists, dictionnaries, arrays, etc. using
+-- Treesitter. Since it uses Treesitter, it handles many cases better than the buitlin `J` command, for instance, but
+-- on the other hand, it doesn't work when Treesitter is not available, or on comment blocks for instance.
 
 return {
   "Wansmer/treesj",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
-  keys = {
-    {
-      "S",
-      function() require("treesj").toggle() end,
-      desc = "Split node toggle",
-    },
-  },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  keys = { { "S", function() require("treesj").toggle() end, desc = "Split/join node" } }, -- Like "J" (join lines)
   opts = {
     use_default_keymaps = false,
-    max_join_length = 200,
+    max_join_length = 200, -- Set a high limit to be able to join over regular line-length limits, but not too high
   },
 }
