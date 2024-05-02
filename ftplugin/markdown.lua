fac564489efb484eded7e7c440d286c4a1d5dd26
clementjumel <clement.jumel@gmail.com>
2024-05-02 13:35:39 +0200

    ✨ Keymap to search for todo-items in Markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -61,9 +61,17 @@ end
 
 local utils = require("utils")
 
--- This keymap also catches comments with `#` in code blocks (e.g. in Python), but this is good enough
+local title_pattern = "^#" -- Also catches comments with `#` in code blocks (e.g. in Python), but this is good enough
+local todo_pattern = "- 🎯 \\|- ⏳ " -- Don't look for todo-items done or cancelled
+
 utils.keymap.set_move_pair(
-  { "[t", "]t" }, -- Correspond to todo-comments in other filetypes, but not used in Markdown
-  { function() vim.fn.search("^#") end, function() vim.fn.search("^#", "b") end },
+  { "[[", "][" }, -- Correspond to classes in other filetypes, but not in Markdown
+  { function() vim.fn.search(title_pattern) end, function() vim.fn.search(title_pattern, "b") end },
   { { desc = "Next title", buffer = true }, { desc = "Previous title", buffer = true } }
+)
+
+utils.keymap.set_move_pair(
+  { "[t", "]t" }, -- Correpond to todo-comments in other filetypes, but not in Markdown
+  { function() vim.fn.search(todo_pattern) end, function() vim.fn.search(todo_pattern, "b") end },
+  { { desc = "Next todo item", buffer = true }, { desc = "Previous todo item", buffer = true } }
 )
