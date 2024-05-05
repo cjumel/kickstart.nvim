3f78cf7849e9919e9e4d9aa87fca761c4f410121
clementjumel <clement.jumel@gmail.com>
2024-05-05 19:50:15 +0200

    🩹 Improve Oil snippets conditions

--- lua/plugins/core/luasnip/snippets/oil/keywords.lua
+++ lua/plugins/core/luasnip/snippets/oil/keywords.lua
@@ -9,22 +9,20 @@ local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
 
-local is_in_empty_line = custom_conds.line_begin * show_conds.line_end
-
 return {
-  s({ trig = ".json", show_condition = is_in_empty_line }, { i(1, "name"), t(".json") }),
-  s({ trig = ".json", show_condition = -custom_conds.line_begin }, { t(".json") }),
+  s({ trig = ".json", show_condition = custom_conds.line_begin * show_conds.line_end }, { i(1, "name"), t(".json") }),
+  s({ trig = ".json", show_condition = -custom_conds.line_begin * show_conds.line_end }, { t(".json") }),
 
-  s({ trig = ".lua", show_condition = is_in_empty_line }, {
+  s({ trig = ".lua", show_condition = custom_conds.line_begin * show_conds.line_end }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".lua") }),
       t("init.lua"),
       t("temp.lua"),
     }),
   }),
-  s({ trig = ".lua", show_condition = -is_in_empty_line }, { t(".lua") }),
+  s({ trig = ".lua", show_condition = -custom_conds.line_begin * show_conds.line_end }, { t(".lua") }),
 
-  s({ trig = ".md", show_condition = is_in_empty_line }, {
+  s({ trig = ".md", show_condition = custom_conds.line_begin * show_conds.line_end }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".md") }),
       t("README.md"),
@@ -32,31 +30,36 @@ return {
       t("TODO.md"),
     }),
   }),
-  s({ trig = ".md", show_condition = -custom_conds.line_begin }, { t(".md") }),
+  s({ trig = ".md", show_condition = -custom_conds.line_begin * show_conds.line_end }, { t(".md") }),
 
-  s({ trig = ".py", show_condition = is_in_empty_line }, {
+  s({ trig = ".py", show_condition = custom_conds.line_begin * show_conds.line_end }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".py") }),
       t("__init__.py"),
       t("temp.py"),
     }),
   }),
-  s({ trig = ".py", show_condition = -is_in_empty_line }, { t(".py") }),
+  s({ trig = ".py", show_condition = -custom_conds.line_begin * show_conds.line_end }, { t(".py") }),
 
-  s({ trig = ".sh", show_condition = is_in_empty_line }, { i(1, "name"), t(".sh") }),
-  s({ trig = ".sh", show_condition = -custom_conds.line_begin }, { t(".sh") }),
+  s({ trig = ".sh", show_condition = custom_conds.line_begin * show_conds.line_end }, { i(1, "name"), t(".sh") }),
+  s({ trig = ".sh", show_condition = -custom_conds.line_begin * show_conds.line_end }, { t(".sh") }),
 
-  s({ trig = ".toml", show_condition = is_in_empty_line }, { i(1, "name"), t(".toml") }),
-  s({ trig = ".toml", show_condition = -custom_conds.line_begin }, { t(".toml") }),
+  s({ trig = ".toml", show_condition = custom_conds.line_begin * show_conds.line_end }, { i(1, "name"), t(".toml") }),
+  s({ trig = ".toml", show_condition = -custom_conds.line_begin * show_conds.line_end }, { t(".toml") }),
 
-  s({ trig = ".yaml", show_condition = is_in_empty_line }, {
+  s({ trig = ".yaml", show_condition = custom_conds.line_begin * show_conds.line_end }, {
     c(1, {
       sn(nil, { i(1, "name"), t(".yaml") }),
       sn(nil, { i(1, "name"), t(".yml") }),
     }),
   }),
-  s({ trig = ".yaml", show_condition = -custom_conds.line_begin }, { c(1, { t(".yaml"), t(".yml") }) }),
+  s({ trig = ".yaml", show_condition = -custom_conds.line_begin * show_conds.line_end }, {
+    c(1, {
+      t(".yaml"),
+      t(".yml"),
+    }),
+  }),
 
-  s({ trig = ".zsh", show_condition = is_in_empty_line }, { i(1, "name"), t(".zsh") }),
-  s({ trig = ".zsh", show_condition = -custom_conds.line_begin }, { t(".zsh") }),
+  s({ trig = ".zsh", show_condition = custom_conds.line_begin * show_conds.line_end }, { i(1, "name"), t(".zsh") }),
+  s({ trig = ".zsh", show_condition = -custom_conds.line_begin * show_conds.line_end }, { t(".zsh") }),
 }
