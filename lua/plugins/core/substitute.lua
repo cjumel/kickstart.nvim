fddcf87cd91393cec9f0c8dd77e758717cc2ead0
clementjumel <clement.jumel@gmail.com>
2024-05-14 14:16:16 +0200

    🩹 Fix & complete substitute operators

--- lua/plugins/core/substitute.lua
+++ lua/plugins/core/substitute.lua
@@ -16,22 +16,29 @@ return {
 
     return {
       { "gp", substitute.operator, desc = "Paste over" },
-      { "gpc", substitute.line, desc = "Paste over current line" },
       { "gp", substitute.visual, mode = "x", desc = "Paste over" },
+      { "gpc", substitute.line, desc = "Paste over current line" },
       { "go", function() range.operator(overwrite_range_opts) end, desc = "Overwrite" },
+      { "go", function() range.visual(overwrite_range_opts) end, mode = "x", desc = "Overwrite" },
       {
         "goc",
         function() range.operator(vim.tbl_deep_extend("force", overwrite_range_opts, { range = "%" })) end,
         desc = "Overwrite in current buffer",
       },
-      { "go", function() range.visual_range(overwrite_range_opts) end, mode = "x", desc = "Overwrite" },
+      {
+        "goc",
+        function() range.visual(vim.tbl_deep_extend("force", overwrite_range_opts, { range = "%" })) end,
+        mode = "x",
+        desc = "Overwrite in current buffer",
+      },
       { "gs", range.operator, desc = "Substitute" },
+      { "gs", range.visual, mode = "x", desc = "Substitute" },
       { "gsc", function() range.operator({ range = "%" }) end, desc = "Substitute in current buffer" },
-      { "gs", range.visual_range, mode = "x", desc = "Substitute" },
+      { "gsc", function() range.visual({ range = "%" }) end, mode = "x", desc = "Substitute in current buffer" },
       -- I don't use builtin "ge", it can be replaced with Hop's equivalent or with "F" so let's remap it here
       { "ge", exchange.operator, desc = "Exchange" },
-      { "gec", exchange.line, desc = "Exchange current line" },
       { "ge", exchange.visual, mode = "x", desc = "Exchange" },
+      { "gec", exchange.line, desc = "Exchange current line" },
     }
   end,
   opts = {},
