085316a9ff2d362ae361b48bd35d92f31962eb69
clementjumel <clement.jumel@gmail.com>
2024-05-17 11:58:06 +0200

    🎨 Re-organize Lazy setup

--- init.lua
+++ init.lua
@@ -1,9 +1,46 @@
--- Set <space> as the leader key
--- Must happen before plugins are required (otherwise wrong leader will be used)
+-- Set <Space> as the leader key
+-- This must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- [[ Install `lazy.nvim` plugin manager & configure plugins ]]
-require("lazy-plugins")
+-- Install `lazy.nvim` plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup `lazy.nvim` plugins and options
+local plugins = {
+  -- Color schemes plugins define the highlight groups used in the editor
+  { import = "plugins.colorschemes" },
+
+  -- Core plugins bring features to edit buffers efficiently (text objects, motions, LSP,
+  -- formatters, etc.)
+  { import = "plugins.core" },
+
+  -- Navigation plugins enable fast navigation and exploration between files (file fuzzy finding,
+  -- file explorer, etc.) but also various navigation interfaces
+  { import = "plugins.navigation" },
+
+  -- Tools plugins bring extra features not related directly to editing buffer (DAP, database
+  -- explorer, etc.)
+  { import = "plugins.tools" },
+
+  -- User interface plugins bring various visual improvements (colorscheme, statusline, etc.)
+  { import = "plugins.ui" },
+}
+local opts = {
+  ui = { border = "rounded" }, -- Improve visibility with transparent background
+  change_detection = { enabled = false },
+}
+require("lazy").setup(plugins, opts)
 
 -- vim: ts=2 sts=2 sw=2 et
