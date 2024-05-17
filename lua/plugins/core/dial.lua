6e8869035c034863f2ce139af6529c4178745d33
clementjumel <clement.jumel@gmail.com>
2024-05-17 15:08:11 +0200

    🩹 Change digit increment/decrement behavior

--- lua/plugins/core/dial.lua
+++ lua/plugins/core/dial.lua
@@ -4,37 +4,40 @@
 
 return {
   "monaqa/dial.nvim",
-  keys = {
-    { "<C-a>", function() require("dial.map").manipulate("increment", "normal") end, mode = "n", desc = "Increment" },
-    { "<C-a>", function() require("dial.map").manipulate("increment", "visual") end, mode = "v", desc = "Increment" },
-    { "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end, mode = "n", desc = "Decrement" },
-    { "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end, mode = "v", desc = "Decrement" },
-  },
+  keys = function()
+    local map = require("dial.map")
+    return {
+      { "<C-a>", function() map.manipulate("increment", "normal") end, mode = "n", desc = "Increment" },
+      { "<C-a>", function() map.manipulate("increment", "visual") end, mode = "v", desc = "Increment" },
+      { "<C-x>", function() map.manipulate("decrement", "normal") end, mode = "n", desc = "Decrement" },
+      { "<C-x>", function() map.manipulate("decrement", "visual") end, mode = "v", desc = "Decrement" },
+    }
+  end,
   config = function(_, _)
     local augend = require("dial.augend")
     local config = require("dial.config")
 
     config.augends:register_group({
       default = {
-        augend.integer.alias.decimal,
-        augend.constant.alias.bool,
+        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, word = false }),
+        augend.constant.new({ elements = { "true", "false" } }),
       },
     })
 
     config.augends:on_filetype({
       lua = {
-        augend.integer.alias.decimal,
-        augend.constant.alias.bool,
+        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, word = false }),
+        augend.constant.new({ elements = { "true", "false" } }),
         augend.constant.new({ elements = { "==", "~=" }, word = false }),
         augend.constant.new({ elements = { "if", "elseif", "else" } }),
       },
       markdown = {
-        augend.integer.alias.decimal,
-        augend.misc.alias.markdown_header,
+        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, word = false }),
         augend.constant.new({ elements = { "🎯", "⏳", "✅", "❌" } }),
+        augend.misc.alias.markdown_header,
       },
       python = {
-        augend.integer.alias.decimal,
+        augend.constant.new({ elements = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, word = false }),
         augend.constant.new({ elements = { "True", "False" } }),
         augend.constant.new({ elements = { "==", "!=" }, word = false }),
         augend.constant.new({ elements = { "if", "elif", "else" } }),
