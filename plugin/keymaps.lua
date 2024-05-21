0723f02dee48ad05866f0dec1ec26efcbfedf902
clementjumel <clement.jumel@gmail.com>
2024-05-21 17:41:55 +0200

    🔥 Remove <C-i>/<C-o> visual mode mappings

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -74,10 +74,6 @@ vim.keymap.set("v", "<C-c>", "<cmd>ClearWindow<CR>", { desc = "Clear" })
 vim.keymap.set("i", "<C-c>", "<cmd>ClearAll<CR>", { desc = "Clear" })
 vim.keymap.set("c", "<C-c>", "<cmd>ClearInsertMode<CR>", { desc = "Clear" }) -- Don't clean the cmdline popup itself
 
--- Use <C-i> to indent in visual mode, & <C-o> because it's next to it
-vim.keymap.set("v", "<C-i>", ">gv", { desc = "Indent selection" })
-vim.keymap.set("v", "<C-o>", "<gv", { desc = "Unindent selection" })
-
 -- Shortcuts for quicker access to some of the main registers
 vim.keymap.set({ "n", "v" }, "+", '"+', { desc = "System clipboard register" })
 vim.keymap.set({ "n", "v" }, "_", '"_', { desc = "Black hole register" })
