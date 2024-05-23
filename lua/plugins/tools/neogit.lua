623c86d1d14c596996091d36136b3b222c968a9a
clementjumel <clement.jumel@gmail.com>
2024-05-23 20:10:58 +0200

    ⬆️ Upgrade Neogit & revamp config
    This is a huge upgrade of Neogit & the plugin stopped working,
    so this fixes everything. It especially gives up the custom
    keymaps because I couldn't get them to work again.

--- lua/plugins/tools/neogit.lua
+++ lua/plugins/tools/neogit.lua
@@ -13,6 +13,7 @@ return {
   },
   init = function()
     -- Customize the Neogit command to close zen-mode before opening Neogit
+    -- Let's create a user command so that it can be used from Gitsigns as well
     vim.api.nvim_create_user_command("NeogitCustom", function()
       -- If zen-mode is loaded, close it before counting windows as it will be closed anyway & might mess things up
       local zen_mode = package.loaded["zen-mode"]
@@ -20,86 +21,13 @@ return {
         zen_mode.close()
       end
       require("neogit").open()
-    end, {})
+    end, { desc = "Open Neogit" })
   end,
   keys = { { "<leader>gn", "<cmd>NeogitCustom<CR>", desc = "[G]it: [N]eogit" } },
   opts = {
-    use_default_keymaps = false,
-    disable_hint = true,
-    kind = "tab",
-    commit_view = { kind = "tab" },
-    commit_editor = { kind = "split" },
-    rebase_editor = { kind = "split" },
-    mappings = {
-      commit_editor = {
-        ["q"] = "Close",
-        ["<c-c><c-c>"] = "Submit",
-        ["<c-c><c-k>"] = "Abort",
-        ["<m-p>"] = "PrevMessage",
-        ["<m-n>"] = "NextMessage",
-        ["<m-r>"] = "ResetMessage",
-      },
-      rebase_editor = {
-        ["p"] = "Pick",
-        ["r"] = "Reword",
-        ["e"] = "Edit",
-        ["s"] = "Squash",
-        ["f"] = "Fixup",
-        ["x"] = "Execute",
-        ["d"] = "Drop",
-        ["b"] = "Break",
-        ["q"] = "Close",
-        ["<cr>"] = "OpenCommit",
-        ["gk"] = "MoveUp",
-        ["gj"] = "MoveDown",
-        ["<c-c><c-c>"] = "Submit",
-        ["<c-c><c-k>"] = "Abort",
-      },
-      finder = {
-        ["<cr>"] = "Select",
-        ["<c-c>"] = "Close",
-        ["<esc>"] = "Close",
-        ["<c-n>"] = "Next",
-        ["<c-p>"] = "Previous",
-        ["<down>"] = "Next",
-        ["<up>"] = "Previous",
-        ["<tab>"] = "MultiselectToggleNext",
-        ["<s-tab>"] = "MultiselectTogglePrevious",
-        ["<c-j>"] = "NOP",
-      },
-      popup = {
-        ["gb"] = "BranchPopup",
-        ["gc"] = "CommitPopup",
-        ["gd"] = "DiffPopup",
-        ["gf"] = "FetchPopup",
-        ["gl"] = "LogPopup",
-        ["gsh"] = "StashPopup",
-        ["gpl"] = "PullPopup",
-        ["gps"] = "PushPopup",
-        ["grb"] = "RebasePopup",
-        ["grs"] = "ResetPopup",
-        ["grv"] = "RevertPopup",
-        ["gw"] = "WorktreePopup",
-        ["?"] = "HelpPopup",
-      },
-      status = {
-        ["q"] = "Close",
-        ["s"] = "Stage",
-        ["S"] = "StageUnstaged",
-        ["<C-s>"] = "StageAll",
-        ["u"] = "Unstage",
-        ["U"] = "UnstageStaged",
-        ["x"] = "Discard",
-        ["$"] = "CommandHistory",
-        ["#"] = "Console",
-        ["<CR>"] = "GoToFile",
-        ["<Tab>"] = "Toggle",
-        ["<C-r>"] = "RefreshBuffer",
-        ["<C-v>"] = "VSplitOpen",
-        ["<C-x>"] = "SplitOpen",
-        [","] = "GoToNextHunkHeader",
-        [";"] = "GoToPreviousHunkHeader",
-      },
-    },
+    disable_hint = true, -- Don't always show help hint
+    use_default_keymaps = true, -- Implementing custom keymaps make the plugin break often when updating it
+    commit_view = { kind = "tab" }, -- Use "tab" view instead of "split", to better visualize the whole commit
+    rebase_editor = { kind = "split" }, -- Force the "split" below (instead of "auto")
   },
 }
