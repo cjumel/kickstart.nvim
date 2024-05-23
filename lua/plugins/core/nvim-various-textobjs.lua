53f8baa803ab6e253ced895fa6f32b2b1299b48d
clementjumel <clement.jumel@gmail.com>
2024-05-23 12:33:36 +0200

    ⬆️ Upgrade nvim-various-textobjs & fix deprecations

--- lua/plugins/core/nvim-various-textobjs.lua
+++ lua/plugins/core/nvim-various-textobjs.lua
@@ -8,23 +8,21 @@ return {
     local textobjs = require("various-textobjs")
     local charwise_textobjs = require("various-textobjs.charwise-textobjs")
 
-    -- Implement a smarter "gx" keymap for normal mode, with nvim-various-textobjs lookahead
-    -- This is adapted from: https://github.com/chrisgrieser/nvim-various-textobjs?tab=readme-ov-file#smarter-gx
+    -- Implement smarter versions of builtin `gx` keymap for normal mode, but specific to URLs and with lookahead or
+    -- buffer-wide search
+    -- This is taken from https://github.com/chrisgrieser/nvim-various-textobjs?tab=readme-ov-file#smarter-gx
 
-    --- Open the provided URL in the system's default application.
-    ---@param url string The URL to open.
+    --- Open the URL under the cursor or with lookahead.
     ---@return nil
-    local function open_url(url)
-      local opener
-      if vim.fn.has("macunix") == 1 then
-        opener = "open"
-      elseif vim.fn.has("linux") == 1 then
-        opener = "xdg-open"
-      elseif vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
-        opener = "start"
+    local function open_cursor_url()
+      textobjs.url() -- Select the URL under the cursor or with lookahead
+      local url_found = vim.fn.mode():find("v")
+      if not url_found then
+        return
       end
-      local open_command = string.format("%s '%s' >/dev/null 2>&1", opener, url)
-      vim.fn.system(open_command)
+      vim.cmd.normal({ '"zy', bang = true }) -- Retrieve the URL with the z-register as intermediary
+      local url = vim.fn.getreg("z")
+      vim.ui.open(url)
     end
 
     --- Find all URLs in the current buffer and prompt the user to select one to open.
@@ -35,34 +33,20 @@ return {
       local buf_text = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
       local urls = {}
       for url in buf_text:gmatch(url_pattern) do
-        if not vim.tbl_contains(urls, url) then
+        if not vim.tbl_contains(urls, url) then -- Avoid duplicates
           table.insert(urls, url)
         end
       end
       if #urls == 0 then
         return
       end
+
       -- Select an URL & open it
       vim.ui.select(urls, { prompt = "Select URL:" }, function(choice)
         if choice then
-          open_url(choice)
+          vim.ui.open(choice)
         end
       end)
-    end
-
-    --- Open an URL under the cursor or with lookahead using the system's default application.
-    ---@return nil
-    local function smart_gx()
-      textobjs.url() -- Select an URL
-      local url_found = vim.fn.mode():find("v")
-      if url_found then
-        -- Retrieve the URL with the z-register as intermediary
-        vim.cmd.normal({ '"zy', bang = true })
-        local url = vim.fn.getreg("z")
-        open_url(url)
-      else
-        open_any_url()
-      end
     end
 
     return {
@@ -74,7 +58,6 @@ return {
       { "is", function() textobjs.subword("inner") end, mode = { "x", "o" }, desc = "inner subword" },
       { "C", textobjs.toNextClosingBracket, mode = { "x", "o" }, desc = "Next right bracket" },
       { "Q", textobjs.toNextQuotationMark, mode = { "x", "o" }, desc = "Next quotation mark" },
-      { "gm", textobjs.multiCommentedLines, mode = { "x", "o" }, desc = "Multi-line comment" },
       { "gG", textobjs.entireBuffer, mode = { "x", "o" }, desc = "Entire buffer" },
       { "g$", textobjs.nearEoL, mode = { "x", "o" }, desc = "Near end of line" },
       { "-", function() textobjs.lineCharacterwise("inner") end, mode = { "x", "o" }, desc = "Line characterwise" },
@@ -84,7 +67,7 @@ return {
       { "ik", function() textobjs.key("inner") end, mode = { "x", "o" }, desc = "inner key-value pair key" },
       { "gx", textobjs.url, mode = { "x", "o" }, desc = "URL" },
       -- [[ Other ]]
-      { "gx", smart_gx, mode = { "n" }, desc = "Open URL under cursor" },
+      { "gx", open_cursor_url, mode = { "n" }, desc = "Open URL under cursor" },
       { "gX", open_any_url, mode = { "n" }, desc = "Open any URL in buffer" },
     }
   end,
