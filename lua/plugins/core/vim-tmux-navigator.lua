3f87622d9436f72e819be65dff1ead8b12f45dea
clementjumel <clement.jumel@gmail.com>
2024-05-24 15:27:20 +0200

    🩹 Improve vim-tmux-navigator config/documentation

--- lua/plugins/core/vim-tmux-navigator.lua
+++ lua/plugins/core/vim-tmux-navigator.lua
@@ -1,34 +1,14 @@
 -- vim-tmux-navigator
 --
--- Enable to use control + h/j/k/l seamlessly to navigate between neovim windows and tmux panes
--- when running neovim within tmux.
+-- This plugin enables seamless navigation between Tmux panes and (Neo)Vim splits through a handful of simple commands.
+-- It is a must for anyone using Vim or Neovim inside Tmux sessions.
 
 return {
   "christoomey/vim-tmux-navigator",
   keys = {
-    {
-      "<C-h>",
-      "<cmd> TmuxNavigateLeft <CR>",
-      mode = { "n", "t" },
-      desc = "Window left",
-    },
-    {
-      "<C-l>",
-      "<cmd> TmuxNavigateRight <CR>",
-      mode = { "n", "t" },
-      desc = "Window right",
-    },
-    {
-      "<C-j>",
-      "<cmd> TmuxNavigateDown <CR>",
-      mode = { "n", "t" },
-      desc = "Window down",
-    },
-    {
-      "<C-k>",
-      "<cmd> TmuxNavigateUp <CR>",
-      mode = { "n", "t" },
-      desc = "Window up",
-    },
+    { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", mode = { "n", "t" }, desc = "Window left" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<CR>", mode = { "n", "t" }, desc = "Window right" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<CR>", mode = { "n", "t" }, desc = "Window down" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<CR>", mode = { "n", "t" }, desc = "Window up" },
   },
 }
