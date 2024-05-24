923f46db56307a73f190c5acc05f5256bbff3308
clementjumel <clement.jumel@gmail.com>
2024-05-24 12:36:56 +0200

    🔥 Remove useless keymap deactivations
    I think these keymaps are useless, so let's get rid of them.

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -3,12 +3,6 @@ local utils = require("utils")
 -- [[ Disable builtin keymaps ]]
 -- Disable some builtin keymaps, because they are useless or they are conflicting with other keymaps
 
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
--- Disable builtin auto-completion keymaps (avoid writting letters when calling them and nvim-cmp is not enabled)
-vim.keymap.set("i", "<C-n>", "<Nop>", { silent = true })
-vim.keymap.set("i", "<C-p>", "<Nop>", { silent = true })
-
 -- Disable builtin navigation keymaps
 -- Some keymaps can't be properly disabled; in that case, let's give them no effect & make Which-Key ignore them
 vim.keymap.set("n", "[(", function() end, { desc = "which_key_ignore" })
