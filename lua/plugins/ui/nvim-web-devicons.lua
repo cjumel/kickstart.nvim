1c9a8233f196c0584a108610042c45a5fb472b9f
clementjumel <clement.jumel@gmail.com>
2024-05-25 19:48:51 +0200

    ⬆️ Upgrade nvim-web-devicons
    This fixes the broken custom icons.

--- lua/plugins/ui/nvim-web-devicons.lua
+++ lua/plugins/ui/nvim-web-devicons.lua
@@ -3,9 +3,6 @@
 -- A Lua fork of vim-devicons, providing icons as well as colors for each icon. This plugin is used by many other
 -- plugins, to provide user-friendly and customizable icons.
 
--- BUG: custom icons (using `set_icon`) stopped working I believe when upgrading nvim to 0.10
---  This is likely the same issue as https://github.com/nvim-tree/nvim-web-devicons/issues/465
-
 return {
   "nvim-tree/nvim-web-devicons",
   lazy = true, -- Dependency of several plugins
