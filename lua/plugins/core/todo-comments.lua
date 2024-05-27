91366c4e35a72237528de48d86f8c0ca264bfa2f
clementjumel <clement.jumel@gmail.com>
2024-05-27 11:12:21 +0200

    🩹 Simplify todo-comment keymaps

--- lua/plugins/core/todo-comments.lua
+++ lua/plugins/core/todo-comments.lua
@@ -10,49 +10,18 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   event = { "BufNewFile", "BufReadPre" },
-  keys = function()
-    --- Open todo-comments in the current project or in the current directory if an Oil buffer is open, using Telescope
-    --- or Trouble.
-    ---@param opts table|nil Options to open todo-comments.
-    ---@return nil
-    local function open_tdc(opts)
-      opts = opts or {}
-      local only_todos = opts.only_todos or false
-      local opener = opts.opener or "Telescope"
-
-      local cmd
-      if opener == "Trouble" then
-        if package.loaded.trouble == nil then -- Load Trouble if not already loaded
-          require("trouble")
-        end
-        cmd = "TodoTrouble"
-      else
-        if package.loaded.telescope == nil then -- Load Telescope if not already loaded
+  keys = {
+    {
+      "<leader>ft",
+      function()
+        if package.loaded.telescope == nil then -- Lazy load telescope if needed
           require("telescope")
         end
-        cmd = "TodoTelescope previewer=false"
-      end
-      if only_todos then
-        cmd = cmd .. " keywords=TODO"
-      end
-      if vim.bo.filetype == "oil" then
-        cmd = cmd .. " cwd=" .. package.loaded.oil.get_current_dir() -- Oil is already loaded if in an Oil buffer
-      end
-
-      vim.cmd(cmd)
-    end
-
-    return {
-      { "<leader>ft", function() open_tdc({ only_todos = true }) end, desc = "[F]ind: [T]odo-comments (only TODOs)" },
-      { "<leader>fT", function() open_tdc() end, desc = "[F]ind: [T]odo-comments (all)" },
-      {
-        "<leader>xt",
-        function() open_tdc({ opener = "Trouble", only_todos = true }) end,
-        desc = "Trouble: [T]odo-comments (only TODOs)",
-      },
-      { "<leader>xT", function() open_tdc({ opener = "Trouble" }) end, desc = "Trouble: [T]odo-comments (all)" },
-    }
-  end,
+        vim.cmd("TodoTelescope previewer=false")
+      end,
+      desc = "[F]ind: [T]odo-comments",
+    },
+  },
   opts = {
     -- Include hidden files when searching for todo comments
     search = { args = { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--hidden" } },
