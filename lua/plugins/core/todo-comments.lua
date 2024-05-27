f6fb44cd7f1f356133a31c5934e9e7dc2c24e74b
clementjumel <clement.jumel@gmail.com>
2024-05-27 13:19:31 +0200

    🩹 Simplify further todo-comment keymaps

--- lua/plugins/core/todo-comments.lua
+++ lua/plugins/core/todo-comments.lua
@@ -1,10 +1,10 @@
--- todo-comments
+-- Todo Comments
 --
--- todo-comments is a plugin to highlight and search for todo comments like TODO, HACK, BUG in the code base. It is
--- very convenient to document directly in the code the next steps to do, or to document long-term issues left for
--- the future, like unresolved bugs or performance issues. Besides, it works very nicely with other plugins, like
--- Telescope or Trouble, to navigate between todo-comments, or code snippets (e.g. Luasnip), to define custom
--- todo-comment snippets to blazingly-fast add them to the code.
+-- Todo Comments is a plugin to highlight and search for todo-comments (like `TODO`, `HACK` or `BUG`), in the code
+-- base. It is very convenient to document directly in the code the next steps to do, or to document long-term issues
+-- left for the future, like unresolved bugs or performance issues. Besides, it works very nicely with other plugins,
+-- like Telescope or Trouble, to navigate between todo-comments, or code snippets (e.g. Luasnip), to define custom
+-- todo-comment snippets.
 
 return {
   "folke/todo-comments.nvim",
@@ -26,16 +26,4 @@ return {
     -- Include hidden files when searching for todo comments
     search = { args = { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--hidden" } },
   },
-  config = function(_, opts)
-    local tdc = require("todo-comments")
-
-    local utils = require("utils")
-
-    tdc.setup(opts)
-
-    utils.keymap.set_move_pair({ "[t", "]t" }, {
-      tdc.jump_next,
-      tdc.jump_prev,
-    }, { { desc = "Next todo-comment" }, { desc = "Previous todo-comment" } })
-  end,
 }
