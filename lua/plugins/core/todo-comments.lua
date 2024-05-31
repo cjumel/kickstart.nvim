cd3b66750b935ef946d1bc7aa536e42547220c60
clementjumel <clement.jumel@gmail.com>
2024-05-31 11:19:36 +0200

    ⏪️ Re-implement some todo-comments keymaps

--- lua/plugins/core/todo-comments.lua
+++ lua/plugins/core/todo-comments.lua
@@ -14,16 +14,37 @@ return {
     {
       "<leader>ft",
       function()
-        if package.loaded.telescope == nil then -- Lazy load telescope if needed
+        if package.loaded.telescope == nil then -- Lazy load Telescope if needed
           require("telescope")
         end
         vim.cmd("TodoTelescope previewer=false")
       end,
       desc = "[F]ind: [T]odo-comments",
     },
+    {
+      "<leader>xt",
+      function()
+        if package.loaded.trouble == nil then -- Lazy load Trouble if needed
+          require("trouble")
+        end
+        vim.cmd("TodoTrouble filter.buf=0")
+      end,
+      desc = "Trouble: [T]odo-comments",
+    },
   },
   opts = {
     -- Include hidden files when searching for todo comments
     search = { args = { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--hidden" } },
   },
+  config = function(_, opts)
+    local tdc = require("todo-comments")
+    local utils = require("utils")
+
+    tdc.setup(opts)
+
+    utils.keymap.set_move_pair({ "[t", "]t" }, {
+      tdc.jump_next,
+      tdc.jump_prev,
+    }, { { desc = "Next todo-comment" }, { desc = "Previous todo-comment" } })
+  end,
 }
