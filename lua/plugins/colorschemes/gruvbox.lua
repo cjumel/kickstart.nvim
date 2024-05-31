9cc02a68e1db52f04ad0ee3b480d2188528c1680
clementjumel <clement.jumel@gmail.com>
2024-05-31 18:22:59 +0200

    🔥 Remove Neogit highlight group fixes in colorschemes

--- lua/plugins/colorschemes/gruvbox.lua
+++ lua/plugins/colorschemes/gruvbox.lua
@@ -16,16 +16,5 @@ return {
 
     vim.o.background = theme.get_field("gruvbox", "background") or "dark"
     vim.cmd.colorscheme("gruvbox")
-
-    -- Fix inconsistent highlight groups for Neogit (foreground color is either white or red
-    -- for diffs when they are in context or not)
-    -- Default highlight groups for Neogit with this color scheme are:
-    --   DiffAddxxx guibg=#62693e (linked to NeogitDiffAdd)
-    --   NeogitDiffAddHighlight cterm= gui= guifg=#b8bb26 guibg=#62693e
-    --   DiffDelete guibg=#722529 (linked to NeogitDiffDelete)
-    --   NeogitDiffDeleteHighlight cterm= gui= guifg=#fb4934 guibg=#722529
-    -- Let's make the non-highlight groups same as the highlight ones
-    vim.api.nvim_command("hi NeogitDiffAdd guifg=#b8bb26 guibg=#62693e")
-    vim.api.nvim_command("hi NeogitDiffDelete guifg=#fb4934 guibg=#722529")
   end,
 }
