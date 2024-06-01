c64bc63b789158ddadbaf4cd83fe70020c7c41e3
clementjumel <clement.jumel@gmail.com>
2024-06-01 17:57:28 +0200

    ♻️ Rework Luasnip snippets for Oil

--- lua/plugins/core/luasnip/snippets/oil/keywords.lua
+++ lua/plugins/core/luasnip/snippets/oil/keywords.lua
@@ -1,74 +1,35 @@
+local custom_conditions = require("plugins.core.luasnip.conditions")
 local ls = require("luasnip")
 local ls_show_conditions = require("luasnip.extras.conditions.show")
 
-local custom_conditions = require("plugins.core.luasnip.conditions")
-
-local i = ls.insert_node
 local c = ls.choice_node
 local s = ls.snippet
-local sn = ls.snippet_node
 local t = ls.text_node
 
+local extension_condition = -custom_conditions.line_begin * ls_show_conditions.line_end
+local preset_file_condition = custom_conditions.line_begin * ls_show_conditions.line_end
+
 return {
-  s(
-    { trig = ".json", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end },
-    { i(1), t(".json") }
-  ),
-  s({ trig = ".json", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, { t(".json") }),
+  -- File extensions
+  s({ trig = ".json", show_condition = extension_condition }, { t(".json") }),
+  s({ trig = ".lua", show_condition = extension_condition }, { t(".lua") }),
+  s({ trig = ".md", show_condition = extension_condition }, { t(".md") }),
+  s({ trig = ".py", show_condition = extension_condition }, { t(".py") }),
+  s({ trig = ".sh", show_condition = extension_condition }, { t(".sh") }),
+  s({ trig = ".toml", show_condition = extension_condition }, { t(".toml") }),
+  s({ trig = ".txt", show_condition = extension_condition }, { t(".toml") }),
+  s({ trig = ".yaml", show_condition = extension_condition }, { c(1, { t(".yaml"), t(".yml") }) }),
+  s({ trig = ".zsh", show_condition = extension_condition }, { t(".zsh") }),
 
-  s({ trig = ".lua", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end }, {
-    c(1, {
-      sn(nil, { i(1), t(".lua") }),
-      t("init.lua"),
-      t("temp.lua"),
-    }),
-  }),
-  s({ trig = ".lua", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, { t(".lua") }),
-
-  s({ trig = ".md", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end }, {
-    c(1, {
-      sn(nil, { i(1), t(".md") }),
-      t("README.md"),
-      t("NOTES.md"),
-      t("TODO.md"),
-    }),
-  }),
-  s({ trig = ".md", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, { t(".md") }),
-
-  s({ trig = ".py", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end }, {
-    c(1, {
-      sn(nil, { i(1), t(".py") }),
-      t("__init__.py"),
-      t("temp.py"),
-    }),
-  }),
-  s({ trig = ".py", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, { t(".py") }),
-
-  s({ trig = ".sh", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end }, { i(1), t(".sh") }),
-  s({ trig = ".sh", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, { t(".sh") }),
-
-  s(
-    { trig = ".toml", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end },
-    { i(1), t(".toml") }
-  ),
-  s({ trig = ".toml", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, { t(".toml") }),
-
-  s({ trig = ".yaml", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end }, {
-    c(1, {
-      sn(nil, { i(1), t(".yaml") }),
-      sn(nil, { i(1), t(".yml") }),
-    }),
-  }),
-  s({ trig = ".yaml", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, {
-    c(1, {
-      t(".yaml"),
-      t(".yml"),
-    }),
-  }),
-
-  s(
-    { trig = ".zsh", show_condition = custom_conditions.line_begin * ls_show_conditions.line_end },
-    { i(1), t(".zsh") }
-  ),
-  s({ trig = ".zsh", show_condition = -custom_conditions.line_begin * ls_show_conditions.line_end }, { t(".zsh") }),
+  -- Preset files
+  -- Lua
+  s({ trig = "init.lua", show_condition = preset_file_condition }, { t("init.lua") }),
+  s({ trig = "temp.lua", show_condition = preset_file_condition }, { t("temp.lua") }),
+  -- Markdown
+  s({ trig = "NOTES.md", show_condition = preset_file_condition }, { t("NOTES.md") }),
+  s({ trig = "README.md", show_condition = preset_file_condition }, { t("README.md") }),
+  s({ trig = "TODO.md", show_condition = preset_file_condition }, { t("TODO.md") }),
+  -- Python
+  s({ trig = "__init__.py", show_condition = preset_file_condition }, { t("__init__.py") }),
+  s({ trig = "temp.py", show_condition = preset_file_condition }, { t("temp.py") }),
 }
