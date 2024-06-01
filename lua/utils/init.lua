1480824d7b07fba9ae0b7708459e19d30fc9a5ee
clementjumel <clement.jumel@gmail.com>
2024-06-01 23:34:41 +0200

    ⚰️ Remove unused utils

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-M.dir = require("utils.dir")
-M.file = require("utils.file")
 M.ftplugin = require("utils.ftplugin")
 M.keymap = require("utils.keymap")
 M.path = require("utils.path")
