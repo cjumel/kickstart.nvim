d2d6dd545baf4ea60a02e3113e0805b13ddb5965
clementjumel <clement.jumel@gmail.com>
2024-06-04 11:13:51 +0200

    ✨ Auto-command to clean unnamed buffers

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -40,10 +40,36 @@ vim.api.nvim_create_user_command("ClearAll", clear_all, { desc = "Clear for all 
 
 -- [[ Auto-commands ]]
 
+local augroup = vim.api.nvim_create_augroup("CustomCommands", { clear = true })
+
 -- Highlight yanked text
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank() end,
-  group = highlight_group,
+  group = augroup,
+  pattern = "*",
+})
+
+-- Clear unnamed buffers which are empty (created when no buffer is open)
+local function clear_unnamed_buffers()
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if
+      vim.api.nvim_buf_is_loaded(bufnr)
+      and vim.api.nvim_buf_get_name(bufnr) == ""
+      and vim.api.nvim_get_option_value("buftype", { buf = bufnr }) == ""
+    then
+      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false) -- Get all lines in the buffer
+      local total_characters = 0
+      for _, line in ipairs(lines) do
+        total_characters = total_characters + #line
+      end
+      if total_characters == 0 then
+        vim.api.nvim_buf_delete(bufnr, { force = true })
+      end
+    end
+  end
+end
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = clear_unnamed_buffers,
+  group = augroup,
   pattern = "*",
 })
