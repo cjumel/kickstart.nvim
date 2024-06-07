e5cc7e8bc82950a8027b9ec289baf926790d4c74
clementjumel <clement.jumel@gmail.com>
2024-06-07 11:05:28 +0200

    ✨ Custom mappings for Neogit

--- lua/plugins/tools/neogit.lua
+++ lua/plugins/tools/neogit.lua
@@ -26,14 +26,107 @@ return {
     },
   },
   opts = {
-    disable_hint = true, -- Don't always show help hint
-    -- FIXME: the plugin would be better with custom keymaps, but I have some issues implementing them:
-    --  - when disabling default mappings & copy pasting the default ones from the documentation, some issues
-    --    arrise (they can be adressed by looking into the error trace & the source code of the plugin)
-    --  - once the issues above are solved, committing stopped working; this is maybe linked to
-    --    https://github.com/NeogitOrg/neogit/issues/1342
-    use_default_keymaps = true, -- Implementing custom keymaps make the plugin break often when updating it
-    commit_view = { kind = "tab" }, -- Use "tab" view instead of "split", to decrease visual clutter
-    rebase_editor = { kind = "tab" }, -- Use "tab" view instead of "auto", to decrease visual clutter
+    disable_hint = true,
+    use_default_keymaps = false,
+    commit_view = { kind = "tab" }, -- Decrease visual clutter when using this view
+    rebase_editor = { kind = "tab" }, -- Decrease visual clutter when using this view
+    mappings = {
+      commit_editor = {
+        ["q"] = "Close",
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+        ["<c-c><c-p>"] = "PrevMessage", -- Forgotten in the documentation but mandatory to define
+        ["<c-c><c-n>"] = "NextMessage", -- Forgotten in the documentation but mandatory to define
+        ["<c-c><c-r>"] = "ResetMessage", -- Forgotten in the documentation but mandatory to define
+      },
+      commit_editor_I = {
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+      },
+      rebase_editor = {
+        ["p"] = "Pick",
+        ["r"] = "Reword",
+        ["e"] = "Edit",
+        ["s"] = "Squash",
+        ["f"] = "Fixup",
+        ["x"] = "Execute",
+        ["d"] = "Drop",
+        ["b"] = "Break",
+        ["q"] = "Close",
+        ["<cr>"] = "OpenCommit",
+        ["gk"] = "MoveUp",
+        ["gj"] = "MoveDown",
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+        ["[c"] = "OpenOrScrollUp",
+        ["]c"] = "OpenOrScrollDown",
+      },
+      rebase_editor_I = {
+        ["<c-c><c-c>"] = "Submit",
+        ["<c-c><c-k>"] = "Abort",
+      },
+      finder = {
+        ["<cr>"] = "Select",
+        ["<c-c>"] = "Close",
+        ["<esc>"] = "Close",
+        ["<c-n>"] = "Next",
+        ["<c-p>"] = "Previous",
+        ["<down>"] = "Next",
+        ["<up>"] = "Previous",
+        ["<tab>"] = "MultiselectToggleNext",
+        ["<s-tab>"] = "MultiselectTogglePrevious",
+        ["<c-j>"] = "NOP",
+      },
+      popup = {
+        ["?"] = "HelpPopup",
+        ["A"] = "CherryPickPopup",
+        ["d"] = "DiffPopup",
+        ["M"] = "RemotePopup",
+        ["ps"] = "PushPopup",
+        ["rs"] = "ResetPopup",
+        ["z"] = "StashPopup",
+        ["b"] = "BranchPopup",
+        ["B"] = "BisectPopup",
+        ["c"] = "CommitPopup",
+        ["f"] = "FetchPopup",
+        ["l"] = "LogPopup",
+        ["m"] = "MergePopup",
+        ["pl"] = "PullPopup",
+        ["rb"] = "RebasePopup",
+        ["rv"] = "RevertPopup",
+        ["w"] = "WorktreePopup",
+      },
+      status = {
+        ["k"] = "MoveUp",
+        ["j"] = "MoveDown",
+        ["q"] = "Close",
+        ["o"] = "OpenTree",
+        ["I"] = "InitRepo",
+        ["1"] = "Depth1",
+        ["2"] = "Depth2",
+        ["3"] = "Depth3",
+        ["4"] = "Depth4",
+        ["<tab>"] = "Toggle",
+        ["x"] = "Discard",
+        ["s"] = "Stage",
+        ["S"] = "StageUnstaged",
+        ["<c-s>"] = "StageAll",
+        ["K"] = "Untrack",
+        ["u"] = "Unstage",
+        ["U"] = "UnstageStaged",
+        ["$"] = "CommandHistory",
+        ["R"] = "ShowRefs", -- Forgotten in the documentation but mandatory to define
+        ["Y"] = "YankSelected",
+        ["<c-r>"] = "RefreshBuffer",
+        ["<enter>"] = "GoToFile",
+        ["<c-v>"] = "VSplitOpen",
+        ["<c-x>"] = "SplitOpen",
+        ["<c-t>"] = "TabOpen",
+        [";"] = "GoToPreviousHunkHeader",
+        [","] = "GoToNextHunkHeader",
+        ["[c"] = "OpenOrScrollUp",
+        ["]c"] = "OpenOrScrollDown",
+      },
+    },
   },
 }
