0cd504d62ddfb574bcae4c9a953313ba8af8cf18
clementjumel <clement.jumel@gmail.com>
2024-06-12 18:13:34 +0200

    🔥 Remove Poetry templates in Overseer
    These templates doesn't bring any additional value over simply
    opening a terminal & using aliases. Since it requires a bit of
    maintenance (e.g. adding the `remove` command), let's just give
    up on this & focus on maintaining aliases.

--- lua/plugins/tools/overseer/templates/init.lua
+++ lua/plugins/tools/overseer/templates/init.lua
@@ -2,7 +2,6 @@ local M = {}
 
 vim.list_extend(M, require("plugins.tools.overseer.templates.luajit"))
 vim.list_extend(M, require("plugins.tools.overseer.templates.mypy"))
-vim.list_extend(M, require("plugins.tools.overseer.templates.poetry"))
 vim.list_extend(M, require("plugins.tools.overseer.templates.pre-commit"))
 vim.list_extend(M, require("plugins.tools.overseer.templates.pytest"))
 vim.list_extend(M, require("plugins.tools.overseer.templates.python"))
--- lua/plugins/tools/overseer/templates/poetry.lua
+++
@@ -1,8 +0,0 @@
-local template_builder = require("plugins.tools.overseer.template_builder")
-
-local condition_callback = function(_) return vim.fn.filereadable("poetry.lock") == 1 end
-
-return {
-  template_builder.cmd({ "poetry", "add" }, { condition_callback = condition_callback, has_args = true }),
-  template_builder.cmd({ "poetry", "update" }, { condition_callback = condition_callback }),
-}
