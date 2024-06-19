f3fc2ab137022ffa811e3be92c4aa54ca2289223
clementjumel <clement.jumel@gmail.com>
2024-06-19 14:49:50 +0200

    ♻️ Simplify the management of Lualine's dirty fix

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -15,6 +15,12 @@ return {
     "ThePrimeagen/harpoon",
   },
   priority = 100, -- Main UI stuff should be loaded first
+  init = function()
+    -- Don't show Neovim mode in status line as it is redundant with Lualine's onw feature
+    -- This can be re-enabled in the `config` for status lines missing the corresponding feature, with the custom
+    -- parameter `_keep_showmode`
+    vim.opt.showmode = false
+  end,
   opts = theme.make_opts("lualine", {
     options = {
       icons_enabled = true,
@@ -27,10 +33,30 @@ return {
     sections = sections.default,
     extensions = extensions.build_extensions(sections.default),
   }),
-  config = theme.get_field("lualine", "config") or function(_, opts)
-    require("lualine").setup(opts)
+  config = function(_, opts)
+    local lualine = require("lualine")
 
-    -- Status line global options
-    vim.opt.showmode = false -- Don't show mode in status line as it is redundant with Lualine's onw feature
+    lualine.setup(opts)
+
+    if opts._keep_showmode then
+      vim.opt.showmode = true -- Show Neovim status in status line
+
+      -- HACK: prevent Lualine to show insert mode after a Telescope insert-mode search when `vim.opt.showmode = true`
+      --  This fixes an issue which might be related to https://github.com/nvim-telescope/telescope.nvim/issues/2995, and
+      --    the fix was taken from https://github.com/nvim-telescope/telescope.nvim/issues/2766; it can be removed once
+      --    the issue is not observed anymore.
+      --  To reproduce the issue:
+      --    - open Neovim with `vim.opt.showmode = true` (default value)
+      --    - trigger the LSP progress computation and wait for it to be finished as it prevents the issue from appearing
+      --    - open a file with Telescope in insert mode
+      --    - the Lualine should show the insert mode indicator (while not actually being in insert mode)
+      vim.api.nvim_create_autocmd("WinLeave", {
+        callback = function()
+          if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes([[<C-\><C-n>]], true, false, true), "i", false)
+          end
+        end,
+      })
+    end
   end,
 }
--- lua/theme/catppuccin-macchiato.lua
+++ lua/theme/catppuccin-macchiato.lua
@@ -20,27 +20,8 @@ local custom_sections = vim.tbl_deep_extend("force", sections.empty, {
 M.lualine_opts = {
   sections = custom_sections,
   extensions = extensions.build_extensions(custom_sections),
+  _keep_showmode = true,
 }
-M.lualine_config = function(_, opts)
-  require("lualine").setup(opts)
-
-  -- HACK: prevent Lualine to show insert mode after a Telescope insert-mode search when `vim.opt.showmode = true`
-  --  This fixes an issue which might be related to https://github.com/nvim-telescope/telescope.nvim/issues/2995, and
-  --    the fix was taken from https://github.com/nvim-telescope/telescope.nvim/issues/2766. This can be removed once
-  --    the issue is not observed anymore.
-  --  To reproduce the issue:
-  --    - open Neovim with `vim.opt.showmode = true` (default value)
-  --    - trigger the LSP progress computation and wait for it to be finished as it prevents the issue from appearing
-  --    - open a file with Telescope in insert mode
-  --    - the Lualine should show the insert mode indicator (while not actually being in insert mode)
-  vim.api.nvim_create_autocmd("WinLeave", {
-    callback = function()
-      if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes([[<C-\><C-n>]], true, false, true), "i", false)
-      end
-    end,
-  })
-end
 
 M.headlines_opts = {
   markdown = {
