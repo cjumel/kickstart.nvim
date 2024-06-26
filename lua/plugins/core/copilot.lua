618d311f3047abef400a82600ce823bca940d9b1
clementjumel <clement.jumel@gmail.com>
2024-06-26 09:54:40 +0200

    🩹 Re-use <Tab> for Copilot suggestions
    I don't like the custom implementation of the "accept whole suggestion"
    feature in Copilot (create a flicker on the screen) compared to the one
    automatically created, so this re-introduces the latter. Besides, this
    commit makes "accept-word" mapped to <C-CR>, which works very nicely
    with <C-BS> as "delete word". New keymaps are similar to the new ones
    used in zsh-autosuggestions.

--- lua/plugins/core/copilot.lua
+++ lua/plugins/core/copilot.lua
@@ -6,20 +6,14 @@
 
 return {
   "github/copilot.vim",
-  -- When lazy-loading more agressively (e.g. on `InsertEnter` event), the plugin doesn't write properly right away
+  -- When lazy-loading more agressively (e.g. on `InsertEnter` event), the plugin doesn't work properly right away
   event = { "BufNewFile", "BufReadPre" },
   cmd = { "Copilot" }, -- For other plugins using directly the command, like the settings in Hydra.nvim
   config = function()
-    vim.g.copilot_no_tab_map = true -- Don't automatically map the <Tab> key
     vim.g.copilot_filetypes = { markdown = true } -- Enable Copilot on additional filetypes
 
-    vim.keymap.set("i", "<C-]>", "<Plug>(copilot-accept-word)", { desc = "Copilot: accept word" })
-    vim.keymap.set("i", "<M-CR>", "<Plug>(copilot-accept-line)", { desc = "Copilot: accept line" }) -- <C-CR>
-    vim.keymap.set( -- This keymap doesn't work super well, a floating window flickers on the screen when using it
-      "i",
-      "<C-\\>",
-      'copilot#Accept("\\<CR>")',
-      { expr = true, replace_keycodes = false, desc = "Copilot: accept all" }
-    )
+    -- <Tab> is automatically mapped to "accept suggestion"
+    vim.keymap.set("i", "<C-]>", "<Plug>(copilot-accept-line)", { desc = "Copilot: accept line" })
+    vim.keymap.set("i", "<M-CR>", "<Plug>(copilot-accept-word)", { desc = "Copilot: accept word" }) -- <C-CR>
   end,
 }
