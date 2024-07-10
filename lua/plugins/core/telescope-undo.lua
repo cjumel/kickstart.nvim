f6fd1f35df35b3801c2513fde064d2df45491f0a
clementjumel <clement.jumel@gmail.com>
2024-07-10 18:17:37 +0200

    🩹 Improve undo-tree & telescope-undo setup

--- lua/plugins/core/telescope-undo.lua
+++ lua/plugins/core/telescope-undo.lua
@@ -1,19 +1,19 @@
 -- telescope-undo.nvim
 --
--- Visualize your undo tree and fuzzy-search changes in it.
+-- telescope-undo is a Telescope extension to visualize the undotree and fuzzy-search changes in it. I don't find it
+-- as convenient as the undotree plugin for simple visualization and navigation in the undotree and I've encountered
+-- issues with telescope-undo to restore some past states, but it enables fuzzy-searching changes in it, which is a
+-- super nice feature and makes telescope-undo nicely complementary with undotree.
 
 return {
   "debugloop/telescope-undo.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-  },
+  dependencies = { "nvim-telescope/telescope.nvim" },
   keys = {
     {
-      "<leader>u",
+      "<leader>fu",
       function()
-        local telescope = require("telescope")
-
         local custom_utils = require("utils")
+        local telescope = require("telescope")
 
         local opts = {
           tiebreak = function(current, existing, _) return current.index < existing.index end, -- Sort entries by recency
@@ -21,25 +21,28 @@ return {
         if custom_utils.visual.is_visual_mode() then
           opts.default_text = custom_utils.visual.get_text()
         end
-
         telescope.extensions.undo.undo(opts)
       end,
       mode = { "n", "v" },
-      desc = "[U]ndo tree history",
+      desc = "[F]ind: [U]ndotree",
     },
   },
   opts = {
-    diff_context_lines = 5,
+    diff_context_lines = 5, -- Number of lines to display around each change
     mappings = {
-      i = { -- Let's not overwrite potentially useful keymaps like <C-a>, <C-d>, <C-r> or <C-u>
+      i = {
         ["<CR>"] = function(bufnr) return require("telescope-undo.actions").yank_additions(bufnr) end,
-        ["<C-y>"] = function(bufnr) return require("telescope-undo.actions").yank_deletions(bufnr) end,
-        -- Disable default keymaps
+        ["<M-CR>"] = function(bufnr) return require("telescope-undo.actions").yank_deletions(bufnr) end, -- <C-CR>
+        -- Disable default plugin keymaps
         ["<S-cr>"] = false,
         ["<C-cr>"] = false,
+        ["<C-y>"] = false,
         ["<C-r>"] = false,
       },
-      n = { -- Let's just disable default keymaps as I simply don't use this mode
+      n = {
+        ["<CR>"] = function(bufnr) return require("telescope-undo.actions").yank_additions(bufnr) end,
+        ["<M-CR>"] = function(bufnr) return require("telescope-undo.actions").yank_deletions(bufnr) end, -- <C-CR>
+        -- Disable default plugin keymaps
         ["y"] = false,
         ["Y"] = false,
         ["u"] = false,
--- lua/plugins/core/undotree.lua
+++ lua/plugins/core/undotree.lua
@@ -1,18 +1,14 @@
 -- undotree
 --
--- The undo history visualizer for VIM.
+-- undotree is a plugin providing a simple but powerfull undo history visualizer for Vim and Neovim. It is very simple
+-- of use and provide a convenient interface to visualize the undo tree, visualize different branches and restore
+-- any past state, even those unacessible by simple undo/redo actions.
 
 return {
   "mbbill/undotree",
-  keys = {
-    {
-      "<leader>U",
-      function() vim.cmd("UndotreeToggle") end,
-      desc = "[U]ndo tree menu",
-    },
-  },
+  keys = { { "<leader>u", "<cmd>UndotreeToggle<CR>", desc = "[U]ndotree" } },
   config = function()
     vim.g.undotree_WindowLayout = 3 -- Window on the right of the screen
-    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_SetFocusWhenToggle = 1 -- Focus the undotree window when toggling it
   end,
 }
