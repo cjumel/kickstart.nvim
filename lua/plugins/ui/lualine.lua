0e789024112fc084bd9873b11adce33fc745054f
clementjumel <clement.jumel@gmail.com>
2024-08-02 10:32:37 +0200

    🐛 Remove fix of Telescope remaining in insert mode
    The removed fix was causing the "toggle search in all files" custom
    action of Telescope pickers to be broken.

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -40,23 +40,6 @@ return {
 
     if opts._keep_showmode then
       vim.opt.showmode = true -- Show Neovim status in status line
-
-      -- Prevent Lualine to show insert mode after a Telescope insert-mode search when `vim.opt.showmode = true`
-      -- This fixes an issue which might be related to https://github.com/nvim-telescope/telescope.nvim/issues/2995, and
-      --  the fix was taken from https://github.com/nvim-telescope/telescope.nvim/issues/2766; it can be removed once
-      --  the issue is not observed anymore, but it doesn't hurt much.
-      -- To reproduce the issue:
-      --  - open Neovim with `vim.opt.showmode = true` (default value)
-      --  - trigger the LSP progress computation and wait for it to be finished as it prevents the issue from appearing
-      --  - open a file with Telescope in insert mode
-      --  - the Lualine should show the insert mode indicator (while not actually being in insert mode)
-      vim.api.nvim_create_autocmd("WinLeave", {
-        callback = function()
-          if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes([[<C-\><C-n>]], true, false, true), "i", false)
-          end
-        end,
-      })
     end
   end,
 }
