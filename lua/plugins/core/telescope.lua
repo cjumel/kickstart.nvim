86e862ba70f41ceca279ffc7337dcd9b30690838
clementjumel <clement.jumel@gmail.com>
2024-08-02 11:14:43 +0200

    🩹 Remove Oil as Telescope dependency

--- lua/plugins/core/telescope.lua
+++ lua/plugins/core/telescope.lua
@@ -11,7 +11,6 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "nvim-treesitter/nvim-treesitter",
-    "stevearc/oil.nvim", -- Oil could have been lazy-loaded by Telescope but it is already not in the global setup
     {
       "nvim-telescope/telescope-fzf-native.nvim", -- Fuzzy finder algorithm requiring local dependencies to be built
       build = "make",
