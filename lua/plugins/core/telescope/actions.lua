f9d2cf9e3b4d5cbec8ebec699b683f7b4ed5c589
clementjumel <clement.jumel@gmail.com>
2024-08-13 23:12:27 +0200

    🩹 Further improve Trouble config

--- lua/plugins/core/telescope/actions.lua
+++ lua/plugins/core/telescope/actions.lua
@@ -13,7 +13,7 @@ function M.smart_open_quickfix(prompt_bufnr, _mode)
   local telescope_actions = require("telescope.actions")
   local trouble = require("trouble") -- Lazy loaded thanks to function wrapping
   telescope_actions.smart_send_to_qflist(prompt_bufnr, _mode)
-  trouble.open("quickfix")
+  trouble.open("qflist")
 end
 
 return M
--- lua/plugins/core/trouble.lua
+++ lua/plugins/core/trouble.lua
@@ -1,32 +1,52 @@
 -- Trouble
 --
 -- Trouble provides pretty lists for showing diagnostics, references, telescope results, quickfix lists, etc., to help
--- you solve all the trouble your code is causing. The user interface it provides is very nice and integrates with many
--- other tools, making it a very useful plugin in many situations.
+-- you solve all the trouble your code is causing. With a very cool user interface and the integratation of many other
+-- tools (like the Todo comments plugins), it is very complementary with plugins like Telescope, since Telescope shines
+-- with fuzzy searching and for "one-time" navigation (once we select entries, the Telescope menu disappears), while
+-- Trouble is better equipped for long-term navigation of items (we can select an item while keeping the Trouble window
+-- open).
 
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   cmd = { "Trouble" }, -- Useful for other plugins calling Trouble through a command
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble: diagnostics" },
+    { "<leader>xd", "<cmd>Trouble diagnostics_document toggle<CR>", desc = "Trouble: [D]iagnostics (document)" },
+    { "<leader>xD", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble: [D]iagnostics (workspace)" },
     { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Trouble: [L]ocation list" },
     { "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Trouble: [Q]uickfix list" },
   },
   opts = {
-    focus = true, -- Focus the Trouble window when opened by default
     modes = {
+      -- Builtin modes
       diagnostics = {
-        focus = false, -- Don't focus the Trouble window when opened
+        title = "{hl:Title}Workspace Diagnostics{hl} {count}",
         open_no_results = true, -- Open the Trouble window even when there is no item as they can appear dynamically
-        filter = { buf = 0 }, -- List items from the current buffer only
       },
+      diagnostics_document = {
+        mode = "diagnostics",
+        title = "{hl:Title}Document Diagnostics{hl} {count}",
+        filter = { buf = 0 }, -- List items from the current buffer only
+        groups = {}, -- Remove groups as this mode includes only current buffer items
+      },
+      loclist = {
+        title = "{hl:Title}Location list{hl} {count}",
+        focus = true, -- Focus the Trouble window when opened
+      },
+      qflist = {
+        title = "{hl:Title}Quickfix list{hl} {count}",
+        focus = true, -- Focus the Trouble window when opened
+      },
+
+      -- LSP modes (see nvim-lspconfig)
       lsp_references = {
+        focus = true, -- Focus the Trouble window when opened
         auto_refresh = false, -- Don't re-compute references when cursor moves in the main window
       },
       symbols = {
-        focus = false, -- Don't focus the Trouble window when opened
         open_no_results = true, -- Open the Trouble window even when there is no item as they can appear dynamically
+        groups = {}, -- Remove groups as this mode includes only current buffer items
       },
     },
   },
