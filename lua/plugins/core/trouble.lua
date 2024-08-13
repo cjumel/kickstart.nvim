953e89a7ac62fec0c21816014a762e80c799fa9a
clementjumel <clement.jumel@gmail.com>
2024-08-13 14:40:30 +0200

    🩹 Simplify Trouble config

--- lua/plugins/core/trouble.lua
+++ lua/plugins/core/trouble.lua
@@ -2,42 +2,27 @@
 --
 -- Trouble provides pretty lists for showing diagnostics, references, telescope results, quickfix lists, etc., to help
 -- you solve all the trouble your code is causing. The user interface it provides is very nice and integrates with many
--- other tools, making it a very useful plugin to have.
+-- other tools, making it a very useful plugin in many situations.
 
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  cmd = { "Trouble" }, -- Especially useful for other plugins calling Trouble through a command
+  cmd = { "Trouble" }, -- Useful for other plugins calling Trouble through a command
   keys = {
-    { "<leader>xd", "<cmd>Trouble diagnostics_buffer toggle<CR>", desc = "Trouble: [D]iagnostics (buffer)" },
-    { "<leader>xD", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble: [D]iagnostics (workspace)" },
-    { "<leader>xr", "<cmd>Trouble lsp_references_buffer toggle<CR>", desc = "Trouble: LSP [R]eferences (buffer)" },
-    { "<leader>xR", "<cmd>Trouble lsp_references toggle<CR>", desc = "Trouble: LSP [R]eferences (workspace)" },
-    { "<leader>xa", "<cmd>Trouble lsp toggle<CR>", desc = "Trouble: [A]ll LSP symbol information" },
-    { "<leader>xs", "<cmd>Trouble symbols toggle<CR>", desc = "Trouble: LSP [S]ymbols" },
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble: diagnostics" },
     { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Trouble: [L]ocation list" },
     { "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Trouble: [Q]uickfix list" },
   },
   opts = {
-    focus = true, -- Focus the Trouble window when opened
+    focus = true, -- Focus the Trouble window when opened by default
     modes = {
       diagnostics = {
         focus = false, -- Don't focus the Trouble window when opened
         open_no_results = true, -- Open the Trouble window even when there is no item as they can appear dynamically
-      },
-      diagnostics_buffer = {
-        mode = "diagnostics", -- Inherit from existing mode
         filter = { buf = 0 }, -- List items from the current buffer only
       },
       lsp_references = {
         auto_refresh = false, -- Don't re-compute references when cursor moves in the main window
-      },
-      lsp_references_buffer = {
-        mode = "lsp_references", -- Inherit from existing mode
-        filter = { buf = 0 }, -- List items from the current buffer only
-      },
-      lsp = {
-        auto_refresh = false, -- Don't re-compute references, declarations, etc. when cursor moves in the main window
       },
       symbols = {
         focus = false, -- Don't focus the Trouble window when opened
