8281906110075e2f3c3acc1847dcdd0c6d77825e
clementjumel <clement.jumel@gmail.com>
2024-08-20 14:54:50 +0200

    ✨ New light terminal theme

--- lua/theme/onelight.lua
+++ lua/theme/onelight.lua
@@ -1,10 +1,13 @@
 local M = {}
 
 M.onedark_enabled = true
+M.onedark_opts = {
+  style = "light",
+}
 
 M.lualine_opts = {
   options = {
-    theme = "onedark",
+    theme = "onelight",
   },
 }
 
