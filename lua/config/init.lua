7863f7e0630408cd658140259df0717eb4ad89a2
clementjumel <clement.jumel@gmail.com>
2024-09-27 15:21:28 +0200

    ✨ Simplify access to custom neovim config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,8 +2,8 @@
 --
 -- The default Neovim configuration is defined in `./default.lua` and can be used as template for the other
 -- configuration files. A global configuration can be defined in a `.nvim.global.lua` file in the Neovim configuration
--- directory (typically in `~/.config/nvim/`), it will override the default configuration in the whole system. 
--- Per-project configurations can be additionally defined, by adding a `.nvim.lua` file in any directory, and it will 
+-- directory (typically in `~/.config/nvim/`), it will override the default configuration in the whole system.
+-- Per-project configurations can be additionally defined, by adding a `.nvim.lua` file in any directory, and it will
 -- override all other configurations for any project within this directory or its sub-directories. Finally, additional
 -- configuration can be done through environment variables, using the prefix `NVIM_` followed by the configuration name
 -- in all capital letters.
@@ -16,8 +16,8 @@
 --    configurations (however, I couldn't make it fit my quite simple use-case, as loading this plugin in the condition
 --    of other plugins, at plugin definition time, didn't work)
 
-local config = {}
-local found_neovim_configs = {} -- To keep track of found configurations, for debugging purposes
+local config = {} -- Final, merged configuration
+local found_configs = {} -- To keep track of found configurations
 
 -- Fetch the default configuration file
 -- This file is known in this repository, so it doesn't need to be fetched securely, by opposition with the others,
@@ -25,7 +25,7 @@ local found_neovim_configs = {} -- To keep track of found configurations, for de
 require("config.default") -- Source the configuration
 local default_config = vim.g.neovim_config or {}
 config = vim.tbl_deep_extend("force", config, default_config)
-found_neovim_configs["default"] = default_config
+found_configs["default"] = default_config
 
 -- Fetch the global configuration file securely, if it exists
 local global_config_path = vim.env.HOME .. "/.config/nvim/.nvim.global.lua"
@@ -34,7 +34,7 @@ if global_config_code ~= nil then -- Configuration file is found and trusted
   load(global_config_code)() -- Source the configuration file code
   local global_config = vim.g.neovim_config or {}
   config = vim.tbl_deep_extend("force", config, global_config)
-  found_neovim_configs["global"] = global_config
+  found_configs["global"] = global_config
 end
 
 -- Look recursively upward from the cwd until the home directory for a `.nvim.lua` configuration file, and fetch it
@@ -46,12 +46,12 @@ if project_config_path ~= "" then -- Configuration file is found
     load(project_config_code)() -- Source the configuration file code
     local project_config = vim.g.neovim_config or {}
     config = vim.tbl_deep_extend("force", config, project_config)
-    found_neovim_configs["project"] = project_config
+    found_configs["project"] = project_config
   end
 end
 
--- Look for environment variables with keys prefixed by `NVIM_`, followed by the configuration key name in capital 
---  letters. This only work for boolean values ("0", "1", "true", "false"), arrays of strings (comma-separated list) or 
+-- Look for environment variables with keys prefixed by `NVIM_`, followed by the configuration key name in capital
+--  letters. This only work for boolean values ("0", "1", "true", "false"), arrays of strings (comma-separated list) or
 --  strings.
 local env_config = {}
 for _, key in ipairs(vim.tbl_keys(default_config)) do
@@ -70,9 +70,10 @@ for _, key in ipairs(vim.tbl_keys(default_config)) do
 end
 if not vim.tbl_isempty(env_config) then
   config = vim.tbl_deep_extend("force", config, env_config)
-  found_neovim_configs["env"] = env_config
+  found_configs["env"] = env_config
 end
 
-vim.g.found_neovim_configs = found_neovim_configs
+vim.g.neovim_config = config
+vim.g.found_neovim_configs = found_configs
 
 return config
