80b885c85bd9e2234b07da92f14bc7720765af18
clementjumel <clement.jumel@gmail.com>
2024-09-27 15:14:39 +0200

    ✨ Support additional configuration through environement variables

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,9 +2,11 @@
 --
 -- The default Neovim configuration is defined in `./default.lua` and can be used as template for the other
 -- configuration files. A global configuration can be defined in a `.nvim.global.lua` file in the Neovim configuration
--- directory (typically in `~/.config/nvim/`), it will override the default configuration. Per-project configurations
--- can be additionally defined, by adding a `.nvim.lua` file in any directory, and it will override all other
--- configurations for any project within this directory or its sub-directories.
+-- directory (typically in `~/.config/nvim/`), it will override the default configuration in the whole system. 
+-- Per-project configurations can be additionally defined, by adding a `.nvim.lua` file in any directory, and it will 
+-- override all other configurations for any project within this directory or its sub-directories. Finally, additional
+-- configuration can be done through environment variables, using the prefix `NVIM_` followed by the configuration name
+-- in all capital letters.
 --
 -- This module takes ideas from:
 --  - exrc, a builtin Neovim feature which *securely* sources `.nvim.lua` files for project-level configuration
@@ -46,6 +48,29 @@ if project_config_path ~= "" then -- Configuration file is found
     config = vim.tbl_deep_extend("force", config, project_config)
     found_neovim_configs["project"] = project_config
   end
+end
+
+-- Look for environment variables with keys prefixed by `NVIM_`, followed by the configuration key name in capital 
+--  letters. This only work for boolean values ("0", "1", "true", "false"), arrays of strings (comma-separated list) or 
+--  strings.
+local env_config = {}
+for _, key in ipairs(vim.tbl_keys(default_config)) do
+  local value = vim.env["NVIM_" .. string.upper(key)]
+  if value ~= nil then
+    if vim.tbl_contains({ "1", "true" }, string.lower(value)) then
+      env_config[key] = true
+    elseif vim.tbl_contains({ "0", "false" }, string.lower(value)) then
+      env_config[key] = false
+    elseif string.find(value, ",") then
+      env_config[key] = vim.split(value, ",")
+    else
+      env_config[key] = value
+    end
+  end
+end
+if not vim.tbl_isempty(env_config) then
+  config = vim.tbl_deep_extend("force", config, env_config)
+  found_neovim_configs["env"] = env_config
 end
 
 vim.g.found_neovim_configs = found_neovim_configs
