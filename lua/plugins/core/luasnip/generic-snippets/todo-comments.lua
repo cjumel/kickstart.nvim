f118f66075a73b0256fe72bf6da4402ce70e69ee
clementjumel <clement.jumel@gmail.com>
2024-09-30 10:31:16 +0200

    ✨ Upgrade todo-comments snippet implementation

--- lua/plugins/core/luasnip/generic-snippets/todo-comments.lua
+++ lua/plugins/core/luasnip/generic-snippets/todo-comments.lua
@@ -1,26 +1,25 @@
--- Provide todo-comment snippets, either using Treesitter to detect if we are within a comment or a string, or not using
--- it at all, in case it's not available.
--- Original implementation is a simpler version of
--- https://github.com/L3MON4D3/LuaSnip/wiki/Cool-Snippets#all---todo-commentsnvim-snippets
--- It was then simplified to avoid relying on the `Comment.nvim` plugin
-
-local ls = require("luasnip")
+-- Provide todo-comment snippets, either using Treesitter to detect if we are within a comment or a string, or not, in
+-- case it's not available. The original implementation is a simpler version of:
+--  https://github.com/L3MON4D3/LuaSnip/wiki/Cool-Snippets#all---todo-commentsnvim-snippets
+-- It was then simplified to avoid relying on the `Comment.nvim` plugin and use only builtin Neovim features.
 
 local custom_conditions = require("plugins.core.luasnip.conditions")
+local ls = require("luasnip")
 
 local c = ls.choice_node
 local f = ls.function_node
 local i = ls.insert_node
+local r = ls.restore_node
 local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
 
---- Get the comment string start and end, depending on the current file type.
+--- Get the comment string start and end, taking the current file type into account.
 ---@return string[]
 local function get_comment_strings()
   -- Fetch the comment string for the filetype (e.g. '-- %s' for `lua`)
   -- Compared to the initial implementation using `Comment.nvim`, this doesn't work for code regions with different
-  -- comment string than the filetype (e.g. code embedded in Markdown files)
+  --  comment string than the filetype (e.g. code embedded in Markdown files), but this is good enough for me
   local cstring = vim.bo.commentstring
 
   -- Initially, comment strings are ready to be used by `format`, so we want to split the left and right side
@@ -41,10 +40,11 @@ local function get_comment_strings()
   end
   return { left, right }
 end
+
 local function get_comment_string_start() return get_comment_strings()[1] end
 local function get_comment_string_end() return get_comment_strings()[2] end
 
-local todo_comment_keywords = {
+local todo_comment_keywords = { -- List the todo-comment keywords supported by the snippets
   "_TODO", -- Custom "private TODO" keyword for stuff to do right now & which should not be shared in the codebase
   "TODO",
   "FIXME",
@@ -56,31 +56,50 @@ local todo_comment_keywords = {
   "NOTE",
 }
 
---- Get the todo-comment keyword option snippet nodes.
----@return table todo_comment_sn_options Table of todo-comment keyword option snippet nodes.
+--- Get the todo-comment keyword option snippet nodes (without the comment string).
+---@return table
 local function get_todo_comment_sn_options()
   local todo_comment_sn_options = {}
-  for _, keyword in ipairs(todo_comment_keywords) do
-    table.insert(todo_comment_sn_options, sn(nil, { t(keyword), t(": "), i(1) }))
+  for idx, keyword in ipairs(todo_comment_keywords) do
+    if idx == 0 then
+      table.insert(todo_comment_sn_options, sn(nil, { t(keyword), t(": "), r(1, "content", i(1)) }))
+    else
+      table.insert(todo_comment_sn_options, sn(nil, { t(keyword), t(": "), r(1, "content") }))
+    end
   end
   return todo_comment_sn_options
 end
 
+local docstring = [[
+Supported keywords:
+- ]] .. table.concat(todo_comment_keywords, "\n- ")
+
 return {
   no_ts = {
-    s({ trig = "todo-comment" }, {
+    s({
+      trig = "todo-comment",
+      docstring = docstring,
+    }, {
       f(get_comment_string_start),
       c(1, get_todo_comment_sn_options()),
       f(get_comment_string_end),
     }),
   },
   ts = {
-    s({ trig = "todo-comment", show_condition = custom_conditions.is_in_code }, {
+    s({
+      trig = "todo-comment",
+      show_condition = custom_conditions.is_in_code,
+      docstring = docstring,
+    }, {
       f(get_comment_string_start),
       c(1, get_todo_comment_sn_options()),
       f(get_comment_string_end),
     }),
-    s({ trig = "todo-comment", show_condition = custom_conditions.is_in_comment }, {
+    s({
+      trig = "todo-comment",
+      show_condition = custom_conditions.is_in_comment,
+      docstring = docstring,
+    }, {
       c(1, get_todo_comment_sn_options()),
     }),
   },
