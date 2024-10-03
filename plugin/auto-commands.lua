409576ea46ada856e47fcc6fb69bab3b25b8ecd8
clementjumel <clement.jumel@gmail.com>
2024-10-03 17:15:59 +0200

    🔥 Remove unnamed buffer cleaning auto-command
    This command is not that important and it makes Harpoon break, so since
    I want to re-introduce Harpoon, I'm removing it.

--- plugin/auto-commands.lua
+++ plugin/auto-commands.lua
@@ -6,36 +6,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
 })
 
--- [[ Clear Unnamed Buffers ]]
--- This auto-command removes the empty unnamed buffers (created when no buffer is opened) whenever another buffer is
---  opened. This prevents showing unnamed buffers in the buffers Telescope picker, for instance.
-
---- Delete all unnamed buffers whose content is empty.
----@return nil
-local function clear_unnamed_buffers()
-  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-    if
-      vim.api.nvim_buf_is_loaded(bufnr)
-      and vim.api.nvim_buf_get_name(bufnr) == ""
-      and vim.api.nvim_get_option_value("buftype", { buf = bufnr }) == ""
-    then
-      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false) -- Get all lines in the buffer
-      local total_characters = 0
-      for _, line in ipairs(lines) do
-        total_characters = total_characters + #line
-      end
-      if total_characters == 0 then
-        vim.api.nvim_buf_delete(bufnr, { force = true })
-      end
-    end
-  end
-end
-
-vim.api.nvim_create_autocmd("BufReadPost", {
-  callback = clear_unnamed_buffers,
-  group = vim.api.nvim_create_augroup("ClearUnamedBuffers", { clear = true }),
-})
-
 -- [[ Automatic Color Column ]]
 -- This auto-command automatically sets the value of color column according to the `textwidth` buffer option. This makes
 --  the management of color columns very easy, as the values of `textwidth` can be simply managed through Neovim's
