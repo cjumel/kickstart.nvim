9570a95ecde7fae56ccb4fe5da74c07da61df9db
clementjumel <clement.jumel@gmail.com>
2024-10-14 11:21:59 +0200

    ⚡️ Rework Hydras

--- lua/plugins/ui/hydra/hunk_hydra.lua
+++ lua/plugins/ui/hydra/hunk_hydra.lua
@@ -1,4 +1,6 @@
--- The hunk Hydra provides a simple interface to interact with Git hunks using Gitsigns features.
+-- The hunk Hydra provides a simple menu to interact with Git hunks (e.g. navigate between them, stage, discard or
+-- unstage them, etc.) using gitsigns.nvim, without having to press repeatedly a keymap prefix ("<leader>h" in my case)
+-- to perform several actions.
 
 local Hydra = require("hydra")
 
@@ -8,25 +10,26 @@ Hydra({
     desc = "[H]unk Hydra",
     color = "pink", -- For synchron buffer actions
     -- Setting `buffer=true` or `buffer=bufnr` makes the hunk manager keymaps only work in a single buffer, while still
-    -- being able to switch buffer (as `foreign_keys="run"` can't be overriden for pink Hydra). In that case, the Hydra
-    -- is still opened but the keymaps don't work in the new buffer, which is quite confusing.
+    --  being able to switch buffer (as `foreign_keys="run"` can't be overriden for pink Hydra). In that case, the Hydra
+    --  is still opened but the keymaps don't work in the new buffer, which is quite confusing, so I prefer not to use
+    --  this.
     buffer = nil,
   },
   mode = { "n", "v" },
   hint = [[
-   ^ ^                            ^ ^          Hunk Hydra           ^ ^                                
-   _,_ ➜ Next hunk                _K_ ➜ Hover hunk                  _u_ ➜ [U]nstage last staged hunk   
-   _;_ ➜ Previous hunk            _s_ ➜ [S]tage hunk/selection      _x_ ➜ Discard hunk/selection   
+                                    Hunk Hydra
+   _K_ ➜ Hover hunk               _u_ ➜ [U]nstage staged hunk    _,_ ➜ Next hunk    
+   _s_ ➜ [S]tage hunk/selection   _x_ ➜ Discard hunk/selection   _;_ ➜ Previous hunk    
 ]],
   heads = {
-    -- "," & ";" are not repeatable on purpose, to be able to resume the previous movements after leaving the Hydra
-    { ",", function() require("gitsigns").next_hunk({ navigation_message = false }) end },
-    { ";", function() require("gitsigns").prev_hunk({ navigation_message = false }) end },
-    -- Don't use a key for preview which could be used for navigating (like "h", "j", "k", "l") or selecting ("v", or
-    -- any key involved in a text object) to be able to use them to stage
+    -- In the following, let's not use keys which could be used for buffer navigation (like "h", "j", "k", "l") or
+    --  text selection ("v", or any key involved in a text object), that way we can use them to navigate between hunks
+    --  and select part of them, before staging, discarding, etc. while remaining in the Hydra.
+
     { "K", function() require("gitsigns").preview_hunk() end },
     {
-      -- "a" (like in `git add`) doesn't work well in visual mode, there is a delay due to text-objects keymaps with `a`
+      -- "a" (like in `git add`) doesn't work well in visual mode, there is a delay due to text-objects keymaps
+      --  starting with `a`, so let's avoid using it
       "s",
       function()
         if vim.fn.mode() == "n" then
@@ -47,6 +50,12 @@ Hydra({
         end
       end,
     },
+    -- "," & ";" are not repeatable on purpose, to be able to resume the previous next/previous keymaps after leaving
+    --  the Hydra
+    { ",", function() require("gitsigns").next_hunk({ navigation_message = false }) end },
+    { ";", function() require("gitsigns").prev_hunk({ navigation_message = false }) end },
+
+    -- Exit heads
     { "q", nil, { exit = true, mode = "n", desc = false } },
     { "<Esc>", nil, { exit = true, mode = "n", desc = false } },
   },
--- lua/plugins/ui/hydra/window_hydra.lua
+++ lua/plugins/ui/hydra/window_hydra.lua
@@ -1,5 +1,6 @@
--- The window Hydra uses builtin keymaps and descriptions to re-implement window management keymaps in a more
--- user-friendly way.
+-- The window Hydra provides an almost-exhaustive menu to interact with windows and tabs using most of the builtin
+-- keymaps without modification, without having to press repeatedly a keymap prefix ("<C-w>" by default) to perform
+-- several actions.
 
 local Hydra = require("hydra")
 
@@ -8,22 +9,20 @@ Hydra({
   config = {
     desc = "Window Hydra",
   },
+  -- A few keymaps are not re-implemented:
+  --  - "d"/"<C-d>" to show the diagnostics under the cursor, as I have re-implemented it with a custom keymap
+  --  - "_" to max out the height, as not supported by Hydra
+  --  - "|" to max out the width, by consistency with "_"
   hint = [[
-                                            Window Hydra                                              
-   _+_ ➜ Increase height         _h_ ➜ Go to the left window    _q_ ➜ [Q]uit window   
-   _-_ ➜ Decrease height         _j_ ➜ Go to the down window    _s_ ➜ [S]plit window   
-   _>_ ➜ Increase width          _k_ ➜ Go to the up window      _v_ ➜ [V]ertically split window   
-   _<_ ➜ Decrease width          _l_ ➜ Go to the right window   _w_ ➜ Switch windows   
-   _=_ ➜ Equally high and wide   _o_ ➜ Close [O]ther windows    _x_ ➜ Swap current window with next   
+                                        Window Hydra
+   _h_ ➜ Go to the left window    _s_ ➜ [S]plit window              _+_ ➜ Increase height   
+   _j_ ➜ Go to the down window    _T_ ➜ Break window into [T]ab     _-_ ➜ Decrease height   
+   _k_ ➜ Go to the up window      _v_ ➜ Split window [V]ertically   _<_ ➜ Decrease width   
+   _l_ ➜ Go to the right window   _w_ ➜ Switch windows              _=_ ➜ Equally high and wide   
+   _o_ ➜ Close [O]ther windows    _x_ ➜ Swap window with next       _>_ ➜ Increase width   
+   _q_ ➜ [Q]uit window   
 ]],
   heads = {
-    { "+", "<C-w>+" },
-    { "-", "<C-w>-" },
-    { "<", "<C-w><" },
-    { "=", "<C-w>=" },
-    { ">", "<C-w>>" },
-    -- { "_", "<C-w>_" }, -- Not supported by Hydra hint, but not important
-    -- { "|", "<C-w>|" }, -- Not important as well, and disabled by consistency with "_"
     { "h", "<C-w>h" },
     { "j", "<C-w>j" },
     { "k", "<C-w>k" },
@@ -31,10 +30,17 @@ Hydra({
     { "o", "<C-w>o" },
     { "q", "<C-w>q" },
     { "s", "<C-w>s" },
-    -- { "T", "<C-w>T" }, -- I don't use tabs so it's not useful
+    { "T", "<C-w>T" },
     { "v", "<C-w>v" },
     { "w", "<C-w>w" },
     { "x", "<C-w>x" },
+    { "+", "<C-w>+" },
+    { "-", "<C-w>-" },
+    { "<", "<C-w><" },
+    { "=", "<C-w>=" },
+    { ">", "<C-w>>" },
+
+    -- Exit head
     { "<Esc>", nil, { exit = true, desc = false } },
   },
 })
