9c8b6782e9768660170049b20b898a527593d6cf
clementjumel <clement.jumel@gmail.com>
2024-10-14 09:15:26 +0200

    ✨ New "Bd" user-command with bang

--- plugin/user-commands.lua
+++ plugin/user-commands.lua
@@ -1,2 +1,17 @@
 -- Command to write the current buffer without any auto-command, for instance to prevent triggering format-on-save
-vim.api.nvim_create_user_command("W", "noautocmd w", { desc = "Write buffer without auto-commands" })
+vim.api.nvim_create_user_command("W", function(args)
+  if args.bang then
+    vim.cmd("noautocmd w!")
+  else
+    vim.cmd("noautocmd w")
+  end
+end, { desc = "Write buffer without auto-commands", bang = true })
+
+-- Command to close all buffers at once
+vim.api.nvim_create_user_command("Bd", function(args)
+  if args.bang then
+    vim.cmd("bufdo bd!")
+  else
+    vim.cmd("bufdo bd")
+  end
+end, { desc = "Close all opened buffers", bang = true })
