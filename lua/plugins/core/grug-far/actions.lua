b3f68086466c1cd3700a099e4a376539392893c9
clementjumel <clement.jumel@gmail.com>
2024-10-21 12:10:44 +0200

    ⚡️ Improve grug-far actions

--- lua/plugins/core/grug-far/actions.lua
+++ lua/plugins/core/grug-far/actions.lua
@@ -1,38 +1,59 @@
+local buffer = require("buffer")
 local visual_mode = require("visual_mode")
 
 local M = {}
 
-M.grug_far = function(opts)
-  opts = opts or {}
-  local current_buffer_only = opts.current_buffer_only or false
-  local current_filetype_only = opts.current_filetype_only or false
-
-  local grug_far_opts = {
+M.grug_far = function()
+  local opts = {
     prefills = {},
   }
 
   if vim.bo.filetype == "oil" then
-    local cwd = require("oil").get_current_dir()
-    if cwd then
-      grug_far_opts.prefills.paths = vim.fn.expand(cwd)
+    opts.prefills.paths = require("oil").get_current_dir()
+  end
+  if visual_mode.is_on() then
+    opts.prefills.search = visual_mode.get_text()
+  end
+
+  require("grug-far").grug_far(opts)
+end
+
+M.grug_far_buffer = function()
+  local opts = {
+    prefills = {},
+  }
+
+  if vim.bo.filetype ~= "oil" then
+    opts.prefills.paths = vim.fn.expand("%")
+  else
+    error("Grug-far buffer action can't be used on Oil buffers")
+  end
+  if visual_mode.is_on() then
+    opts.prefills.search = visual_mode.get_text()
+  end
+
+  require("grug-far").grug_far(opts)
+end
+
+M.grug_far_filetype = function()
+  local opts = {
+    prefills = {},
+  }
+
+  if vim.bo.filetype ~= "oil" then
+    opts.prefills.filesFilter = "*." .. vim.bo.filetype
+  else
+    opts.prefills.paths = require("oil").get_current_dir()
+    local filetype = buffer.get_project_filetype()
+    if filetype ~= nil then
+      opts.prefills.filesFilter = "*." .. filetype
     end
   end
   if visual_mode.is_on() then
-    grug_far_opts.prefills.search = visual_mode.get_text()
+    opts.prefills.search = visual_mode.get_text()
   end
 
-  if current_buffer_only then
-    grug_far_opts.prefills.paths = vim.fn.expand("%")
-  end
-  if current_filetype_only then
-    grug_far_opts.prefills.filesFilter = "*." .. vim.bo.filetype
-  end
-
-  require("grug-far").grug_far(grug_far_opts)
+  require("grug-far").grug_far(opts)
 end
-
-M.grug_far_buffer = function() M.grug_far({ current_buffer_only = true }) end
-
-M.grug_far_filetype = function() M.grug_far({ current_filetype_only = true }) end
 
 return M
