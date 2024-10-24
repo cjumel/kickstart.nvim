2828c251c390a7b9dc6c54d8a76509fa20acabe5
clementjumel <clement.jumel@gmail.com>
2024-10-24 15:08:55 +0200

    ✨ Add date & time snippets for Markdown

--- lua/plugins/core/luasnip/snippets/markdown/keywords.lua
+++ lua/plugins/core/luasnip/snippets/markdown/keywords.lua
@@ -2,6 +2,7 @@ local conds = require("plugins.core.luasnip.conditions")
 local ls = require("luasnip")
 
 local c = ls.choice_node
+local d = ls.dynamic_node
 local i = ls.insert_node
 local r = ls.restore_node
 local s = ls.snippet
@@ -41,6 +42,26 @@ return {
     t({ "", "" }),
     i(2),
     t({ "", "```" }),
+  }),
+
+  s({
+    trig = "date",
+    desc = [[
+Multiple-choice snippet:
+- `<%Y>-<%m>-<%d>` (ISO 8601)
+- `<%d>/<%m>/<%Y>` (French)]],
+  }, {
+    d(
+      1,
+      function(_)
+        return sn(nil, {
+          c(1, {
+            sn(nil, { t(os.date("%Y-%m-%d")), i(1) }),
+            sn(nil, { t(os.date("%d/%m/%Y")), i(1) }),
+          }),
+        })
+      end
+    ),
   }),
 
   s({
@@ -106,5 +127,25 @@ Multiple-choice snippet:
     t("~"),
     i(1),
     t("~"),
+  }),
+
+  s({
+    trig = "time",
+    desc = [[
+Multiple-choice snippet:
+- `<%H>:<%M>` (ISO 8601)
+- `<%H>h<%M>` (French)]],
+  }, {
+    d(
+      1,
+      function(_)
+        return sn(nil, {
+          c(1, {
+            sn(nil, { t(os.date("%H:%M")), i(1) }),
+            sn(nil, { t(os.date("%Hh%M")), i(1) }),
+          }),
+        })
+      end
+    ),
   }),
 }
