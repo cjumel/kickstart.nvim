2522e27da9884465cbc2e04e4a27e877186be7d9
clementjumel <clement.jumel@gmail.com>
2024-11-09 11:59:12 +0100

    🩹 Fix Ruff condition in Overseer.nvim

--- lua/plugins/tools/overseer/templates/ruff.lua
+++ lua/plugins/tools/overseer/templates/ruff.lua
@@ -16,15 +16,41 @@ return {
   name = "ruff",
   condition = {
     callback = function(_)
-      return vim.fn.executable("ruff") == 1
-        -- Add a condition on the presence of Python files in the project, as, when installed with Mason.nvim,
-        -- Ruff is always executable
+      if vim.fn.executable("ruff") == 0 then
+        return false
+      end
+
+      -- When installed in Neovim with Mason.nvim, Ruff is always executable, so let's add a smarter condition to make
+      -- sure it doesn't always appear in all the projects: look for any potential Ruff configuration file, or any
+      -- Python file in the current project (or an approximation of this)
+
+      local root_path = path_utils.get_project_root(":p")
+      local potential_ruff_config_file_names = { "pyproject.toml", "ruff.toml", ".ruff.toml" }
+      for _, config_file_name in ipairs(potential_ruff_config_file_names) do
+        if vim.fn.filereadable(root_path .. config_file_name) == 1 then
+          return true
+        end
+      end
+
+      local current_path = nil
+      if vim.bo.buftype == "" and vim.bo.filetype ~= "" then -- A regular buffer is opened
+        current_path = vim.fn.expand("%")
+      elseif vim.bo.filetype == "oil" then -- An Oil buffer is opened
+        current_path = require("oil").get_current_dir()
+      end
+      if
+        current_path ~= nil
         and not vim.tbl_isempty(vim.fs.find(function(name, _) return name:match(".*%.py$") end, {
           type = "file",
-          path = vim.fn.expand("%"),
+          path = current_path,
           upward = true,
-          stop = path_utils.get_project_root(),
+          stop = root_path,
         }))
+      then
+        return true
+      end
+
+      return false
     end,
   },
   generator = function(_, cb)
