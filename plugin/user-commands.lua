9bf729edfefa2891eb6a6ddc2d6cafc537d63b8f
clementjumel <clement.jumel@gmail.com>
2024-11-12 21:57:38 +0100

    ✨ More commands to write buffer(s) with no auto-command

--- plugin/user-commands.lua
+++ plugin/user-commands.lua
@@ -1,4 +1,4 @@
--- Command to write the current buffer without any auto-command, for instance to prevent triggering format-on-save
+-- Commands to write buffer(s) without triggering auto-commands (e.g. to avoid triggering format-on-save)
 vim.api.nvim_create_user_command("W", function(args)
   if args.bang then
     vim.cmd("noautocmd w!")
@@ -6,6 +6,20 @@ vim.api.nvim_create_user_command("W", function(args)
     vim.cmd("noautocmd w")
   end
 end, { desc = "Write buffer without auto-commands", bang = true })
+vim.api.nvim_create_user_command("Wq", function(args)
+  if args.bang then
+    vim.cmd("noautocmd wq!")
+  else
+    vim.cmd("noautocmd wq")
+  end
+end, { desc = "Write buffer and quite without auto-commands", bang = true })
+vim.api.nvim_create_user_command("Wqa", function(args)
+  if args.bang then
+    vim.cmd("noautocmd wqa!")
+  else
+    vim.cmd("noautocmd wqa")
+  end
+end, { desc = "Write all buffers and quit without auto-commands", bang = true })
 
 -- Command to close all buffers at once
 vim.api.nvim_create_user_command("Bd", function(args)
