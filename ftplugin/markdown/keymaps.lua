d9b9b6fc37ae2e9d9ec93075e59c00c010a30f6a
clementjumel <clement.jumel@gmail.com>
2024-11-16 16:25:00 +0100

    🔥 Remove markdown-specific keymaps

--- ftplugin/markdown/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- In Markdown, Treesitter class navigation will actually navigate between headers, so this can be used directly for
---  this purpose
-
--- Use builtin sentence text-objects instead of custom sub-word ones
-vim.keymap.set({ "x", "o" }, "is", "is", { desc = "inner sentence (Markdown)", buffer = true })
-vim.keymap.set({ "x", "o" }, "as", "as", { desc = "a sentence (Markdown)", buffer = true })
