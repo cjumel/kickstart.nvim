d98f1ae7dbefee38c1b55f302b6a346f4b28db48
clementjumel <clement.jumel@gmail.com>
2024-12-10 10:09:35 +0100

    ⚡️ Improve snacks.nvim dashboard and remove automatic opening
    Automatically opening the dashboard when all buffers are closed doesn't
    work super well (and it sometimes make errors appear repeatedly), and it
    encourages the user to close buffers, which I don't think is great.

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -13,37 +13,14 @@ return {
     { "<leader>;", function() require("snacks").notifier.show_history() end, desc = "Notification history" },
     { "<leader><BS>", function() require("snacks").bufdelete() end, desc = "Delete buffer" },
   },
-  init = function()
-    -- Automatically open the dashboard when all buffers are deleted
-    local group = vim.api.nvim_create_augroup("SnacksDashboardOpenWhenBuffersAreClosed", { clear = true })
-    vim.api.nvim_create_autocmd("BufDelete", {
-      group = group,
-      callback = function()
-        vim.schedule(function() vim.api.nvim_exec_autocmds("User", { pattern = "BufDeletePost" }) end)
-      end,
-    })
-    vim.api.nvim_create_autocmd("User", {
-      group = group,
-      pattern = "BufDeletePost",
-      callback = function(event)
-        local deleted_name = vim.api.nvim_buf_get_name(event.buf)
-        local deleted_filetype = vim.api.nvim_get_option_value("filetype", { buf = event.buf })
-        local deleted_buftype = vim.api.nvim_get_option_value("buftype", { buf = event.buf })
-        local dashboard_on_empty = deleted_name == "" and deleted_filetype == "" and deleted_buftype == ""
-
-        if dashboard_on_empty then
-          require("snacks").dashboard()
-        end
-      end,
-    })
-  end,
   opts = {
     dashboard = {
       enabled = true,
       sections = {
         { section = "header" },
-        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-        { icon = "󰊢 ", key = "g", desc = "Git status files", action = telescope_builtin.git_status },
+        { icon = " ", key = "c", desc = "Current directory", action = function() require("oil").open() end },
+        { icon = "󰊢 ", key = "n", desc = "Neogit", action = function() require("neogit").open() end },
+        { icon = "󰚰 ", key = "g", desc = "Find Git Status Files", action = telescope_builtin.git_status },
         { icon = " ", key = "f", desc = "Find Files", action = telescope_builtin.find_files },
         { icon = " ", key = "d", desc = "Find Directories", action = telescope_builtin.find_directories },
         { icon = " ", key = "r", desc = "Find Recent files", action = telescope_builtin.recent_files },
