8a27b6e7785bd823ff05fbc26ad779e0c2c19170
clementjumel <clement.jumel@gmail.com>
2024-12-14 19:34:25 +0100

    ♻️ Use snacks.nvim to get git root

--- lua/plugins/tools/overseer/templates/pre-commit.lua
+++ lua/plugins/tools/overseer/templates/pre-commit.lua
@@ -1,23 +1,3 @@
---- Output the root path of the Git repository containing the directory whose path is provided, or nil if there is none.
----@param dir_path string|nil The path of a directory (default to the cwd).
----@param mods string|nil The modifiers to apply to the output path.
----@return string|nil
-local function get_git_root(dir_path, mods)
-  dir_path = dir_path or vim.fn.getcwd()
-
-  -- Find the path of a ".git" directory in the target directory or any of its ancestors
-  local dot_git_dir_path = vim.fn.finddir(".git", dir_path .. ";")
-  if dot_git_dir_path == "" then -- ".git" directory not found
-    return
-  end
-
-  local git_root_path = vim.fn.fnamemodify(dot_git_dir_path, ":h") -- Parent directory of ".git/"
-  if mods then
-    git_root_path = vim.fn.fnamemodify(git_root_path, mods)
-  end
-  return git_root_path
-end
-
 return {
   name = "pre-commit",
   condition = {
@@ -25,7 +5,7 @@ return {
       if vim.fn.executable("pre-commit") == 0 then
         return false
       end
-      local git_root_path = get_git_root()
+      local git_root_path = require("snacks").git.get_root(vim.fn.getcwd())
       if git_root_path == nil then -- Not in a Git repository
         return false
       end
