83bdd5e44b09393de5cc977f9a3b877c416fc29e
clementjumel <clement.jumel@gmail.com>
2024-12-17 22:13:27 +0100

    🐛 Fix pre-commit Overseer template condition

--- lua/plugins/tools/overseer/templates/pre-commit.lua
+++ lua/plugins/tools/overseer/templates/pre-commit.lua
@@ -5,7 +5,8 @@ return {
       if vim.fn.executable("pre-commit") == 0 then
         return false
       end
-      local git_root_path = require("snacks").git.get_root(vim.fn.getcwd())
+      local dir_path = vim.fn.getcwd() .. "/.git" -- Without "/.git", the git root path detection below won't work
+      local git_root_path = require("snacks").git.get_root(dir_path)
       if git_root_path == nil then -- Not in a Git repository
         return false
       end
