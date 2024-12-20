d7777bee0dbe8c0e97e780b96c0bdf4ec27442b2
clementjumel <clement.jumel@gmail.com>
2024-12-20 15:06:10 +0100

    ⚡️ Some improvements in copilot.lua config

--- lua/plugins/core/copilot.lua
+++ lua/plugins/core/copilot.lua
@@ -3,7 +3,7 @@
 -- Lua and fully-featured version of GitHub Copilot integration in Neovim. Compared to copilot.vim, the official
 -- alternative, this version is easy to configure and customize, as it is not using pure vimscript. Using virtual text,
 -- this plugin is nicely complementary with floating-window completion tools, like nvim-cmp. However, since the model is
--- not run locally, this plugins require an active internet connection, in addition of an active GitHub Copilot
+-- not run locally, this plugin requires an active internet connection, in addition of an active GitHub Copilot
 -- subscription to work.
 
 local nvim_config = require("nvim_config")
@@ -11,17 +11,16 @@ local nvim_config = require("nvim_config")
 return {
   "zbirenbaum/copilot.lua",
   cond = not (nvim_config.light_mode or nvim_config.disable_copilot),
-  -- When lazy-loading on InsertEnter, the plugin doesn't work right away (just like copilot.vim)
-  event = { "BufNewFile", "BufReadPre" },
+  -- Spinning the Copilot server a bit before entering insert mode helps to provide suggestions right away
+  event = { "InsertEnter", "BufNewFile", "BufReadPre" },
   cmd = "Copilot",
   opts = {
-    panel = { enabled = false }, -- I don't use Copilot panel & its keymaps will conflict with others
+    panel = { enabled = false }, -- I don't use it & its keymaps conflict with others
     suggestion = {
-      auto_trigger = true, -- Don't require to use next/prev suggestion keymap to trigger Copilot completion
-      -- Disable all keymaps as they are implemented in `plugins/keymaps.lua`, since they are mixed with regular
-      -- features (moving cursor in insert mode), and to prevent any conflict
+      auto_trigger = true,
+      -- Copilot.lua keymaps are implemented in `plugins/keymaps.lua`, so let's disable all default keymaps
       keymap = { accept = false, accept_word = false, accept_line = false, next = false, prev = false, dismiss = false },
     },
-    filetypes = { yaml = true, markdown = true }, -- Enable Copilot on additional filetypes
+    filetypes = { yaml = true, markdown = true, gitcommit = true },
   },
 }
