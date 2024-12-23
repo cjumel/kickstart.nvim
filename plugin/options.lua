1469a732d3789cee19003f49b4675c2ebd82bc29
clementjumel <clement.jumel@gmail.com>
2024-12-23 23:37:48 +0100

    ⚡️ Simplify global options

--- plugin/options.lua
+++ plugin/options.lua
@@ -1,52 +1,42 @@
--- General Vim and Neovim options and related configurations.
-
 -- [[ Options ]]
 
--- Global user interface
-vim.opt.shortmess:append({ I = true }) -- Disable Neovim homepage on startup
+-- Global UI
 vim.o.mouse = "a" -- Enable mouse mode in all modes
-vim.o.breakindent = true -- Indent parts after break in wrapped lines
-vim.o.splitright = true -- Open new vertical split windows to the right
-vim.o.splitbelow = true -- Open new horizontal split windows below
-vim.opt.cursorline = true -- Enable hilighting of line where cursor is
-vim.opt.cursorlineopt = "both" -- The cursorline highlights both the line background and the line number foreground
-vim.opt.colorcolumn = "" -- No colorcolumn by default; ftplugins might change this for specific file types
-vim.opt.conceallevel = 0 -- No concealing by default; ftplugins might change this for specific file types
-vim.opt.fillchars = { eob = " " } -- Hide "~" symbols at the end of buffers
+vim.o.breakindent = true -- After a line wrap, indent the part on the new virtual line
+vim.o.splitright = true -- Open new vertical split window on the right
+vim.o.splitbelow = true -- Open new horizontal split window below
+vim.opt.cursorline = true -- Highlight the cursor line
+vim.o.matchpairs = vim.o.matchpairs .. ",<:>" -- Add recognized character pair
 
 -- Side column
-vim.wo.number = true -- Enable line numbering
-vim.wo.relativenumber = false -- Use absolute line numbering
-vim.wo.signcolumn = "number" -- Make sign column merged in line numbering column
+vim.wo.number = true -- Enable absolute line numbering
+vim.wo.signcolumn = "number" -- Add signs in the number column
 
 -- Search & replace
 vim.o.ignorecase = true -- Make search case-insensitive by default
 vim.o.smartcase = true -- Enable case-sensitive searching when "\C" or capital in search
-vim.o.hlsearch = true -- Enable highlight on search; use `:nohlsearch` to remove the search highlight
-vim.opt.inccommand = "split" -- Set split preview in incremental commands (replace/renaming)
+vim.o.hlsearch = true -- Highlight matches during search
+vim.opt.inccommand = "split" -- Preview modifications in split during incremental commands
 
--- Disable builtin auto-completion in favor of nvim-cmp
+-- Disable builtin auto-completion
 vim.o.complete = ""
 vim.o.completeopt = ""
 
 -- Internals
-vim.o.matchpairs = vim.o.matchpairs .. ",<:>" -- Add new recognized pairs of characters
 vim.o.undofile = true -- Save undo history to file
 vim.o.updatetime = 250 -- Decrease delay for writting swap files
-vim.o.timeout = true -- Enable timeout when receiving mapped key sequences
 vim.o.timeoutlen = 300 -- Decrease delay between keys in mapped sequences
 
 -- [[ Diagnostics ]]
 
 -- Diagnostics configuration
 vim.diagnostic.config({
-  float = { border = "rounded" }, -- Settings for the "open float diagnostic" popup
+  float = { border = "rounded" }, -- Better for transparent backgrounds
   severity_sort = true, -- Display most severe diagnostic in sign column
 })
 
--- Use symbols taken from lualine/components/diagnostics/config.lua in sign column for consistency
-local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = "󰋽 " }
-for type, icon in pairs(signs) do
+-- Use symbols taken from lualine/components/diagnostics/config.lua in sign column
+for type, icon in pairs({ Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = "󰋽 " }) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
