6604f32fb1852d5b09d8ac7f5a70c28f6cb201b8
clementjumel <clement.jumel@gmail.com>
2025-01-08 15:51:02 +0100

    ✨ Improve sonokai theme

--- lua/theme/sonokai.lua
+++ lua/theme/sonokai.lua
@@ -1,6 +1,3 @@
-local extensions = require("plugins.ui.lualine.extensions")
-local sections = require("plugins.ui.lualine.sections")
-
 local M = {}
 
 M.sonokai_enabled = true
@@ -9,11 +6,6 @@ M.lualine_opts = {
   options = {
     theme = "sonokai",
   },
-  sections = sections.minimalist,
-  extensions = extensions.build_extensions(sections.minimalist),
 }
-M.lualine_post_setup = function()
-  vim.opt.showmode = true -- Show mode in status line
-end
 
 return M
