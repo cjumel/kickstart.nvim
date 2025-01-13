497b14aa0e43b40591793c5458d29268a86b9309
clementjumel <clement.jumel@gmail.com>
2025-01-13 11:59:27 +0100

    🔥 Remove nvim-ufo plugin
    I've found an option to define prettier folds, so let's use it instead
    of nvim-ufo, as the latter doesn't bring much additional value (mostly
    hovering folds and supporting both treesitter- and manual-based
    folding), and since I don't use much folds, I don't think it is wort it.

--- plugin/options.lua
+++ plugin/options.lua
@@ -18,6 +18,52 @@ vim.o.smartcase = true -- Enable case-sensitive searching when "\C" or capital i
 vim.o.hlsearch = true -- Highlight matches during search
 vim.opt.inccommand = "split" -- Preview modifications in split during incremental commands
 
+-- Folding
+
+-- Custom fold text function with Treesitter syntax highlighting and fold ending, taken from
+-- https://www.reddit.com/r/neovim/comments/1fzn1zt/custom_fold_text_function_with_treesitter_syntax/
+local function fold_virt_text(result, s, lnum, coloff)
+  if not coloff then
+    coloff = 0
+  end
+  local text = ""
+  local hl
+  for i = 1, #s do
+    local char = s:sub(i, i)
+    local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
+    local _hl = hls[#hls]
+    if _hl then
+      local new_hl = "@" .. _hl.capture
+      if new_hl ~= hl then
+        table.insert(result, { text, hl })
+        text = ""
+        hl = nil
+      end
+      text = text .. char
+      hl = new_hl
+    else
+      text = text .. char
+    end
+  end
+  table.insert(result, { text, hl })
+end
+function _G.custom_foldtext()
+  local start = vim.fn.getline(vim.v.foldstart):gsub("\t", string.rep(" ", vim.o.tabstop))
+  local end_str = vim.fn.getline(vim.v.foldend)
+  local end_ = vim.trim(end_str)
+  local result = {}
+  fold_virt_text(result, start, vim.v.foldstart - 1)
+  table.insert(result, { " ... ", "Delimiter" })
+  fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match("^(%s+)") or ""))
+  return result
+end
+
+vim.opt.foldmethod = "expr" -- Define folding method with `foldexpr`
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use Treesitter-based folding
+vim.opt.foldlevelstart = 99 -- Open buffers with no fold closed
+vim.opt.fillchars = { fold = " " } -- Remove dots filling at the end of folded lines
+vim.opt.foldtext = "v:lua.custom_foldtext()" -- Custom folded line text
+
 -- Disable builtin auto-completion
 vim.o.complete = ""
 vim.o.completeopt = ""
