8408768cd4bd40293d8083d70fb584ffea32cc4a
clementjumel <clement.jumel@gmail.com>
2025-01-21 09:12:59 +0100

    ⚡️ Improve nvim-surround keymaps

--- lua/plugins/core/nvim-surround.lua
+++ lua/plugins/core/nvim-surround.lua
@@ -1,24 +1,32 @@
 -- nvim-surround
 --
--- Plugin to add, change, delete surrounding delimiter pairs with ease in Neovim. This very simple plugin is in my
--- opinion a must-have for any Neovim user.
+-- A classic plugin to add, change, delete surrounding delimiter pairs with ease in Neovim, like tpope's vim-surround.
+-- This is a very simple plugin, yet using a surround plugin, like this one, is, in my opinion a must-have for any
+-- Neovim user.
 
 return {
   "kylechui/nvim-surround",
+  -- Let's implemnt these keymaps using the lazy keys, as they provide more flexibility and support actual descriptions
+  -- (whereas the default keymaps don't)
+  -- Let's use the "(" and ")" keys interchangeably as general prefix for these keymaps:
+  -- - nvim-surround (and vim-surround) use by default "ys", "cs" or "ds", but these conflicts with existing builtin
+  -- keymaps, which makes them in practice not very practical, as you have to type the sequence of keys up until the
+  -- surrounding character (e.g. 'ysaw"') very quickly
+  -- - mini.surround use "s" as a general prefix, I already use "s", but let's re-use the same idea of general prefix
+  -- - I don't use the builtin "(" and ")" (next/previous sentence), so let's recycle it
   keys = {
-    -- insert & insert_line features are not re-implemented as I don't find them super useful
-    { "ys", "<Plug>(nvim-surround-normal)", desc = "Surround" },
-    { "ysc", "<Plug>(nvim-surround-normal-cur)", desc = "Surround current line" },
-    { "yS", "<Plug>(nvim-surround-normal-line)", desc = "Surround on new lines" },
-    { "ySc", "<Plug>(nvim-surround-normal-cur-line)", desc = "Surround current line on new lines" },
-    { "S", "<Plug>(nvim-surround-visual)", mode = "x", desc = "Surround" },
-    { "<C-s>", "<Plug>(nvim-surround-visual-line)", mode = "x", desc = "Surround on new lines" },
-    { "ds", "<Plug>(nvim-surround-delete)", desc = "Delete surrounds" },
-    { "cs", "<Plug>(nvim-surround-change)", desc = "Change surrounds" },
-    { "cS", "<Plug>(nvim-surround-change-line)", desc = "Change surrounds on new lines" },
+    { "(a", "<Plug>(nvim-surround-normal)", desc = "Add surrounds" },
+    { ")a", "<Plug>(nvim-surround-normal)", desc = "Add surrounds" },
+    { "(n", "<Plug>(nvim-surround-normal-line)", desc = "Add surrounds on new lines" },
+    { ")n", "<Plug>(nvim-surround-normal-line)", desc = "Add surrounds on new lines" },
+    { "(c", "<Plug>(nvim-surround-change)", desc = "Change surrounds" },
+    { ")c", "<Plug>(nvim-surround-change)", desc = "Change surrounds" },
+    { "(d", "<Plug>(nvim-surround-delete)", desc = "Delete surrounds" },
+    { ")d", "<Plug>(nvim-surround-delete)", desc = "Delete surrounds" },
+    { "(", "<Plug>(nvim-surround-visual)", mode = "x", desc = "Add surrounds" },
   },
   opts = {
-    keymaps = { -- Disable default keymaps, redefined as Lazy keys for lazy-loading & overwritting descriptions
+    keymaps = {
       insert = false,
       insert_line = false,
       normal = false,
