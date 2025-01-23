ea9038c6977e0bf9831c6f59837c88be8a5c3c2f
cjumel <clement.jumel@gmail.com>
2025-01-23 18:01:53 +0100

    ✨ Add more shorcuts in dashboard

--- lua/plugins/core/telescope.lua
+++ lua/plugins/core/telescope.lua
@@ -25,10 +25,10 @@ return {
     -- Main finders
     { "<leader>ff", custom_builtin.find_files, mode = { "n", "v" }, desc = "[F]ind: [F]iles" },
     { "<leader>fd", custom_builtin.find_directories, mode = { "n", "v" }, desc = "[F]ind: [D]irectories" },
-    { "<leader>fl", custom_builtin.find_lines, mode = { "n", "v" }, desc = "[F]ind: [L]ines" },
-    { "<leader>fg", custom_builtin.live_grep, mode = { "n", "v" }, desc = "[F]ind: by [G]rep" },
-    { "<leader>fo", custom_builtin.old_files, mode = { "n", "v" }, desc = "[F]ind: [O]ld files" },
+    { "<leader>fg", custom_builtin.live_grep, mode = { "n", "v" }, desc = "[F]ind: with [G]rep" },
     { "<leader>fr", custom_builtin.recent_files, mode = { "n", "v" }, desc = "[F]ind: [R]ecent files" },
+    { "<leader>fo", custom_builtin.old_files, mode = { "n", "v" }, desc = "[F]ind: [O]ld files" },
+    { "<leader>fl", custom_builtin.find_lines, mode = { "n", "v" }, desc = "[F]ind: [L]ines" },
 
     -- Help-related
     { "<leader>?h", custom_builtin.help_tags, desc = "Help: help tags" },
