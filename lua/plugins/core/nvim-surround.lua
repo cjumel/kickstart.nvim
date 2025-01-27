532a437ce86698947907f78e75e1f49c42c30044
cjumel <clement.jumel@gmail.com>
2025-01-27 14:53:13 +0100

    🩹 Simplify nvim-surround keymaps

--- lua/plugins/core/nvim-surround.lua
+++ lua/plugins/core/nvim-surround.lua
@@ -7,23 +7,13 @@
 return {
   "kylechui/nvim-surround",
   -- Let's implemnt these keymaps using the lazy keys, as they provide more flexibility and support actual descriptions
-  -- (whereas the default keymaps don't)
-  -- Let's use the "(" and ")" keys interchangeably as general prefix for these keymaps:
-  -- - nvim-surround (and vim-surround) use by default "ys", "cs" or "ds", but these conflicts with existing builtin
-  -- keymaps, which makes them in practice not very practical, as you have to type the sequence of keys up until the
-  -- surrounding character (e.g. 'ysaw"') very quickly
-  -- - mini.surround use "s" as a general prefix, I already use "s", but let's re-use the same idea of general prefix
-  -- - I don't use the builtin "(" and ")" (next/previous sentence), so let's recycle it
+  -- whereas the default keymaps don't
   keys = {
-    { "(a", "<Plug>(nvim-surround-normal)", desc = "Add surrounds" },
-    { ")a", "<Plug>(nvim-surround-normal)", desc = "Add surrounds" },
-    { "(n", "<Plug>(nvim-surround-normal-line)", desc = "Add surrounds on new lines" },
-    { ")n", "<Plug>(nvim-surround-normal-line)", desc = "Add surrounds on new lines" },
-    { "(c", "<Plug>(nvim-surround-change)", desc = "Change surrounds" },
-    { ")c", "<Plug>(nvim-surround-change)", desc = "Change surrounds" },
-    { "(d", "<Plug>(nvim-surround-delete)", desc = "Delete surrounds" },
-    { ")d", "<Plug>(nvim-surround-delete)", desc = "Delete surrounds" },
-    { "(", "<Plug>(nvim-surround-visual)", mode = "x", desc = "Add surrounds" },
+    { "ga", "<Plug>(nvim-surround-normal)", desc = "Add surrounds" },
+    { "ga", "<Plug>(nvim-surround-visual)", mode = "x", desc = "Add surrounds" },
+    { "gA", "<Plug>(nvim-surround-normal-line)", desc = "Add surrounds on new lines" },
+    { "gm", "<Plug>(nvim-surround-change)", desc = "Modify surrounds" },
+    { "gl", "<Plug>(nvim-surround-delete)", desc = "Lose surrounds" },
   },
   opts = {
     keymaps = {
