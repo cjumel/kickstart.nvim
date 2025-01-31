d1cbd139f1b19fb120a156361abd2c875058420d
cjumel <clement.jumel@gmail.com>
2025-01-31 10:51:49 +0100

    ✨ Support yanking modules in lazy directory

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,16 +1,19 @@
 -- [[ Keymaps ]]
 
 vim.keymap.set("n", "<leader>ym", function()
-  local path = (
-    vim
-      .fn
-      .expand("%:.") -- Get the relative file path
-      :gsub("^lua/", "") -- Remove the "lua/" prefix if it exists
-      :gsub("^lua/", "") -- Remove the "lua/" prefix if it exists
-      :gsub("%.lua$", "") -- Remove the ".lua" extension
-      :gsub(".init$", "") -- Remove the ".init" suffix (in case in an init.lua file)
-      :gsub("/", ".") -- Replace slashes with dots
-  )
+  local path = vim.fn.expand("%:.") -- File path relative to cwd
+  -- Only keep the path part after "lua/"
+  local path_split = vim.fn.split(path, "lua/")
+  if #path_split >= 2 then
+    path = path_split[#path_split]
+  elseif path:sub(1, 4) ~= "lua/" then -- File is not inside a "lua/" directory
+    error("File is not a module")
+  end
+  -- Remove various prefixes and suffixes, and replace "/" by "."
+  path = path:gsub("^lua/", "")
+  path = path:gsub("%.lua$", "")
+  path = path:gsub(".init$", "")
+  path = path:gsub("/", ".")
   vim.fn.setreg('"', path)
   vim.notify('Yanked "' .. path .. '"')
 end, { buffer = true, desc = "[Y]ank: [M]odule" })
