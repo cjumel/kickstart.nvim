2d310c9a4ce948b31d21aba64e844e146feeaf50
cjumel <clement.jumel@gmail.com>
2025-02-06 15:04:17 +0100

    ⬆️ Upgrade outdated plugins & fix linkup.nvim config

--- lua/plugins/tools/linkup.lua
+++ lua/plugins/tools/linkup.lua
@@ -1,28 +1,11 @@
 -- linkup.nvim
 --
--- Integrate the Linkup API directly in Neovim. The Linkup API provides access to LLM-augmented web search wich
--- additional access to Linkup premium sources.
+-- Integrate the Linkup API directly in Neovim. The Linkup API provides access to LLM-augmented web search, as well as
+-- to Linkup's premium sources.
 
 return {
   "cjumel/linkup.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  lazy = true,
-  init = function()
-    vim.api.nvim_create_user_command(
-      "LinkupStandardSearch",
-      function() require("linkup").standard_search() end,
-      { desc = "Run a Linkup API standard search" }
-    )
-    vim.api.nvim_create_user_command(
-      "LinkupDeepSearch",
-      function() require("linkup").deep_search() end,
-      { desc = "Run a Linkup API deep search" }
-    )
-    vim.api.nvim_create_user_command(
-      "LinkupOpenWebiste",
-      function() require("linkup").open_website() end,
-      { desc = "Open a website with Linkup API" }
-    )
-  end,
+  cmd = { "LinkupStandardSearch", "LinkupDeepSearch" },
   opts = {},
 }
