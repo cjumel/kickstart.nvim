077177c4c058822019c239d786833289c836dc34
cjumel <clement.jumel@gmail.com>
2025-02-16 13:43:13 +0100

    ✨ Add keymap to discard copilot suggestion

--- lua/plugins/core/copilot.lua
+++ lua/plugins/core/copilot.lua
@@ -1,23 +1,28 @@
 -- copilot.lua
 --
 -- Lua and fully-featured version of GitHub Copilot integration in Neovim. Compared to copilot.vim, the official
--- alternative, this version is easy to configure and customize, as it is not using pure vimscript. Using virtual text,
--- this plugin is nicely complementary with floating-window completion tools, like nvim-cmp. However, since the model is
--- not run locally, this plugin requires an active internet connection, in addition of an active GitHub Copilot
--- subscription to work.
+-- alternative, this plugin is easy to configure and customize. As it is using virtual text, this plugin is nicely
+-- complementary with popup-based completion tools, like nvim-cmp. However, copilot.lua (like copilot.vim) requires an
+-- active GitHub Copilot subscription and an active internet conection to work.
 
 return {
   "zbirenbaum/copilot.lua",
   cond = not (Metaconfig.light_mode or Metaconfig.disable_copilot),
-  -- Spinning the Copilot server a bit before entering insert mode helps to provide suggestions right away
+  -- Spinning the Copilot server a bit before entering insert mode helps providing suggestions right away
   event = { "InsertEnter", "BufNewFile", "BufReadPre" },
   cmd = "Copilot",
   opts = {
     panel = { enabled = false }, -- I don't use it & its keymaps conflict with others
     suggestion = {
       auto_trigger = true,
-      -- Copilot.lua keymaps are implemented in `plugins/keymaps.lua`, so let's disable all default keymaps
-      keymap = { accept = false, accept_word = false, accept_line = false, next = false, prev = false, dismiss = false },
+      keymap = {
+        accept = false, -- <Tab>, defined in `plugins/keymaps.lua`
+        accept_word = false, -- <C-^> (or <C-,> on my keyboard), defined in `plugins/keymaps.lua`
+        accept_line = false, -- <C-e>, defined in `plugins/keymaps.lua`
+        next = false,
+        prev = false,
+        dismiss = "<C-x>", -- By default, <C-x> is used mainly for omni-completion, but I don't use it
+      },
     },
     filetypes = { yaml = true, markdown = true, gitcommit = true },
   },
