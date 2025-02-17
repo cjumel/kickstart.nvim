abf727d88bb02c5b4ee0d16cc059115655a50fb8
cjumel <clement.jumel@gmail.com>
2025-02-18 00:08:08 +0100

    🩹 Small improvements in Trouble config

--- lua/plugins/core/trouble.lua
+++ lua/plugins/core/trouble.lua
@@ -2,14 +2,14 @@
 --
 -- Trouble provides pretty lists for showing diagnostics, references, telescope results, quickfix lists, etc., to help
 -- you solve all the trouble your code is causing. With a very cool and customizable user interface and the
--- integratation of many other tools (like the Todo Comments plugins), it is very complementary with telescope.nvim.
+-- integratation of many other tools (like todo-comments.nvim), it is very complementary with telescope.nvim.
 
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   cmd = { "Trouble" }, -- Useful for other plugins calling Trouble through a command
   keys = {
-    { "<leader>vd", "<cmd>Trouble diagnostics_document toggle<CR>", desc = "[V]iew: buffer [D]iagnostics" },
+    { "<leader>vd", "<cmd>Trouble diagnostics_document toggle<CR>", desc = "[V]iew: document [D]iagnostics" },
     { "<leader>vD", "<cmd>Trouble diagnostics toggle<CR>", desc = "[V]iew: workspace [D]iagnostics" },
     { "<leader>vl", "<cmd>Trouble loclist toggle<CR>", desc = "[V]iew: [L]ocation list" },
     { "<leader>vq", "<cmd>Trouble qflist toggle<CR>", desc = "[V]iew: [Q]uickfix list" },
@@ -17,32 +17,31 @@ return {
   },
   opts = {
     keys = {
-      ["<Tab>"] = "jump",
+      ["<Tab>"] = "fold_toggle",
       [","] = "next",
       [";"] = "prev",
     },
     modes = {
-      -- Builtin modes
+      -- Builtin sources
       diagnostics = {
-        title = "{hl:Title}Workspace Diagnostics{hl} {count}",
-        open_no_results = true, -- Open the Trouble window even when there is no item as they can appear dynamically
+        title = "{hl:Title}Workspace Diagnostics{hl} {count}", -- Add a title
       },
       diagnostics_document = {
         mode = "diagnostics",
-        title = "{hl:Title}Document Diagnostics{hl} {count}",
-        filter = { buf = 0 }, -- List items from the current buffer only
-        groups = {}, -- Remove groups as this mode includes only current buffer items
+        title = "{hl:Title}Document Diagnostics{hl} {count}", -- Add a title
+        filter = { buf = 0 }, -- Filter current buffer items only
+        groups = {}, -- Remove the groups as there is only one
       },
       loclist = {
-        title = "{hl:Title}Location list{hl} {count}",
+        title = "{hl:Title}Location list{hl} {count}", -- Add a title
         focus = true, -- Focus the Trouble window when opened
       },
       qflist = {
-        title = "{hl:Title}Quickfix list{hl} {count}",
+        title = "{hl:Title}Quickfix list{hl} {count}", -- Add a title
         focus = true, -- Focus the Trouble window when opened
       },
 
-      -- LSP (see nvim-lspconfig)
+      -- LSP (see here and nvim-lspconfig)
       lsp_definitions = {
         focus = true, -- Focus the Trouble window when opened
         auto_refresh = false, -- Don't refresh Trouble window when cursor moves in the main window
@@ -57,7 +56,7 @@ return {
       },
       symbols = {
         open_no_results = true, -- Open the Trouble window even when there is no item as the LSP can be slow
-        groups = {}, -- Remove the file name as there is only one
+        groups = {}, -- Remove groups as there is only one
         format = "{kind_icon} {symbol.name}", -- Simplify the displayed information
       },
 
@@ -69,7 +68,7 @@ return {
         mode = "todo",
         title = "{hl:Title}Now todo-comments{hl} {count}", -- Add a title
         filter = { tag = { "NOW" } }, -- Filter todo-comment tags
-        groups = { { "filename", format = "{file_icon} {filename} {count}" } }, -- Remove tag group as we only have one
+        groups = { { "filename", format = "{file_icon} {filename} {count}" } }, -- Remove tag group as there is only one
       },
     },
   },
