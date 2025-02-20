1161d044776ac665df546b5a872856b5a0061c53
cjumel <clement.jumel@gmail.com>
2025-02-20 13:45:48 +0100

    🐛 Fix not available <S-BS> Snacks.nvim keymap

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -11,7 +11,7 @@ return {
   keys = {
     -- Bufdelete
     { "<leader><BS>", function() Snacks.bufdelete.delete() end, desc = "Delete current buffer" },
-    { "<leader><S-BS>", function() Snacks.bufdelete.all() end, desc = "Delete all buffers" },
+    { "<leader><C-BS>", function() Snacks.bufdelete.all() end, desc = "Delete all buffers" },
     { "<leader><M-BS>", function() Snacks.bufdelete.other() end, desc = "Delete other buffers" },
 
     -- Gitbrowse
