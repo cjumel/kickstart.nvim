b151f24a443e2dbc53127a9e6166d3f9213cd901
cjumel <clement.jumel@gmail.com>
2025-02-24 18:49:21 +0100

    🐛 Fix delete word keymaps in dap windows

--- lua/plugins/tools/nvim-dap.lua
+++ lua/plugins/tools/nvim-dap.lua
@@ -42,6 +42,9 @@ return {
         -- Keymaps to go through history like with the arrow keys (<C-n> & <C-p> are taken by nvim-cmp)
         vim.keymap.set("i", "<C-]>", require("dap.repl").on_up, { desc = "Previous history item", buffer = true }) -- <C-$>
         vim.keymap.set("i", "<C-\\>", require("dap.repl").on_down, { desc = "Next history item", buffer = true }) -- <C-`>
+        -- Fix delete word keymaps
+        vim.keymap.set("i", "<C-w>", "<C-S-w>", { desc = "Delete word", buffer = true })
+        vim.keymap.set("i", "<M-BS>", "<C-S-w>", { desc = "Delete word", buffer = true })
       end,
     })
   end,
