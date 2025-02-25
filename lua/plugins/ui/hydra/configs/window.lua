d5e07dd608100947e1c537b90329d97d49c87d34
cjumel <clement.jumel@gmail.com>
2025-02-25 23:43:26 +0100

    ✨ Further improve window menu keymaps

--- lua/plugins/ui/hydra/configs/window.lua
+++ lua/plugins/ui/hydra/configs/window.lua
@@ -1,11 +1,11 @@
 local hint = [[
-                                           Window   
-   _h_ ➜ Go to the left window    _l_ ➜ Go to the right window   _t_ ➜ Break window into [T]ab   
-   _H_ ➜ Decrease width           _L_ ➜ Increase width           _v_ ➜ Split window [V]ertically
-   _j_ ➜ Go to the down window    _o_ ➜ Close [O]ther windows    _w_ ➜ Switch windows   
-   _J_ ➜ Decrease height          _q_ ➜ [Q]uit window            _x_ ➜ Swap window with next
-   _k_ ➜ Go to the up window      _s_ ➜ [S]plit window           _=_ ➜ Equally high and wide   
-   _K_ ➜ Increase height          
+                                          Window   
+   _h_ ➜ Decrease [H]eight         _v_ ➜ Split window [V]ertically   _<C-h>_ ➜ Window left   
+   _H_ ➜ Increase [H]eight         _w_ ➜ Decrease [W]idth            _<C-j>_ ➜ Window down   
+   _o_ ➜ Close [O]ther windows     _W_ ➜ Increase [W]idth            _<C-k>_ ➜ Window up   
+   _q_ ➜ [Q]uit window             _x_ ➜ Swap window with next       _<C-l>_ ➜ Window left   
+   _s_ ➜ [S]plit window            _=_ ➜ Equally high and wide       _<C-w>_ ➜ Switch windows   
+   _t_ ➜ Break window into [T]ab   
 ]]
 return {
   key = { "<C-w>", desc = "Window menu" },
@@ -16,22 +16,22 @@ return {
     },
     hint = hint,
     heads = {
-      { "h", "<C-w>h" },
-      { "H", "<C-w><" },
-      { "j", "<C-w>j" },
-      { "J", "<C-w>-" },
-      { "k", "<C-w>k" },
-      { "K", "<C-w>+" },
-      { "l", "<C-w>l" },
-      { "L", "<C-w>>" },
+      { "h", "<C-w>-" },
+      { "H", "<C-w>+" },
       { "o", "<C-w>o" },
       { "q", "<C-w>q" },
       { "s", "<C-w>s" },
       { "t", "<C-w>T" },
       { "v", "<C-w>v" },
-      { "w", "<C-w>w" },
+      { "w", "<C-w><" },
+      { "W", "<C-w>>" },
       { "x", "<C-w>x" },
       { "=", "<C-w>=" },
+      { "<C-h>", "<C-w>h" },
+      { "<C-j>", "<C-w>j" },
+      { "<C-k>", "<C-w>k" },
+      { "<C-l>", "<C-w>l" },
+      { "<C-w>", "<C-w>w" },
       { "<Esc>", nil, { exit = true, desc = false } },
     },
   },
