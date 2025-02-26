564bda6f7c0748aaa9e903d248457c58e5f2a7e2
cjumel <clement.jumel@gmail.com>
2025-02-26 09:56:06 +0100

    ✨ Further improvements in window menu

--- lua/plugins/core/vim-tmux-navigator.lua
+++ lua/plugins/core/vim-tmux-navigator.lua
@@ -5,10 +5,16 @@
 
 return {
   "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+  },
   keys = {
     { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", mode = { "n", "t" }, desc = "Window left" },
-    { "<C-l>", "<cmd>TmuxNavigateRight<CR>", mode = { "n", "t" }, desc = "Window right" },
     { "<C-j>", "<cmd>TmuxNavigateDown<CR>", mode = { "n", "t" }, desc = "Window down" },
     { "<C-k>", "<cmd>TmuxNavigateUp<CR>", mode = { "n", "t" }, desc = "Window up" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<CR>", mode = { "n", "t" }, desc = "Window right" },
   },
 }
--- lua/plugins/ui/hydra/configs/window.lua
+++ lua/plugins/ui/hydra/configs/window.lua
@@ -1,36 +1,36 @@
 local hint = [[
                                           Window   
-   _h_ ➜ Decrease [H]eight         _v_ ➜ Split window [V]ertically   _<C-h>_ ➜ Window left   
-   _H_ ➜ Increase [H]eight         _w_ ➜ Decrease [W]idth            _<C-j>_ ➜ Window down   
-   _o_ ➜ Close [O]ther windows     _W_ ➜ Increase [W]idth            _<C-k>_ ➜ Window up   
+   _h_ ➜ Increase [H]eight         _v_ ➜ Split window [V]ertically   _<C-h>_ ➜ Window left   
+   _H_ ➜ Decrease [H]eight         _w_ ➜ Increase [W]idth            _<C-j>_ ➜ Window down   
+   _o_ ➜ Close [O]ther windows     _W_ ➜ Decrease [W]idth            _<C-k>_ ➜ Window up   
    _q_ ➜ [Q]uit window             _x_ ➜ Swap window with next       _<C-l>_ ➜ Window left   
    _s_ ➜ [S]plit window            _=_ ➜ Equally high and wide       _<C-w>_ ➜ Switch windows   
    _t_ ➜ Break window into [T]ab   
 ]]
 return {
-  key = { "<C-w>", desc = "Window menu" },
+  key = { "<leader>,", desc = "Window menu" },
   opts = {
-    body = "<C-w>",
+    body = "<leader>,",
     config = {
       desc = "Window menu",
     },
     hint = hint,
     heads = {
-      { "h", "<C-w>-" },
-      { "H", "<C-w>+" },
+      { "h", "<C-w>+" },
+      { "H", "<C-w>-" },
       { "o", "<C-w>o" },
       { "q", "<C-w>q" },
       { "s", "<C-w>s" },
       { "t", "<C-w>T" },
       { "v", "<C-w>v" },
-      { "w", "<C-w><" },
-      { "W", "<C-w>>" },
+      { "w", "<C-w>>" },
+      { "W", "<C-w><" },
       { "x", "<C-w>x" },
       { "=", "<C-w>=" },
-      { "<C-h>", "<C-w>h" },
-      { "<C-j>", "<C-w>j" },
-      { "<C-k>", "<C-w>k" },
-      { "<C-l>", "<C-w>l" },
+      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<CR>" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<CR>" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<CR>" },
       { "<C-w>", "<C-w>w" },
       { "<Esc>", nil, { exit = true, desc = false } },
     },
