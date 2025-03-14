4dae5ac5872ee48d62f82055054f607f560ab556
cjumel <clement.jumel@gmail.com>
2025-03-14 20:35:33 +0100

    ⚡️ Improve DAP menu keymaps

--- lua/plugins/ui/hydra/configs/nvim-dap.lua
+++ lua/plugins/ui/hydra/configs/nvim-dap.lua
@@ -9,18 +9,9 @@ return {
     heads = {
       -- Don't use Neovim arrow keys ("h", "j", "k", "l") or movement keys ("w", "b", "e" and their upper-case variants)
       -- to be able to navigate in the code and preview variables
-      {
-        "a",
-        function() -- Implementation taken from https://github.com/mfussenegger/nvim-dap/issues/1025
-          if vim.g.dap_last_config then
-            require("dap").run(vim.g.dap_last_config)
-          else
-            error("No debug configuration found")
-          end
-        end,
-      },
-      { "c", function() require("dap").clear_breakpoints() end },
+      { "c", function() require("dap").repl.toggle() end },
       { "o", function() require("dap").toggle_breakpoint() end }, -- Mnemonic: "o" has the shape of a breakpoint
+      { "O", function() require("dap").clear_breakpoints() end }, -- Mnemonic: "o" has the shape of a breakpoint
       { "p", function() require("dap.ui.widgets").hover() end },
       {
         "r",
@@ -31,7 +22,16 @@ return {
           require("dap").continue()
         end,
       },
-      { "R", function() require("dap").repl.toggle() end },
+      {
+        "R",
+        function() -- Implementation taken from https://github.com/mfussenegger/nvim-dap/issues/1025
+          if vim.g.dap_last_config then
+            require("dap").run(vim.g.dap_last_config)
+          else
+            error("No debug configuration found")
+          end
+        end,
+      },
       { "s", function() require("dap").pause() end },
       { "t", function() require("dap").terminate() end },
       { "u", function() require("dapui").toggle() end },
@@ -39,10 +39,10 @@ return {
       { "<Esc>", nil, { exit = true, mode = "n", desc = false } },
     },
     hint = [[
-                                 Debug   
-   _a_ ➜ Run [A]gain           _p_ ➜ [P]review variable   _s_ ➜ [S]top   
-   _c_ ➜ [C]lear Breakpoints   _r_ ➜ [R]un                _t_ ➜ [T]erminate   
-   _o_ ➜ Toggle Breakpoint     _R_ ➜ Toggle [R]EPL        _u_ ➜ Toggle [U]I   
+                                  Debug   
+   _c_ ➜ Toggle REPL [C]onsole   _p_ ➜ [P]review variable   _s_ ➜ [S]top   
+   _o_ ➜ Toggle Breakpoint       _r_ ➜ [R]un                _t_ ➜ [T]erminate   
+   _O_ ➜ Clear Breakpoints       _R_ ➜ [R]erun              _u_ ➜ Toggle [U]I   
 ]],
   },
 }
