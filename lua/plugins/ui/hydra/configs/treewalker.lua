f96a64fb46057b1115a66fa2289190c714437144
cjumel <clement.jumel@gmail.com>
2025-03-24 19:20:47 +0100

    🩹 Use "navigate" instead of "navigation" for treewalker.nvim menu

--- lua/plugins/ui/hydra/configs/treewalker.lua
+++ lua/plugins/ui/hydra/configs/treewalker.lua
@@ -1,8 +1,8 @@
 return {
-  key = { "<leader>n", desc = "[N]avigation menu" },
+  key = { "<leader>n", desc = "[N]avigate menu" },
   opts = {
     body = "<leader>n",
-    config = { desc = "[N]avigation menu" },
+    config = { desc = "[N]avigate menu" },
     heads = {
       { "h", "<cmd>Treewalker Left<CR>" },
       { "H", "<cmd>Treewalker SwapLeft<CR>" },
@@ -16,10 +16,10 @@ return {
       { "<Esc>", nil, { exit = true, mode = "n", desc = false } },
     },
     hint = [[
-                     Navigation   
-   _h_ ➜ Go left     _J_ ➜ Swap down    _l_ ➜ Go right   
-   _H_ ➜ Swap left   _k_ ➜ Go up        _L_ ➜ Swap right   
-   _j_ ➜ Go down     _K_ ➜ Swap up   
+                      Navigate   
+   _h_ ➜ Left        _J_ ➜ Swap down   _l_ ➜ Right   
+   _H_ ➜ Swap left   _k_ ➜ Up          _L_ ➜ Swap right   
+   _j_ ➜ Down        _K_ ➜ Swap up   
 ]],
   },
 }
