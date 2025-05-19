eba26b5ef121c93892e4ac7c77e8be652c5eb96c
cjumel <clement.jumel@gmail.com>
2025-05-19 11:51:48 +0200

    🩹 Simplify Lua-specific keymaps

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,13 +1,13 @@
 -- [[ Keymaps ]]
 
-vim.keymap.set("n", "<localleader>ym", function()
+vim.keymap.set("n", "<localleader>m", function()
   local module_ = require("lang_utils.lua").get_module()
   vim.fn.setreg('"', module_)
   vim.notify('Yanked to register `"`:\n```\n' .. module_ .. "\n```")
-end, { buffer = true, desc = "[Y]ank: [M]odule" })
+end, { buffer = true, desc = "Yank [M]odule" })
 
-vim.keymap.set("n", "<localleader>yr", function()
+vim.keymap.set("n", "<localleader>r", function()
   local run_command = [[dofile("]] .. vim.fn.expand("%:p:.") .. [[")]]
   vim.fn.setreg("+", run_command)
   vim.notify("Yanked to register `+`:\n```\n" .. run_command .. "\n```")
-end, { buffer = true, desc = "[Y]ank: [R]EPL run command" })
+end, { buffer = true, desc = "Yank [R]EPL run command" })
