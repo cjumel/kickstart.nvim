8c16355f39018000cd8ae70832b7dc788f4795c8
cjumel <clement.jumel@gmail.com>
2025-06-30 18:31:59 +0200

    ✨ New keymap for zoom and zen modes

--- lua/plugins/ui/hydra/configs/window.lua
+++ lua/plugins/ui/hydra/configs/window.lua
@@ -5,6 +5,7 @@ return {
     config = { desc = "Window menu" },
     heads = {
       { "d", "<cmd>vert new | set buftype=nofile | read ++edit # | 0d_ | diffthis | wincmd p | diffthis<CR>" },
+      { "e", function() Snacks.zen.zen() end, { exit = true } },
       { "h", "<C-w>h" },
       { "j", "<C-w>j" },
       { "k", "<C-w>k" },
@@ -18,7 +19,7 @@ return {
       { "v", "<C-w>v" },
       { "w", "<C-w>w" },
       { "x", "<C-w>x" },
-      { "z", function() Snacks.zen.zen() end, { exit = true } },
+      { "z", function() Snacks.zen.zoom() end, { exit = true } },
       { "!", "<C-w>+" }, -- Mnemonic: opposite shape of "-"
       { "-", "<C-w>-" },
       { "(", "<C-w><" },
@@ -28,13 +29,13 @@ return {
     },
     hint = [[
                                           Window
-   _d_ ➜ Buffer [D]iff            _q_ ➜ [Q]uit window               _z_ ➜ [Z]en mode   
+   _d_ ➜ Buffer [D]iff            _o_ ➜ Quit [O]ther windows        _x_ ➜ Swap window with next   
+   _e_ ➜ [E]nter zen mode         _q_ ➜ [Q]uit window               _z_ ➜ [Z]oom   
    _h_ ➜ Window left              _r_ ➜ [R]eset diagnostics         _!_ ➜ Increase height   
    _j_ ➜ Winndow down             _s_ ➜ [S]plit window              _-_ ➜ Decrease height   
    _k_ ➜ Window up                _t_ ➜ Break window into [T]ab     _(_ ➜ Decrease width   
    _l_ ➜ Window right             _v_ ➜ Split window [V]ertically   _)_ ➜ Increase width   
    _n_ ➜ [N]otification history   _w_ ➜ S[W]itch windows            _=_ ➜ Equalize hight/width   
-   _o_ ➜ Quit [O]ther windows     _x_ ➜ Swap window with next       
 ]],
   },
 }
