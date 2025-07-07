b53ee4ab8a8f993a12c2ac2b3b9da555a3324c8d
cjumel <clement.jumel@gmail.com>
2025-07-07 12:48:47 +0200

    🔥 Remove visual mode module

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -160,9 +160,54 @@ end
 vim.keymap.set("n", "<leader>yb", yank_buffer_content, { desc = "[Y]ank: [B]uffer content" })
 vim.keymap.set("n", "<leader>ya", yank_all_buffer_contents, { desc = "[Y]ank: [A]ll buffer contents" })
 
+local function visual_mode_is_on()
+  return vim.tbl_contains({
+    "v", -- Visual mode
+    "V", -- Visual line mode
+    "\22", -- Visual block modes
+  }, vim.fn.mode())
+end
+local function visual_mode_get_text()
+  local _, srow, scol = unpack(vim.fn.getpos("v"))
+  local _, erow, ecol = unpack(vim.fn.getpos("."))
+
+  local mode = vim.fn.mode()
+  local lines = {}
+  if mode == "V" then -- Visual line mode
+    if srow > erow then
+      lines = vim.api.nvim_buf_get_lines(0, erow - 1, srow, true)
+    else
+      lines = vim.api.nvim_buf_get_lines(0, srow - 1, erow, true)
+    end
+  elseif mode == "v" then
+    if srow < erow or (srow == erow and scol <= ecol) then
+      lines = vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {})
+    else
+      lines = vim.api.nvim_buf_get_text(0, erow - 1, ecol - 1, srow - 1, scol, {})
+    end
+  elseif mode == "\22" then -- Visual block mode
+    if srow > erow then
+      srow, erow = erow, srow
+    end
+    if scol > ecol then
+      scol, ecol = ecol, scol
+    end
+    for i = srow, erow do
+      table.insert(
+        lines,
+        vim.api.nvim_buf_get_text(0, i - 1, math.min(scol - 1, ecol), i - 1, math.max(scol - 1, ecol), {})[1]
+      )
+    end
+  end
+  local trimmed_lines = {}
+  for _, line in ipairs(lines) do
+    line = line:gsub("^%s+", ""):gsub("%s+$", "")
+    table.insert(trimmed_lines, line)
+  end
+  return table.concat(trimmed_lines, " ")
+end
 local function browser_search()
-  local visual_mode = require("visual_mode")
-  local default_text = visual_mode.is_on() and visual_mode.get_text() or nil
+  local default_text = visual_mode_is_on() and visual_mode_get_text() or nil
   vim.ui.input({ prompt = "Web search", default = default_text }, function(input)
     if input == nil or input == "" then
       return
