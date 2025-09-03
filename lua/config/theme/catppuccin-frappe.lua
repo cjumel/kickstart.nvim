b34ccabbdc9d98174aa10cfefb727e9bbf8713cb
cjumel <clement.jumel@gmail.com>
2025-09-03 17:43:09 +0200

    🩹 Simplify catppuccin-frappe theme

--- lua/config/theme/catppuccin-frappe.lua
+++ lua/config/theme/catppuccin-frappe.lua
@@ -3,13 +3,4 @@ local M = {}
 M.catppuccin_enabled = true
 M.catppuccin_style = "frappe"
 
-M.get_lualine_opts = function(opts)
-  return {
-    options = { section_separators = { left = "", right = "" } },
-    sections = vim.tbl_deep_extend("force", opts.sections, {
-      lualine_a = { { "mode", separator = { left = "", right = "" } } },
-    }),
-  }
-end
-
 return M
