7e8738c5dc64166d096eebd85ec3e66a07b27795
cjumel <clement.jumel@gmail.com>
2025-11-23 14:34:20 +0100

    🐛 Fix snippet conditions

--- lua/config/snippets/conditions.lua
+++ lua/config/snippets/conditions.lua
@@ -1,12 +1,13 @@
 local ls_conds = require("luasnip.extras.conditions")
-local utils = require("config.snippets.utils")
+local snippet_utils = require("config.snippets.utils")
 
 local M = {}
 
 ---@param line_to_cursor string
 ---@return boolean
 local function line_begin_function(line_to_cursor)
-  local line_to_trigger = utils.get_line_to_trigger(line_to_cursor)
+  -- This only work with a `min_keyword_length` greater than 0
+  local line_to_trigger = snippet_utils.get_line_to_trigger(line_to_cursor)
   return string.match(line_to_trigger, "^%s*$") ~= nil
 end
 M.line_begin = ls_conds.make_condition(line_begin_function)
@@ -14,9 +15,9 @@ M.line_begin = ls_conds.make_condition(line_begin_function)
 ---@param line_to_cursor string
 ---@return boolean
 local function line_end_function(line_to_cursor)
-  -- Implementation taken from `luasnip.extras.conditions.show`
   local line = vim.api.nvim_get_current_line()
-  return line_to_cursor == line
+  -- With blink.cmp, `line_to_cursor` does not include the character under the cursor
+  return line_to_cursor == line:sub(1, -2)
 end
 M.line_end = ls_conds.make_condition(line_end_function)
 
@@ -35,7 +36,7 @@ local function is_comment_start_function(line_to_cursor)
     return false
   end
   local commentstring_prefix = string.sub(vim.bo.commentstring, 1, #vim.bo.commentstring - 2)
-  local line_to_trigger = utils.get_line_to_trigger(line_to_cursor)
+  local line_to_trigger = snippet_utils.get_line_to_trigger(line_to_cursor)
   local line_to_trigger_end =
     string.sub(line_to_trigger, #line_to_trigger - #commentstring_prefix + 1, #line_to_trigger)
   return line_to_trigger_end == commentstring_prefix
@@ -46,7 +47,7 @@ M.comment_start = ls_conds.make_condition(is_comment_start_function)
 ---@return table
 M.make_prefix_condition = function(prefix)
   return ls_conds.make_condition(function(line_to_cursor)
-    local line_to_trigger = utils.get_line_to_trigger(line_to_cursor)
+    local line_to_trigger = snippet_utils.get_line_to_trigger(line_to_cursor)
     local line_to_trigger_end = string.sub(line_to_trigger, #line_to_trigger - #prefix + 1, #line_to_trigger)
     if line_to_trigger_end ~= prefix then
       return false
@@ -60,7 +61,7 @@ end
 ---@return table
 M.make_strict_prefix_condition = function(prefix)
   return ls_conds.make_condition(function(line_to_cursor)
-    local line_to_trigger = utils.get_line_to_trigger(line_to_cursor)
+    local line_to_trigger = snippet_utils.get_line_to_trigger(line_to_cursor)
     return line_to_trigger == prefix
   end)
 end
@@ -69,7 +70,7 @@ end
 ---@return table
 M.make_ts_node_in_condition = function(node_types)
   return ls_conds.make_condition(function(line_to_cursor)
-    local is_treesitter_parsable_, node = pcall(utils.get_treesitter_node_at_trigger, line_to_cursor)
+    local is_treesitter_parsable_, node = pcall(snippet_utils.get_treesitter_node_at_trigger, line_to_cursor)
     if not is_treesitter_parsable_ then -- Treesitter is not available or the buffer is not parsable
       return false
     end
@@ -84,7 +85,7 @@ end
 ---@return table
 M.make_ts_node_not_in_condition = function(node_types)
   return ls_conds.make_condition(function(line_to_cursor)
-    local is_treesitter_parsable_, node = pcall(utils.get_treesitter_node_at_trigger, line_to_cursor)
+    local is_treesitter_parsable_, node = pcall(snippet_utils.get_treesitter_node_at_trigger, line_to_cursor)
     if not is_treesitter_parsable_ then -- Treesitter is not available or the buffer is not parsable
       return false
     end
