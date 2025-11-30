533699d1f1d36644aa5145b298eee38474a548fb
cjumel <clement.jumel@gmail.com>
2025-11-30 22:21:20 +0100

    ✨ More sh and zsh snippets

--- lua/config/snippets/snippets/sh.lua
+++ lua/config/snippets/snippets/sh.lua
@@ -10,11 +10,26 @@ local t = ls.text_node
 
 return {
   s({ trig = "alias", show_condition = snippet_conds.empty_line }, { t("alias "), i(1), t("='"), i(2), t("'") }),
+
+  s(
+    { trig = "elif", show_condition = snippet_conds.empty_line },
+    { t("elif [ "), i(1), t(" ]; then"), t({ "", "\t" }), i(2) }
+  ),
+
+  s({ trig = "else", show_condition = snippet_conds.empty_line }, { t({ "else", "\t" }), i(1) }),
+
   s({ trig = "export", show_condition = snippet_conds.empty_line }, { t("export "), i(1), t("="), i(2) }),
+
   s(
     { trig = "function", show_condition = snippet_conds.empty_line },
     { t("function "), i(1), t("("), i(2), t({ ") { ", "\t" }), i(3), t({ "", "}" }) }
   ),
+
+  s(
+    { trig = "if", show_condition = snippet_conds.empty_line },
+    { t("if [ "), i(1), t(" ]; then"), t({ "", "\t" }), i(2), t({ "", "fi" }) }
+  ),
+
   s({ trig = "local", show_condition = snippet_conds.empty_line }, {
     c(1, {
       sn(nil, { t("local "), r(1, "name", i(nil)), t("="), i(2) }),
