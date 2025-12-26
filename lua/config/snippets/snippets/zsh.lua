96a66acd6d246e8de66c168f5eefef7ecc1c508c
cjumel <clement.jumel@gmail.com>
2025-12-26 17:08:07 +0100

    🩹 Update snippets

--- lua/config/snippets/snippets/zsh.lua
+++ lua/config/snippets/snippets/zsh.lua
@@ -11,10 +11,12 @@ local t = ls.text_node
 return {
   s({ trig = "alias", show_condition = snippet_conds.empty_line }, { t("alias "), i(1), t("='"), i(2), t("'") }),
 
-  s(
-    { trig = "elif", show_condition = snippet_conds.empty_line },
-    { t("elif [[ "), i(1), t(" ]]; then"), t({ "", "\t" }), i(2) }
-  ),
+  s({ trig = "elif", show_condition = snippet_conds.empty_line }, {
+    c(1, {
+      sn(nil, { t("elif [ "), r(1, "cond", i(nil)), t(" ]; then"), t({ "", "\t" }), r(2, "content", i(nil, "pass")) }),
+      sn(nil, { t("elif [ ! "), r(1, "cond"), t(" ]; then"), t({ "", "\t" }), r(2, "content") }),
+    }),
+  }),
 
   s({ trig = "else", show_condition = snippet_conds.empty_line }, { t({ "else", "\t" }), i(1) }),
 
@@ -27,7 +29,17 @@ return {
 
   s(
     { trig = "if", show_condition = snippet_conds.empty_line },
-    { t("if [[ "), i(1), t(" ]]; then"), t({ "", "\t" }), i(2), t({ "", "fi" }) }
+    c(1, {
+      sn(nil, {
+        t("if [ "),
+        r(1, "cond", i(nil)),
+        t(" ]; then"),
+        t({ "", "\t" }),
+        r(2, "content", i(nil, "pass")),
+        t({ "", "fi" }),
+      }),
+      sn(nil, { t("if [ ! "), r(1, "cond"), t(" ]; then"), t({ "", "\t" }), r(2, "content"), t({ "", "fi" }) }),
+    })
   ),
 
   s({ trig = "local", show_condition = snippet_conds.empty_line }, {
