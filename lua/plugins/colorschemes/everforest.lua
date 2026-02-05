cfb12389af37faa839b3b49012f192714774935d
cjumel <clement.jumel@gmail.com>
2026-02-05 10:06:05 +0100

    🔥 Remove everforest themes

--- lua/plugins/colorschemes/everforest.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sainnhe/everforest",
-  cond = vim.env["NVIM_ENABLE_ALL_PLUGINS"] or ThemeConfig.everforest_enabled or false,
-  priority = 1000, -- Main UI stuff should be loaded first
-  config = function()
-    vim.g.everforest_transparent_background = true
-    vim.cmd.colorscheme("everforest")
-  end,
-}
