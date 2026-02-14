63061a58d2dfd1663ef68e00c7a93ece26e37ec5
cjumel <clement.jumel@gmail.com>
2026-02-14 11:04:31 +0100

    🔥 Replace gruvbox-material.nvim with gruvbox.nvim

--- lua/plugins/colorschemes/gruvbox-material.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sainnhe/gruvbox-material",
-  cond = vim.env["NVIM_ENABLE_ALL_PLUGINS"] or ThemeConfig.colorscheme_name == "gruvbox-material",
-  priority = 1000, -- Main UI stuff should be loaded first
-  config = function()
-    vim.g.gruvbox_material_transparent_background = true
-    vim.cmd.colorscheme("gruvbox-material")
-  end,
-}
