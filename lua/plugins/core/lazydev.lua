e3d19a927aeb1eede64b3ec232ba6cf1096b3250
cjumel <clement.jumel@gmail.com>
2026-03-16 09:46:23 +0100

    ⬆️ Upgrade lazydev.nvim

--- lua/plugins/core/lazydev.lua
+++ lua/plugins/core/lazydev.lua
@@ -1,6 +1,3 @@
--- ISSUE: in recent versions, lazydev.nvim does not work well with LuaLS at a version strictly greater than 3.16.4 (see
--- this issue: https://github.com/folke/lazydev.nvim/issues/136); to install a working version of LuaLS, run:
--- `:MasonInstall lua-language-server@3.16.4`
 return {
   "folke/lazydev.nvim",
   ft = "lua",
@@ -8,7 +5,7 @@ return {
     library = {
       { path = "conform.nvim", words = { "conform%." } },
       { path = "snacks.nvim", words = { "Snacks%." } },
-      { path = vim.fn.stdpath("config"), words = { "nvim_config%." }}
+      { path = vim.fn.stdpath("config"), words = { "nvim_config%." } },
     },
   },
 }
