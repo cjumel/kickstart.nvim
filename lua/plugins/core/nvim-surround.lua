751aee0ec56eb9728d60b18316623111f19ea7fb
cjumel <clement.jumel@gmail.com>
2026-08-24 17:30:48 +0200

    🐛 Fix keymap conflict nvim-surround/substitute.nvim
    Both plugins define the gS keymap in visual mode, nvim-surround taking
    precedence in the cases this happened. To address the issue, I:
    - simplified the setup of nvim-surround for the recommended setup, as it
      avoid implying that the plugin doesn't define gS as a keymap in visual
      mode and prevent lazy-loading issue with this keymap
    - put range substitute keymaps under a shared prefix, gs, to avoid using
      the gS keymap in visual mode

--- lua/plugins/core/nvim-surround.lua
+++ lua/plugins/core/nvim-surround.lua
@@ -1,11 +1,6 @@
 return {
   "kylechui/nvim-surround",
-  keys = {
-    { "ys", desc = "Add surrounds" },
-    { "cs", desc = "Change surrounds" },
-    { "ds", desc = "Delete surrounds" },
-    { "S", mode = "x", desc = "Add surrounds" },
-  },
+  event = "VeryLazy",
   opts = {
     -- Remove the white spaces introduced when using some of the surrounds
     surrounds = { ["("] = false, ["["] = false, ["{"] = false, ["<"] = false },
