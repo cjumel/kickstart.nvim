c48674956680016e860272c733c62228d45a0043
cjumel <clement.jumel@gmail.com>
2026-08-24 17:30:48 +0200

    🐛 Fix keymap conflict nvim-surround/substitute.nvim
    Both plugins define the gS keymap in visual mode, nvim-surround taking
    precedence in the cases this happened. To address the issue, I:
    - simplified the setup of nvim-surround for the recommended setup, as it
      avoid implying that the plugin doesn't define gS as a keymap in visual
      mode and prevent lazy-loading issue with this keymap
    - put range substitute keymaps under a shared prefix, gs, to avoid using
      the gS keymap in visual mode

--- lua/plugins/core/substitute.lua
+++ lua/plugins/core/substitute.lua
@@ -2,43 +2,39 @@ return {
   "gbprod/substitute.nvim",
   dependencies = { "gbprod/yanky.nvim" },
   keys = {
-    -- Overwrite: like the paste operator but without yanking the overwritten text
+    -- Overwrite: overwrite a target with the clipboard content
     { "go", function() require("substitute").operator() end, desc = "Overwrite" },
     { "go", function() require("substitute").visual() end, mode = "x", desc = "Overwrite" },
-    -- Overwrite in range: overwrite all occurrences of a target in a range
+    -- Substitute: substitute occurrences of a target in a range (prefilled)
     {
-      "gO",
-      function() require("substitute.range").operator({ register = "0", auto_apply = true }) end,
-      desc = "Overwrite in range",
-    },
-    {
-      "gO",
-      function() require("substitute.range").visual({ register = "0", auto_apply = true }) end,
-      mode = "x",
-      desc = "Overwrite in range",
-    },
-    -- Substitute: update occurrences of a target in a range
-    {
-      "gs",
+      "gss",
       function() require("substitute.range").operator({ prompt_current_text = true }) end,
-      desc = "Substitute (prefilled)",
+      desc = "Substitute: substitute (prefilled)",
     },
     {
-      "gs",
+      "gss",
       function() require("substitute.range").visual({ prompt_current_text = true }) end,
       mode = "x",
-      desc = "Substitute (prefilled)",
+      desc = "Substitute: substitute (prefilled)",
     },
-    -- Substitute (not prefilled): replace occurrences of a target in a range
-    { "gS", function() require("substitute.range").operator() end, desc = "Substitute (not prefilled)" },
-    { "gS", function() require("substitute.range").visual() end, mode = "x", desc = "Substitute (not prefilled)" },
-    -- Exchange: swap two targets
-    { "ge", function() require("substitute.exchange").operator() end, desc = "Exchange" },
-    { "ge", function() require("substitute.exchange").visual() end, mode = "x", desc = "Exchange" },
+    -- Replace: replace occurrences of a target in a range (not prefilled)
+    { "gsr", function() require("substitute.range").operator() end, desc = "Substitute: replace" },
+    { "gsr", function() require("substitute.range").visual() end, mode = "x", desc = "Substitute: replace" },
+    -- Orverwrite (range): overwrite occurrences of a target in a range with the clipboard content
+    {
+      "gso",
+      function() require("substitute.range").operator({ register = "0", auto_apply = true }) end,
+      desc = "Substitute: overwrite (range)",
+    },
+    {
+      "gso",
+      function() require("substitute.range").visual({ register = "0", auto_apply = true }) end,
+      mode = "x",
+      desc = "Substitute: overwrite (range)",
+    },
+    -- Exchange: swap two targets together
+    { "gse", function() require("substitute.exchange").operator() end, desc = "Substitute: exchange" },
+    { "gse", function() require("substitute.exchange").visual() end, mode = "x", desc = "Substitute: exchange" },
   },
-  opts = function()
-    return {
-      on_substitute = require("yanky.integration").substitute(), -- yanky.nvim integration
-    }
-  end,
+  opts = function() return { on_substitute = require("yanky.integration").substitute() } end,
 }
